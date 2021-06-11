aad185605e8395d786e9c69634ca6497a610c690
Saaappi <alex.metz1024@gmail.com>
2021-06-11 17:00:50 -0500

    Added Desolation Hold flight path to "Desolation Hold Inspection" quest. Documented Southern Barrens quests.

--- FlightPaths.lua
+++ FlightPaths.lua
@@ -2,6 +2,7 @@ local addonName, t = ...;
 
 local flightPaths = {
 	["Forsaken High Command, Silverpine Forest"] = 645,
+	["Desolation Hold, Southern Barrens"] = 391,
 },
 
 t.flightPaths = flightPaths;
