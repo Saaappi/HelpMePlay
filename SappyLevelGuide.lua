60aab1aecfcc8db48fd6d4198671ab164adb9d0b
Saaappi <alex.metz1024@gmail.com>
2021-06-11 15:15:33 -0500

    Documented all Silverpine Forest quests. Added some TODOs.

--- SappyLevelGuide.lua
+++ SappyLevelGuide.lua
@@ -3,6 +3,15 @@
 ]]--
 
 --[[ TODO
+	- Figure out the event that fires when the flight path map is opened.
+	- Figure out how to take a flight path programmatically.
+	- Figure out the event that fires when a flight path is taken.
+		- Mark 'shouldTakeFlightPath' as false here.
+	
+	- Figure out the event(s) that fire when a quest is auto accepted.
+	- Figure out how to automatically acknowledge and auto accepted quest so the notification clears from the quest objective tracker.
+	
+	- Figure out how to auto claim quest rewards.
 ]]--
 
 --[[
@@ -15,17 +24,41 @@ local addonName, t = ...;
 -- Variables
 local e = CreateFrame("Frame");
 local map = 0;
+local shouldTakeFlightPath = false;
+local title = "";
 
 e:RegisterEvent("ADDON_LOADED");
+e:RegisterEvent("CINEMATIC_START");
+e:RegisterEvent("QUEST_ACCEPTED");
+e:RegisterEvent("QUEST_DATA_LOAD_RESULT");
 e:RegisterEvent("QUEST_DETAIL");
 e:RegisterEvent("ZONE_CHANGED_NEW_AREA");
 e:SetScript("OnEvent", function(self, event, addon)
 	if (event == "ADDON_LOADED" and addon == addonName) then
 		map = C_Map.GetBestMapForUnit("player");
 	end
-	if (event == "QUEST_DETAIL") then
+	if (event == "CINEMATIC_START") then
+		local canBeCancelled = CanCancelScene();
+		if (canBeCancelled and t.quests[map][title]["cancelCinematic"]) then
+			StopCinematic();
+		end
+	end
+	if (event == "QUEST_ACCEPTED") then
+		--[[
+		if (t.quests[map][title]["flightPath"]) then -- The quest has a flight path that should be taken.
+			shouldTakeFlightPath = true;
+		end
+		]]
+	end
+	if (event == "QUEST_DATA_LOAD_RESULT" or event == "QUEST_DETAIL") then
+		local id = ...;
 		if (QuestFrame) then
-			local title = QuestInfoTitleHeader:GetText();
+			if (id) then -- The quest's ID is available, so let's use that.
+				title = select(2, C_QuestLine.GetQuestLineInfo(id, map); 
+			else
+				title = QuestInfoTitleHeader:GetText();
+			end
+			
 			if (title) then
 				print(t.quests[map][title]["id"]);
 			end
