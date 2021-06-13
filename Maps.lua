4685f2f4a7d74aab7e8cf33ff3d3c8cb5aaeb9f3
Saaappi <alex.metz1024@gmail.com>
2021-06-12 23:18:33 -0500

    Finished migrating quests. Maps are now in their own table.

---
+++ Maps.lua
@@ -0,0 +1,18 @@
+local addonName, t = ...;
+
+local maps = {
+	[10] = "Northern Barrens",
+	[18] = "Tirisfal Glades",
+	[21] = "Silverpine Forest",
+	[22] = "Western Plaguelands",
+	[25] = "Hillsbrad Foothills",
+	[63] = "Ashenvale",
+	[65] = "Stonetalon Mountains",
+	[70] = "Dustwallow Marsh",
+	[85] = "Orgrimmar",
+	[199] = "Southern Barrens",
+	[525] = "Frostfire Ridge",
+	[572] = "Draenor", -- Cosmic
+};
+
+t.maps = maps;
