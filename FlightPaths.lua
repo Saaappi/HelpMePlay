7e79000b4935c6a22addccd29ae2e26887406fbd
Saaappi <alex.metz1024@gmail.com>
2021-06-15 00:34:14 -0500

    Added another quest to the flight paths table.

--- FlightPaths.lua
+++ FlightPaths.lua
@@ -2,6 +2,7 @@ local addonName, t = ...;
 
 local flightPaths = {
 	["Orcs are in Order"] = "Forsaken High Command, Silverpine Forest",
+	["Only One May Enter"] = "The Forsaken Front, Silverpine Forest",
 };
 
 t.flightPaths = flightPaths;
