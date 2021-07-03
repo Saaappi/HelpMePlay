7725f6a11e45f04cbebc46caebd859ce645f78d7
Saaappi <alex.metz1024@gmail.com>
2021-07-03 11:05:40 -0500

    Removed the Maps and Flight Paths files.

--- FlightPaths.lua
+++
@@ -1,8 +0,0 @@
-local addonName, t = ...;
-
-local flightPaths = {
-	["Orcs are in Order"] = "Forsaken High Command, Silverpine Forest",
-	["Only One May Enter"] = "The Forsaken Front, Silverpine Forest",
-};
-
-t.flightPaths = flightPaths;
