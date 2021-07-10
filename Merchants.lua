c739026a3d4cb7a3a2c34291e47233ac32001ef5
Saaappi <alex.metz1024@gmail.com>
2021-07-10 18:16:43 -0500

    Added support for purchasing quest items from merchants.

---
+++ Merchants.lua
@@ -0,0 +1,35 @@
+local addonName, t = ...
+local e = CreateFrame("Frame")
+
+e:RegisterEvent("MERCHANT_SHOW")
+
+local merchants = {
+	[160601] = { -- Caretaker Mirene
+		[6] = {
+			["name"] = "Elysian Thread",
+			["quantity"] = 1,
+		},
+	},
+	[166640] = { -- Au'larrynar
+		[1] = {
+			["name"] = "Necessary Enhancers",
+			["quantity"] = 1,
+		},
+	},
+}
+
+e:SetScript("OnEvent", function(self, event, ...)
+	if (event == "MERCHANT_SHOW") then
+		local _, _, _, _, _, creatureID = strsplit("-", UnitGUID("target")); creatureID = tonumber(creatureID)
+		for id, indices in pairs(merchants) do
+			if (id == creatureID) then
+				for itemIndex, itemData in pairs(indices) do
+					if ((GetMerchantItemInfo(itemIndex)) == itemData.name) then
+						BuyMerchantItem(itemIndex, itemData.quantity)
+						break
+					end
+				end
+			end
+		end
+	end
+end)
