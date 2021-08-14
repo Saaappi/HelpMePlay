e409146daa74e39b45b8fd9c3cbb62afb8272571
Saaappi <alex.metz1024@gmail.com>
2021-08-14 16:11:08 -0700

    The selection of Threads of Fate should now be entirely automated.

--- Confirmations.lua
+++ Confirmations.lua
@@ -4,11 +4,15 @@ local e = CreateFrame("Frame")
 e:RegisterEvent("PLAYER_CHOICE_UPDATE")
 
 e:SetScript("OnEvent", function(self, event, ...)
-	if (event == "PLAYER_CHOICE_UPDATE") then
-		C_Timer.After(0, function()
-			C_Timer.After(4, function()
-				StaticPopup1EditBox:SetText("FATE")
-			end)
-		end)
+	if event == "PLAYER_CHOICE_UPDATE" then
+		if PlayerChoiceFrame:IsVisible() then
+			local mapId = C_Map.GetBestMapForUnit("player")
+			if mapId == 1670 then
+				-- Player is in Oribos
+				local choiceOptionInfo = C_PlayerChoice.GetPlayerChoiceOptionInfo(1) -- Threads of Fate
+				SendPlayerChoiceResponse(choiceOptionInfo.buttons[1].id)
+				PlayerChoiceFrame:Hide()
+			end
+		end
 	end
 end)
