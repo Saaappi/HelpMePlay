ab2b03a4599ff6bef791437b3d7988f02fe7b031
Saaappi <alex.metz1024@gmail.com>
2021-08-14 14:33:56 -0700

    Started merging SpeakForMe with HelpMeQuest; SpeakForMe will be deprecated.

---
+++ Dialog.lua
@@ -0,0 +1,87 @@
+local addonName, addonTable = ...
+local e = CreateFrame("Frame")
+
+e:RegisterEvent("GOSSIP_CONFIRM")
+e:RegisterEvent("GOSSIP_CONFIRM_CANCEL")
+e:RegisterEvent("GOSSIP_SHOW")
+e:RegisterEvent("MODIFIER_STATE_CHANGED")
+
+local creatures = {
+	[1] = { -- NPCs without Cost
+		["gossips"] = {
+			"Are you enjoying yourself?", -- Quest: Mix, Mingle, and Meddle
+			"Begin pet battle.",
+			"Let's do battle!",
+			"I am ready to go.",
+			"I am ready.",
+			"I'm ready",
+			"There is no time left. Run!",
+			"I will assist you.",
+			"Can you return me to the present time?",
+			"Let us begin.",
+			"Yes, I will help.",
+			"I would like to challenge both of you to a spar.",
+			"Will you spar with me?",
+			"Now tell me, did you see who killed the Furlbrows?",
+			"Is there any way I can help?",
+			"O.K.",
+			"Is there anything you need?",
+			"Very well. Let us fight.",
+			"Au'narim claims you owe her anima.",
+			"Maldraxxus needs your service. Will you fight?",
+			"The Lady of the Falls wanted to make sure you were safe.",
+			"<Ask about suspicious activity>",
+			"<Request tithe>",
+			"<Present Lajos's invitation>",
+			"We are ready",
+			"Take us back to Zuldazar.",
+			"Go hunt somewhere else!",
+			"Let's duel.",
+			"I have heard this story before.",
+			"I've heard this tale before.",
+			"<Show him the Word of Zul.>",
+			"<Show her the Word of Zul.>",
+		}
+	},
+	[2] = { -- NPCs with Cost
+		["gossips"] = {
+			"I'd like to heal and revive my battle pets.",
+		},
+		["cost"] = 10000,
+	},
+	[171791] = { -- Nazgrim, Orgrimmar
+		["gossips"] = {
+			"Tell me what happened."
+		},
+	}
+}
+
+e:SetScript("OnEvent", function(self, event, ...)
+	if event == "GOSSIP_SHOW" then
+		local index = 1
+		local unitGUID = UnitGUID("target") or UnitGUID("mouseover")
+		if unitGUID then
+			local _, _, _, _, _, npcID = strsplit("-", unitGUID); npcID = tonumber(npcID)
+			local gossipOptions = C_GossipInfo.GetOptions()
+			for index, gossipOptionsSubTable in ipairs(gossipOptions) do
+				for id, _ in pairs(creatures) do
+					if id == npcID then -- The target's ID is in the table, so use its configuration.
+						for i = 1, #creatures[id]["gossips"] do
+							if (string.find(gossipOptionsSubTable["name"], creatures[id]["gossips"][i])) then
+								C_GossipInfo.SelectOption(index)
+							end
+						end
+					else
+						for i = 1, 2 do
+							for j = 1, #creatures[i]["gossips"] do
+								if (string.find(gossipOptionsSubTable["name"], creatures[i]["gossips"][j])) then
+									C_GossipInfo.SelectOption(index)
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+end)
