0eaa86b6a203d7371ffefa7ca20d05baa9b670fc
Saaappi <alex.metz1024@gmail.com>
2021-08-16 18:45:45 -0700

    Did some work on the "The Games We Play" quest in Ardenweald. The addon should no longer fail the 'thanks' emote.

--- Speech.lua
+++ Speech.lua
@@ -7,7 +7,7 @@ e:SetScript("OnEvent", function(self, event, ...)
 	if event == "CHAT_MSG_MONSTER_SAY" then
 		local msg, monster = ...
 		if monster == "Playful Trickster" then
-			if string.find(msg, "Not so much as a word of praise! Hmph!") then
+			if string.find(msg, "Not so much as a word of praise!") then
 				DoEmote("praise", nil)
 			elseif string.find(msg, "The other spriggans were all cheering my name for days!") then
 				DoEmote("cheer", nil)
@@ -15,8 +15,8 @@ e:SetScript("OnEvent", function(self, event, ...)
 				DoEmote("dance", nil)
 			elseif string.find(msg, "Introductions are an important part of first impressions!") then
 				DoEmote("bow", nil)
-			elseif string.find(msg, "Some ask permission") then
-				DoEmote("thanks", nil)
+			elseif string.find(msg, "Some ask permission, and always thank us for our trouble.") then
+				DoEmote("thank", nil)
 			elseif string.find(msg, "How strong he must have been!") then
 				DoEmote("flex", nil)
 			end
