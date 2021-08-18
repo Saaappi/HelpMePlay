1967aa4fb9bd2d45e48a51ed2c3967f52071c54d
Saaappi <alex.metz1024@gmail.com>
2021-08-18 12:45:55 -0700

    The addon now automatically puts the player in Legion's timeline for Chromie Time. Updated the change log.

--- ChromieTime.lua
+++ ChromieTime.lua
@@ -1,1 +1,14 @@
 local addonName, addonTable = ...
+local e = CreateFrame("Frame")
+
+e:RegisterEvent("CHROMIE_TIME_OPEN")
+
+e:SetScript("OnEvent", function(self, event, ...)
+	if event == "CHROMIE_TIME_OPEN" then
+		local playerLevel = UnitLevel("player")
+		if playerLevel <= 49 then
+			-- Select Legion for the player.
+			C_ChromieTime.SelectChromieTimeOption(10)
+		end
+	end
+end)
