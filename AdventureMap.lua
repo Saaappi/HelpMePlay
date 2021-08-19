b7a901c630516e39b8703542e7ee611b2f5a8efd
Saaappi <alex.metz1024@gmail.com>
2021-08-19 12:43:53 -0700

    Automated both BfA adventure maps.

--- AdventureMap.lua
+++ AdventureMap.lua
@@ -5,10 +5,17 @@ e:RegisterEvent("ADVENTURE_MAP_OPEN")
 
 e:SetScript("OnEvent", function(self, event, ...)
 	if event == "ADVENTURE_MAP_OPEN" then
-		local mapID = C_AdventureMap.GetMapID()
-		if mapID == 1011 then
-			-- Player is in Zandalar (The Great Seal)
+		local mapID = C_AdventureMap.GetMapID(); print(mapID)
+		if mapID == 1011 then -- Zandalar (The Great Seal)
 			C_AdventureMap.StartQuest(47514) -- Zuldazar
+		elseif mapID == 1014 then -- Kul Tiras (The Banshee's Wail)
+			if C_QuestLog.IsQuestFlaggedCompleted(51802) == false then
+				C_AdventureMap.StartQuest(51802) -- Foothold: Stormsong Valley
+			elseif C_QuestLog.IsQuestFlaggedCompleted(51800) == false then
+				C_AdventureMap.StartQuest(51800) -- Foothold: Tiragarde Sound
+			else
+				C_AdventureMap.StartQuest(51801) -- Foothold: Drustvar
+			end
 		end
 	end
 end)
