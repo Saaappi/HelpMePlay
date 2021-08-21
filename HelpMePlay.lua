1cf3e541498d4fb33d8b65810526dddca7c5fdf5
Saaappi <alex.metz1024@gmail.com>
2021-08-21 11:57:29 -0700

    Localized the slash command.

--- HelpMePlay.lua
+++ HelpMePlay.lua
@@ -104,7 +104,7 @@ local function GetGreetingQuests()
 	end
 end
 
-SLASH_HelpMePlay1 = "/hmp"
+SLASH_HelpMePlay1 = L["Slash HMP"]
 SlashCmdList["HelpMePlay"] = function(command, editbox)
 	local _, _, command, arguments = string.find(command, "%s?(%w+)%s?(.*)") -- Using pattern matching the addon will be able to interpret subcommands.
 	if not command or command == "" then
