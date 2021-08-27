4f557d93a3ca135d51c9d995a14af5f5cd33ba0c
Saaappi <alex.metz1024@gmail.com>
2021-08-27 15:26:54 -0700

    Added a garrison level check. Updated the change log.

--- GarrisonArchitectTable.lua
+++ GarrisonArchitectTable.lua
@@ -6,14 +6,17 @@ e:RegisterEvent("GARRISON_ARCHITECT_OPENED")
 
 e:SetScript("OnEvent", function(self, event, ...)
 	if event == "GARRISON_ARCHITECT_OPENED" then
-		local _, _, _, _, _, isActive = C_Garrison.GetBuildingTooltip(26)
-		if isActive == false then
-			local plots = C_Garrison.GetPlots(1)
-			for i = 1, #plots do
-				if plots[i].size == 3 then
-					-- This is a large plot. Place the Barracks here.
-					C_Garrison.PlaceBuilding(plots[i].id, 26)
-					PlaySound(SOUNDKIT.UI_GARRISON_ARCHITECT_TABLE_UPGRADE_START)
+		local garrisonLevel = C_Garrison.GetGarrisonInfo(2)
+		if garrisonLevel == 1 then
+			local _, _, _, _, _, isActive = C_Garrison.GetBuildingTooltip(26)
+			if isActive == false then
+				local plots = C_Garrison.GetPlots(1)
+				for i = 1, #plots do
+					if plots[i].size == 3 then
+						-- This is a large plot. Place the Barracks here.
+						C_Garrison.PlaceBuilding(plots[i].id, 26)
+						PlaySound(SOUNDKIT.UI_GARRISON_ARCHITECT_TABLE_UPGRADE_START)
+					end
 				end
 			end
 		end
