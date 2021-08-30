b2b12a59dd2b8da9d0417f71bc69bd3b21fc1464
Saaappi <alex.metz1024@gmail.com>
2021-08-30 11:35:58 -0700

    Fixed an issue where Alliance weren't opted in to War Mode reliably. Updated the change log.

--- WarMode.lua
+++ WarMode.lua
@@ -4,13 +4,26 @@ local L = addonTable.L
 
 local playerLevel = 50
 
+e:RegisterEvent("CHAT_MSG_CHANNEL_JOIN")
+e:RegisterEvent("PLAYER_LOGIN")
 e:RegisterEvent("PLAYER_UPDATE_RESTING")
 
 e:SetScript("OnEvent", function(self, event, ...)
-	if event == "PLAYER_UPDATE_RESTING" then
+	if event == "PLAYER_UPDATE_RESTING" or event == "CHAT_MSG_CHANNEL_JOIN" then
 		if C_PvP.CanToggleWarModeInArea() and UnitLevel("player") < playerLevel then
 			-- Player is in Stormwind City or Orgrimmar
 			C_PvP.ToggleWarMode()
 		end
+	end
+	if event == "PLAYER_LOGIN" then
+		C_Timer.After(0, function()
+			C_Timer.After(2.5, function()
+				local mapId = C_Map.GetBestMapForUnit("player")
+				if mapId == 84 or mapId == 85 and UnitLevel("player") < playerLevel then
+					-- Player is in Stormwind City or Orgrimmar
+					C_PvP.ToggleWarMode()
+				end
+			end)
+		end)
 	end
 end)
