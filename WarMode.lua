e1bd27df8ada5dd3b3a64cceb05d4216b69adfb9
Saaappi <alex.metz1024@gmail.com>
2021-08-29 17:51:12 -0700

    Added a max level check for War Mode. Updated the change log.

--- WarMode.lua
+++ WarMode.lua
@@ -2,11 +2,13 @@ local addonName, addonTable = ...
 local e = CreateFrame("Frame")
 local L = addonTable.L
 
+local playerLevel = 50
+
 e:RegisterEvent("PLAYER_UPDATE_RESTING")
 
 e:SetScript("OnEvent", function(self, event, ...)
 	if event == "PLAYER_UPDATE_RESTING" then
-		if C_PvP.CanToggleWarModeInArea() then
+		if C_PvP.CanToggleWarModeInArea() and UnitLevel("player") < playerLevel then
 			-- Player is in Stormwind City or Orgrimmar
 			C_PvP.ToggleWarMode()
 		end
