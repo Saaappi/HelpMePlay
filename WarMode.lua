217372447d181638c01ec741ee5a68677b2ed946
Saaappi <alex.metz1024@gmail.com>
2021-08-30 11:40:59 -0700

    Alliance should no longer automatically opt out of War Mode. Updated the change log.

--- WarMode.lua
+++ WarMode.lua
@@ -10,18 +10,21 @@ e:RegisterEvent("PLAYER_UPDATE_RESTING")
 
 e:SetScript("OnEvent", function(self, event, ...)
 	if event == "PLAYER_UPDATE_RESTING" or event == "CHAT_MSG_CHANNEL_JOIN" then
-		if C_PvP.CanToggleWarModeInArea() and UnitLevel("player") < playerLevel then
-			-- Player is in Stormwind City or Orgrimmar
-			C_PvP.ToggleWarMode()
+		local mapId = C_Map.GetBestMapForUnit("player")
+		if (mapId == 84 or mapId == 85) and UnitLevel("player") < playerLevel then
+			if C_PvP.IsWarModeActive() == false then
+				C_PvP.ToggleWarMode()
+			end
 		end
 	end
 	if event == "PLAYER_LOGIN" then
 		C_Timer.After(0, function()
 			C_Timer.After(2.5, function()
 				local mapId = C_Map.GetBestMapForUnit("player")
-				if mapId == 84 or mapId == 85 and UnitLevel("player") < playerLevel then
-					-- Player is in Stormwind City or Orgrimmar
-					C_PvP.ToggleWarMode()
+				if (mapId == 84 or mapId == 85) and UnitLevel("player") < playerLevel then
+					if C_PvP.IsWarModeActive() == false then
+						C_PvP.ToggleWarMode()
+					end
 				end
 			end)
 		end)
