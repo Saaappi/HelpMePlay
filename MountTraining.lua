a7e91ec47c03c366668bb252db704ea94c6a0efa
Saaappi <alex.metz1024@gmail.com>
2021-09-12 12:40:10 -0700

    Mount Training is now auto learned if the player has +300 gold over the training OR has twice the training cost in money. Updated the change log.

--- MountTraining.lua
+++ MountTraining.lua
@@ -10,7 +10,7 @@ e:SetScript("OnEvent", function(self, event, ...)
 		if numTrainerServices > 0 then
 			for i = 1, numTrainerServices do
 				if GetTrainerServiceSkillLine(i) == L["Riding"] then
-					if GetMoney() > (tonumber(GetTrainerServiceCost(i)+3000000)) then
+					if GetMoney() > (tonumber(GetTrainerServiceCost(i)+3000000)) or GetMoney() > (tonumber(GetTrainerServiceCost(i)*2)) then
 						-- If the player has at least twice the amount of cost for the Riding training, then buy it.
 						BuyTrainerService(i)
 					end
