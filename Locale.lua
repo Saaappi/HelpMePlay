3ac5ac3fa3e9ab509f87ab0a626ac6a05c0f00ca
Saaappi <alex.metz1024@gmail.com>
2021-09-17 00:46:26 -0700

    Removed the second 'specific' in the Merchants tooltip.

--- Locale.lua
+++ Locale.lua
@@ -53,7 +53,7 @@ if (locale == "enGB" or locale == "enUS") then
 	L["Emotes Check Button"]													= "Automates emoting at specific NPCs.\n|cffFFFFFFExample|r: The runestones during the \"Repeat After Me\" quest in Maldraxxus."
 	L["Garrison Tables Check Button"]											= "Automates the start of the first Garrison mission for both factions, as well as\nthe plotting of the Barracks building for Level 1 Garrisons."
 	L["Mail Check Button"]														= "Automates the collection of ONLY gold from your mailbox."
-	L["Merchants Check Button"]													= "Automates the purchase of specific items from specific merchants.\n|cffFFFFFFExample|r: The [Bottle of Airspark] from Lanna Skyspark in Highmountain."
+	L["Merchants Check Button"]													= "Automates the purchase of specific items from merchants.\n|cffFFFFFFExample|r: The [Bottle of Airspark] from Lanna Skyspark in Highmountain."
 	L["Mount Training Check Button"]											= "Automates the purchase of mount training.\nThe player must have either double the cost of the training OR\nat least 300 more gold than the cost of the training."
 	L["Speech Check Button"]													= "Automates interacting with specific NPC speech.\n|cffFFFFFFExample|r: The Playful Trickster during \"The Games We Play\" quest in Ardenweald."
 	L["War Mode Check Button"]													= "Automates opting the player into War Mode when entering Orgrimmar or Stormwind City.\nThis only applies to players above level 20, but below 50."
