14d8e5a183fefc38ade7a7a08cca3f443c2dc3af
Saaappi <alex.metz1024@gmail.com>
2021-09-17 17:44:52 -0700

    Possible fix for the nil error on Mouseover.

--- Mouseover.lua
+++ Mouseover.lua
@@ -12,34 +12,36 @@ local hasBuff = false
 -- This will need to be reconsidered for creatures since
 -- an Id can be used for them opposed to a localized name.
 GameTooltip:HookScript("OnUpdate", function(self)
-	if GameTooltip:GetOwner():GetName() == "UIParent" then
-		mouseoverName = _G["GameTooltipTextLeft"..1]:GetText()
-		for name, data in pairs(addonTable.mouseoverDB) do
-			if name == mouseoverName then
-				for i = 1, self:NumLines() do
-					if string.find(_G[self:GetName().."TextLeft"..i]:GetText(), addonName) then return end
-				end
-				if data.buffId ~= 0 or data.buffId ~= nil then
-					for i = 1, 16 do
-						local _, _, _, _, _, expiration, _, _, _, spellId = UnitAura("player", i)
-						if spellId == data.buffId and expiration > 0 then
-							startTime = GetTime()
-							timeLeft = expiration - startTime
-							hasBuff = true
-							break
-						end
+	if GameTooltip:IsVisible() then
+		if GameTooltip:GetOwner():GetName() == "UIParent" then
+			mouseoverName = _G["GameTooltipTextLeft"..1]:GetText()
+			for name, data in pairs(addonTable.mouseoverDB) do
+				if name == mouseoverName then
+					for i = 1, self:NumLines() do
+						if string.find(_G[self:GetName().."TextLeft"..i]:GetText(), addonName) then return end
 					end
-					if hasBuff then
-						GameTooltip:AddLine("|cffFFFFFF" .. addonName .. "|r: " .. data.note .. " (|cffFFFFFF" .. string.format("%.2f", timeLeft) .. "|r)")
-						GameTooltip:Show()
-						hasBuff = false
+					if data.buffId ~= 0 or data.buffId ~= nil then
+						for i = 1, 16 do
+							local _, _, _, _, _, expiration, _, _, _, spellId = UnitAura("player", i)
+							if spellId == data.buffId and expiration > 0 then
+								startTime = GetTime()
+								timeLeft = expiration - startTime
+								hasBuff = true
+								break
+							end
+						end
+						if hasBuff then
+							GameTooltip:AddLine("|cffFFFFFF" .. addonName .. "|r: " .. data.note .. " (|cffFFFFFF" .. string.format("%.2f", timeLeft) .. "|r)")
+							GameTooltip:Show()
+							hasBuff = false
+						else
+							GameTooltip:AddLine("|cffFFFFFF" .. addonName .. "|r: " .. data.note)
+							GameTooltip:Show()
+						end
 					else
 						GameTooltip:AddLine("|cffFFFFFF" .. addonName .. "|r: " .. data.note)
 						GameTooltip:Show()
 					end
-				else
-					GameTooltip:AddLine("|cffFFFFFF" .. addonName .. "|r: " .. data.note)
-					GameTooltip:Show()
 				end
 			end
 		end
