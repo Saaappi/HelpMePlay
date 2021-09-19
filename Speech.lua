cc707072d97629a417109dcec6371abd726bc326
Saaappi <alex.metz1024@gmail.com>
2021-09-19 03:31:10 -0700

    A test to see if I can function-ize the glow effect on action bar buttons.

--- Speech.lua
+++ Speech.lua
@@ -4,6 +4,13 @@ local L = addonTable.L
 
 e:RegisterEvent("CHAT_MSG_MONSTER_SAY")
 
+local function AddGlowToActionBarButton(buttonId)
+	ActionButton_ShowOverlayGlow(_G["OverrideActionBarButton"]..buttonId)
+	C_Timer.After(3, function()
+		ActionButton_HideOverlayGlow(_G["OverrideActionBarButton"]..buttonId)
+	end)
+end
+
 e:SetScript("OnEvent", function(self, event, ...)
 	if event == "CHAT_MSG_MONSTER_SAY" then
 		if HelpMePlayOptionsDB.Speech == false then return end
@@ -26,38 +33,44 @@ e:SetScript("OnEvent", function(self, event, ...)
 		elseif monster == L["Al'dalil"] then
 			-- Quest: Dead Drop (Bastion)
 			if string.find(string.lower(msg), string.lower(L["Al'dalil: Buttons 1"])) or string.find(string.lower(msg), string.lower(L["Al'dalil: Buttons 2"])) then
-				ActionButton_ShowOverlayGlow(OverrideActionBarButton1)
+				--[[ActionButton_ShowOverlayGlow(OverrideActionBarButton1)
 				C_Timer.After(3, function()
 					ActionButton_HideOverlayGlow(OverrideActionBarButton1)
-				end)
+				end)]]
+				AddGlowToActionBarButton(1)
 			elseif string.find(string.lower(msg), string.lower(L["Al'dalil: Lever 1"])) or string.find(string.lower(msg), string.lower(L["Al'dalil: Lever 2"])) then
-				ActionButton_ShowOverlayGlow(OverrideActionBarButton2)
+				--[[ActionButton_ShowOverlayGlow(OverrideActionBarButton2)
 				C_Timer.After(3, function()
 					ActionButton_HideOverlayGlow(OverrideActionBarButton2)
-				end)
+				end)]]
+				AddGlowToActionBarButton(2)
 			elseif string.find(string.lower(msg), string.lower(L["Al'dalil: Bonk 1"])) or string.find(string.lower(msg), string.lower(L["Al'dalil: Bonk 2"])) then
-				ActionButton_ShowOverlayGlow(OverrideActionBarButton3)
+				--[[ActionButton_ShowOverlayGlow(OverrideActionBarButton3)
 				C_Timer.After(3, function()
 					ActionButton_HideOverlayGlow(OverrideActionBarButton3)
-				end)
+				end)]]
+				AddGlowToActionBarButton(3)
 			end
 		elseif monster == L["Trainer Ikaros"] then
 			-- World Quest: We'll Make an Aspirant Out of You (Bastion)
 			if string.find(string.lower(msg), string.lower(L["Trainer Ikaros: Jab"])) or string.find(string.lower(msg), string.lower(L["Trainer Ikaros: Strike"])) then
-				ActionButton_ShowOverlayGlow(OverrideActionBarButton1)
+				--[[ActionButton_ShowOverlayGlow(OverrideActionBarButton1)
 				C_Timer.After(3, function()
 					ActionButton_HideOverlayGlow(OverrideActionBarButton1)
-				end)
+				end)]]
+				AddGlowToActionBarButton(1)
 			elseif string.find(string.lower(msg), string.lower(L["Trainer Ikaros: Kick"])) or string.find(string.lower(msg), string.lower(L["Trainer Ikaros: Sweep"])) then
-				ActionButton_ShowOverlayGlow(OverrideActionBarButton2)
+				--[[ActionButton_ShowOverlayGlow(OverrideActionBarButton2)
 				C_Timer.After(3, function()
 					ActionButton_HideOverlayGlow(OverrideActionBarButton2)
-				end)
+				end)]]
+				AddGlowToActionBarButton(2)
 			elseif string.find(string.lower(msg), string.lower(L["Trainer Ikaros: Dodge"])) or string.find(string.lower(msg), string.lower(L["Trainer Ikaros: Parry"])) then
-				ActionButton_ShowOverlayGlow(OverrideActionBarButton3)
+				--[[ActionButton_ShowOverlayGlow(OverrideActionBarButton3)
 				C_Timer.After(3, function()
 					ActionButton_HideOverlayGlow(OverrideActionBarButton3)
-				end)
+				end)]]
+				AddGlowToActionBarButton(3)
 			end
 		end
 	end
