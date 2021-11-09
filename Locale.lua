1c62ec76ca904fdc06475171a3ba3da07c0f3414
Saaappi <alex.metz1024@gmail.com>
2021-11-09 13:38:18 -0800

    Added a 'Talents' file and setup the infrastructure for suggesting talents to select while leveling.

--- Locale.lua
+++ Locale.lua
@@ -41,6 +41,7 @@ if (locale == "enGB" or locale == "enUS") then
 	L["No"]																		= "No"
 	L["Box Of Many Things Purchase Text"]										= "Purchase the following talent: "
 	L["Box Of Many Things Not Enough Text"]										= "You need more |cff0070ddTower Knowledge|r! Amount Needed: "
+	L["Level Congrats Text"]													= "Congratulations on reaching level %c!"
 	-- Symbols
 	L["Red X Raid Target"]														= "|TInterface\\TARGETINGFRAME\\UI-RaidTargetingIcon_7:0|t"
 	-- Options Panel Text
