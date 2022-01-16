9c89a6a6dbe18e6a7a1a4f3366fa27b8823c2b7f
Saaappi <alex.metz1024@gmail.com>
2022-01-15 18:01:44 -0800

    Added support for the Titan Research Archive.

--- GarrisonTalentNPC.lua
+++ GarrisonTalentNPC.lua
@@ -1,6 +1,29 @@
 local addonName, addonTable = ...
 local e = CreateFrame("Frame")
 local L = addonTable.L
+local titanResearchArchiveTalents = {
+	-- Corrupted Memementos: 1744
+	791, -- Orb Operation Manual (Rank 1) (100 CM)
+	798, -- Expansive Mind (Rank 1) (150 CM)
+	798, -- Expansive Mind (Rank 2) (250 CM)
+	798, -- Expansive Mind (Rank 3) (400 CM)
+	802, -- Clear Sight (Rank 1) (750 CM)
+	802, -- Clear Sight (Rank 2) (900 CM)
+	802, -- Clear Sight (Rank 3) (1100 CM)
+	805, -- Elite Extermination (Rank 1) (1350 CM)
+	806, -- Vision Hunter (Rank 1) (1800 CM)
+	806, -- Vision Hunter (Rank 2) (2200 CM)
+	806, -- Vision Hunter (Rank 3) (2700 CM)
+	792, -- Gift of the Titans (Rank 1) (3300 CM)
+	794, -- Singular Sanitation Expertise (Rank 1) (4200 CM)
+	794, -- Singular Sanitation Expertise (Rank 2) (4800 CM)
+	794, -- Singular Sanitation Expertise (Rank 3) (5500 CM)
+	795, -- Emergency Cranial Defibrillation (Rank 1) (6500 CM)
+	804, -- Experimental Destabilization (Rank 1) (6500 CM)
+	804, -- Experimental Destabilization (Rank 2) (6500 CM)
+	804, -- Experimental Destabilization (Rank 3) (6500 CM)
+	800, -- Synchronized Mind Stabilizer (Rank 1) (6500 CM)
+}
 local boxOfManyThingsTalents = {
 	-- Tower Knowledge: 1904
 	1783, -- Blessing of the Ancients (Rank 1) (20 TK)
@@ -45,36 +68,49 @@ local boxOfManyThingsTalents = {
 	1793, -- Undeterred (Rank 5) (150 TK)
 }
 
-local function GetTalentTreeInfo(talentTreeId)
-	if talentTreeId == 461 then
-		-- The Box of Many Things
-		local towerKnowledge = 0
-		local talentInfo = ""
-		for k, v in ipairs(boxOfManyThingsTalents) do
-			talentInfo = C_Garrison.GetTalentInfo(v)
-			if talentInfo.researched == false then
-				-- The player doesn't have the talent
-				-- researched, so let's move forward.
-				towerKnowledge = C_CurrencyInfo.GetCurrencyInfo(1904).quantity
-				if towerKnowledge > talentInfo["researchCurrencyCosts"][1].currencyQuantity then
-					-- The player has enough Tower Knowledge
-					-- to purchase the talent.
+local function PrintLine(text)
+	print(L["Colored Addon Name"] .. ": " .. text)
+end
+
+local function CheckTalents(talentTree, currencyId)
+	local currency = 0
+	local talentInfo = ""
+	for k, v in ipairs(talentTree) do
+		talentInfo = C_Garrison.GetTalentInfo(v)
+		if talentInfo.researched == false then
+			-- The player doesn't have the talent
+			-- researched, so let's move forward.
+			currency = C_CurrencyInfo.GetCurrencyInfo(currencyId).quantity
+			if currency > talentInfo["researchCurrencyCosts"][1].currencyQuantity then
+				-- The player has enough currency
+				-- to purchase the talent.
+				if talentInfo.perkSpellID ~= 0 then
 					if talentInfo.perkSpellID ~= 0 then
-						print(L["Colored Addon Name"] .. ": " .. L["Box Of Many Things Purchase Text"] .. "\n" .. GetSpellLink(talentInfo.perkSpellID))
+						PrintLine(L["Talent Purchase Text"] .. "\n" .. GetSpellLink(talentInfo.perkSpellID))
 					else
-						print(L["Colored Addon Name"] .. ": " .. L["Box Of Many Things Purchase Text"] .. talentInfo.name)
+						PrintLine(L["Talent Purchase Text"] .. talentInfo.name)
 					end
 					break
-				else
-					-- Print to the chat frame how much
-					-- more Tower Knowledge the player
-					-- will need for the next talent,
-					-- then break from the loop.
-					print(L["Colored Addon Name"] .. ": " .. L["Box Of Many Things Not Enough Text"] .. talentInfo["researchCurrencyCosts"][1].currencyQuantity-towerKnowledge .. " " .. "|T4037125:0|t (" .. talentInfo.name .. ")")
-					break
 				end
+			else
+				-- Print to the chat frame how much
+				-- currency the player
+				-- will need for the next talent,
+				-- then break from the loop.
+				PrintLine(L["Talent Not Enough Currency Text"] .. talentInfo["researchCurrencyCosts"][1].currencyQuantity-currency .. " " .. "|T" .. talentInfo.icon .. ":0|t (" .. talentInfo.name .. ")")
+				break
 			end
 		end
+	end
+end
+
+local function GetTalentTreeInfo(talentTreeId)
+	if talentTreeId == 271 then
+		-- Titan Research Archive
+		CheckTalents(titanResearchArchiveTalents, 1744)
+	elseif talentTreeId == 461 then
+		-- The Box of Many Things
+		CheckTalents(boxOfManyThingsTalents, 1904)
 	elseif talentTreeId == 474 then
 		-- Cypher Research Console
 	end
