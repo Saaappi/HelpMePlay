2d36c9255858497e978cbf4ec52cb6d4e4a26a21
Saaappi <alex.metz1024@gmail.com>
2022-02-28 01:40:30 -0800

    Factored in support for prerequisite talents.

--- Systems/GarrisonTalentNPC.lua
+++ Systems/GarrisonTalentNPC.lua
@@ -90,18 +90,39 @@ local function CheckTalents(talentTree, currencyId)
 			-- The player doesn't have the talent
 			-- researched, so let's move forward.
 			currency = C_CurrencyInfo.GetCurrencyInfo(currencyId).quantity
+			-- The player has enough currency to
+			-- research the talent.
 			if currency >= talentInfo["researchCurrencyCosts"][1].currencyQuantity then
-				-- The player has enough currency
-				-- to purchase the talent.
-				--
-				-- The perk has a spell ID, so let's
-				-- send a spell link to the chat frame.
-				-- If not, then use the perk name.
-				if talentInfo.perkSpellID ~= 0 then
-					PrintLine(L["Talent Purchase Text"] .. "\n" .. GetSpellLink(talentInfo.perkSpellID))
-				else
-					PrintLine(L["Talent Purchase Text"] .. "|T" .. talentInfo.icon .. ":0|t " .. talentInfo.name)
+				-- Let's check to see if the talent
+				-- has a prerequisite talent. If so,
+				-- let's also check if that prerequisite
+				-- has been researched.
+				if talentInfo.prerequisiteTalentID ~= nil then
+					local prerequisiteTalentId = C_Garrison.GetTalentInfo(talentInfo.prerequisiteTalentID)
+					-- The player has researched the prerequisite
+					-- talent, so let's move forward.
+					if prerequisiteTalentId.researched == true then
+						-- The perk has a spell ID, so let's
+						-- send a spell link to the chat frame.
+						-- If not, then use the perk name.
+						if talentInfo.perkSpellID ~= 0 then
+							PrintLine(L["Talent Purchase Text"] .. "\n" .. GetSpellLink(talentInfo.perkSpellID))
+						else
+							PrintLine(L["Talent Purchase Text"] .. "|T" .. talentInfo.icon .. ":0|t |cff|cffEFC503" .. talentInfo.name .. "|r")
+						end
+					else
+						-- Let's do some quick math to determine
+						-- the hours, minutes, and seconds left
+						-- on the research without requiring the
+						-- player to hover over the talent.
+						local hours, minutes, seconds
+						hours, minutes = string.split(".", tostring(prerequisiteTalentId.timeRemaining / 3600))
+						minutes, seconds = string.split(".", tonumber(("0."..minutes) * 60))
+						seconds = string.split(".", tonumber(("0."..seconds) * 60))
+						PrintLine(L["Prequisite Talent Being Researched"] .. " |T" .. prerequisiteTalentId.icon .. ":0|t |cffEFC503" .. prerequisiteTalentId.name .. "|r: " .. hours .. ":" .. minutes .. ":" .. seconds)
+					end
 				end
+				
 				return
 			else
 				-- Print to the chat frame how much
