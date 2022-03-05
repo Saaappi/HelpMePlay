d4c4535ebbb34363e2b79a3c12d70ea69beb7bfb
Saaappi <alex.metz1024@gmail.com>
2022-03-05 02:51:23 -0800

    Added the ability to emote at specified targets rather than only gossip.

--- Automations/Emotes.lua
+++ Automations/Emotes.lua
@@ -3,13 +3,15 @@ local e = CreateFrame("Frame")
 local L = addonTable.L
 
 e:RegisterEvent("GOSSIP_SHOW")
+e:RegisterEvent("PLAYER_TARGET_CHANGED")
 
 local creatures = {
-	[162553] = "flex", -- Runestone of Constructs
-	[162559] = "kneel", -- Runestone of Rituals
-	[162562] = "bleed", -- Runestone of Plague
-	[162584] = "sneak", -- Runestone of Eyes
-	[162592] = "salute", -- Runestone of Chosen
+	[41539]		= "whistle", 	-- Stolen Ram
+	[162553] 	= "flex", 		-- Runestone of Constructs
+	[162559] 	= "kneel", 		-- Runestone of Rituals
+	[162562] 	= "bleed", 		-- Runestone of Plague
+	[162584] 	= "sneak", 		-- Runestone of Eyes
+	[162592] 	= "salute", 	-- Runestone of Chosen
 }
 
 e:SetScript("OnEvent", function(self, event, ...)
@@ -17,11 +19,23 @@ e:SetScript("OnEvent", function(self, event, ...)
 		if HelpMePlayOptionsDB.Emotes == false or HelpMePlayOptionsDB.Emotes == nil then return end
 		local guid = UnitGUID("target")
 		if guid then
-			local _, _, _, _, _, npcID = strsplit("-", guid); npcID = tonumber(npcID)
+			local _, _, _, _, _, npcId = strsplit("-", guid); npcId = tonumber(npcId)
 			for id, emote in pairs(creatures) do
-				if id == npcID then
+				if id == npcId then
 					DoEmote(emote, nil)
 					GossipFrame:Hide()
+				end
+			end
+		end
+	end
+	if event == "PLAYER_TARGET_CHANGED" then
+		if HelpMePlayOptionsDB.Emotes == false or HelpMePlayOptionsDB.Emotes == nil then return end
+		local guid = UnitGUID("target")
+		if guid then
+			local _, _, _, _, _, npcId = strsplit("-", guid); npcId = tonumber(npcId)
+			for id, emote in pairs(creatures) do
+				if id == npcId then
+					DoEmote(emote, nil)
 				end
 			end
 		end
