3ffe1b763e24fa4ef047bf8c74436c91129964b4
Saaappi <alex.metz1024@gmail.com>
2022-03-08 01:09:08 -0800

    Slight change to notes.

--- Systems/Notes.lua
+++ Systems/Notes.lua
@@ -27,7 +27,7 @@ e:SetScript("OnEvent", function(self, event, ...)
 						for i=1,GameTooltip:NumLines() do
 							if string.find(_G[GameTooltip:GetName().."TextLeft"..i]:GetText(), addonName) then return end
 						end
-						GameTooltip:AddLine("|cffFFFFFF" .. addonName .. "|r: " .. v)
+						GameTooltip:AddLine(L_GLOBALSTRINGS["Colored Addon Name"] .. ": " .. v)
 						GameTooltip:Show()
 					end
 				end
