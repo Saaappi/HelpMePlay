496d5c4e9aad76c98b041e8c9421e32fa01425c4
Saaappi <alex.metz1024@gmail.com>
2022-03-08 13:38:41 -0800

    Added another daily and more display ID code.

--- Data/WaypointsDB.lua
+++ Data/WaypointsDB.lua
@@ -23,21 +23,49 @@ local waypointDB = {
 		},
 	},
 	[65363] = { -- Dangerous State (Daily)
-		{
-			1970,						-- Zereth Mortis
-			44.4, 						-- X
-			42.2, 						-- Y
-			"Leashkeeper Helzid",		-- Title
-			nil,						-- Icon
-			184426,						-- DisplayID
+		{														
+			1970,												-- Zereth Mortis
+			44.4, 												-- X
+			42.2, 												-- Y
+			"Leashkeeper Helzid",								-- Title
+			nil,												-- Icon
+			100683,												-- DisplayID
 		},
 		{
-			1970,						-- Zereth Mortis
-			47.2, 						-- X
-			40.2, 						-- Y
-			"Dominated Jiro",			-- Title
-			nil,						-- Icon
-			184425,						-- DisplayID
+			1970,												-- Zereth Mortis
+			47.2, 												-- X
+			40.2, 												-- Y
+			"Dominated Jiro (scattered in the area)",			-- Title
+			nil,												-- Icon
+			98978,												-- DisplayID
+		},
+	},
+	[64977] = { -- Step into the Ring (Daily)
+		{														
+			1970,												-- Zereth Mortis
+			58.0, 												-- X
+			44.4, 												-- Y
+			"Nexus of Actualization",							-- Title
+			nil,												-- Icon
+			nil,												-- DisplayID
+		},
+	},
+	[64785] = { -- Overgrown Story (Daily)
+		{														
+			1970,												-- Zereth Mortis
+			60.6, 												-- X
+			64.6, 												-- Y
+			"Gaiagantic",										-- Title
+			nil,												-- Icon
+			102117,												-- DisplayID
+		},
+		{
+			1970,												-- Zereth Mortis
+			61.6, 												-- X
+			68.6, 												-- Y
+			"Overgrown Geomental (scattered in the area)",		-- Title
+			nil,												-- Icon
+			100374,												-- DisplayID
 		},
 	},
 }
