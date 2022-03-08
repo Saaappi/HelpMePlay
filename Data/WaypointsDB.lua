5a93a0fd9f94918362130a49dff63c4ec482aeeb
Saaappi <alex.metz1024@gmail.com>
2022-03-08 14:26:52 -0800

    Added more waypoints!

--- Data/WaypointsDB.lua
+++ Data/WaypointsDB.lua
@@ -162,6 +162,110 @@ local waypointDB = {
 			102043,												-- DisplayID
 		},
 	},
+	[64854] = { -- Trappings of Success (Daily)
+		{														
+			1970,												-- Zereth Mortis
+			39.6, 												-- X
+			52.0, 												-- Y
+			"Runefur",											-- Title
+			nil,												-- Icon
+			102146,												-- DisplayID
+		},
+		{														
+			1970,												-- Zereth Mortis
+			41.6, 												-- X
+			62.4, 												-- Y
+			"Bitterbeak",										-- Title
+			nil,												-- Icon
+			98833,												-- DisplayID
+		},
+		{														
+			1970,												-- Zereth Mortis
+			38.8, 												-- X
+			58.6, 												-- Y
+			"Cipherclad",										-- Title
+			nil,												-- Icon
+			102145,												-- DisplayID
+		},
+		{														
+			1970,												-- Zereth Mortis
+			37.4, 												-- X
+			57.6, 												-- Y
+			"Mawsworn Cryptoseer (scattered around)",			-- Title
+			nil,												-- Icon
+			92781,												-- DisplayID
+		},
+	},
+	[65177] = { -- Fruit of the Bloom (Daily)
+		{														
+			1970,												-- Zereth Mortis
+			47.4, 												-- X
+			82.4, 												-- Y
+			"Fruit of the Bloom",								-- Title
+			nil,												-- Icon
+			16915,												-- DisplayID
+		},
+	},
+	[65326] = { -- Circle of Strife (Daily)
+		{														
+			1970,												-- Zereth Mortis
+			44.0, 												-- X
+			52.2, 												-- Y
+			"Automa Constructor (scattered around)",			-- Title
+			nil,												-- Icon
+			104781,												-- DisplayID
+		},
+	},
+	[64592] = { -- Historic Protection (Daily)
+		{														
+			1970,												-- Zereth Mortis
+			59.6, 												-- X
+			29.6, 												-- Y
+			"Terraforming Record",								-- Title
+			nil,												-- Icon
+			nil,												-- DisplayID
+		},
+		{														
+			1970,												-- Zereth Mortis
+			59.9, 												-- X
+			27.4, 												-- Y
+			"Terraforming Record",								-- Title
+			nil,												-- Icon
+			nil,												-- DisplayID
+		},
+		{														
+			1970,												-- Zereth Mortis
+			61.2, 												-- X
+			28.2, 												-- Y
+			"Terraforming Record",								-- Title
+			nil,												-- Icon
+			nil,												-- DisplayID
+		},
+		{														
+			1970,												-- Zereth Mortis
+			61.2, 												-- X
+			30.9, 												-- Y
+			"Terraforming Record",								-- Title
+			nil,												-- Icon
+			nil,												-- DisplayID
+		},
+		{														
+			1970,												-- Zereth Mortis
+			63.4, 												-- X
+			26.3, 												-- Y
+			"Terraforming Record",								-- Title
+			nil,												-- Icon
+			nil,												-- DisplayID
+		},
+		{														
+			1970,												-- Zereth Mortis
+			66.0, 												-- X
+			26.6, 												-- Y
+			"Terraforming Record",								-- Title
+			nil,												-- Icon
+			nil,												-- DisplayID
+		},
+	},
 }
 
 addonTable.waypointDB = waypointDB
