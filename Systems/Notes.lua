865c3ee8f7a6b6a9d2e6f840e7ca858cd77d5545
Saaappi <alex.metz1024@gmail.com>
2022-03-08 01:08:00 -0800

    Made the Notes system more efficient.

--- Systems/Notes.lua
+++ Systems/Notes.lua
@@ -5,34 +5,13 @@ local L_DIALOG = addonTable.L_DIALOG
 local L_NOTES = addonTable.L_NOTES
 local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
 local creatureDb = {
-	[170080] = { -- Exposed Boggart
-		["t"] = "note",
-		["n"] = L_NOTES["Note: Exposed Boggart"],
-	},
-	[159915] = { -- Gatamatos
-		["t"] = "note",
-		["n"] = L_NOTES["Note: CLICK ME!"],
-	},
-	[173806] = { -- Gatamatos
-		["t"] = "note",
-		["n"] = L_NOTES["Note: CLICK ME!"],
-	},
-	[161726] = { -- Kael'thas Sunstrider (Sin #1)
-		["t"] = "note",
-		["n"] = L_NOTES["Note: CLICK ME!"],
-	},
-	[161734] = { -- Kael'thas Sunstrider (Sin #2)
-		["t"] = "note",
-		["n"] = L_NOTES["Note: CLICK ME!"],
-	},
-	[161737] = { -- Kael'thas Sunstrider (Sin #3)
-		["t"] = "note",
-		["n"] = L_NOTES["Note: CLICK ME!"],
-	},
-	[164675] = { -- Lever (Torghast Chest)
-		["t"] = "note",
-		["n"] = L_NOTES["Note: Torghast Lever"],
-	},
+	[170080] = L_NOTES["Note: CLICK ME!"]		-- Exposed Boggart
+	[159915] = L_NOTES["Note: CLICK ME!"],		-- Gatamatos
+	[173806] = L_NOTES["Note: CLICK ME!"],		-- Gatamatos
+	[161726] = L_NOTES["Note: CLICK ME!"],		-- Kael'thas Sunstrider (Sin #1)
+	[161734] = L_NOTES["Note: CLICK ME!"],		-- Kael'thas Sunstrider (Sin #2)
+	[161737] = L_NOTES["Note: CLICK ME!"],		-- Kael'thas Sunstrider (Sin #3)
+	[164675] = L_NOTES["Note: Torghast Lever"],	-- Lever (Torghast Chest)
 }
 
 e:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
@@ -43,16 +22,13 @@ e:SetScript("OnEvent", function(self, event, ...)
 			local guid = UnitGUID("mouseover")
 			if guid then
 				local _, _, _, _, _, npcId = strsplit("-", guid); npcId = tonumber(npcId)
-				for nameOrId, data in pairs(creatureDb) do
-					if nameOrId == npcId then
-						local tooltipString = ""
+				for k,v in pairs(creatureDb) do
+					if k == npcId then
 						for i=1,GameTooltip:NumLines() do
 							if string.find(_G[GameTooltip:GetName().."TextLeft"..i]:GetText(), addonName) then return end
 						end
-						if data.t == "note" then
-							GameTooltip:AddLine("|cffFFFFFF" .. addonName .. "|r: " .. data.n)
-							GameTooltip:Show()
-						end
+						GameTooltip:AddLine("|cffFFFFFF" .. addonName .. "|r: " .. v)
+						GameTooltip:Show()
 					end
 				end
 			end
