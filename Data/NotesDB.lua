78ebbf0cd42fbaeb49dd4d2862e1a67f6f1fce28
Saaappi <alex.metz1024@gmail.com>
2022-03-08 20:25:41 -0800

    Migrated Notes to the Data folder.

---
+++ Data/NotesDB.lua
@@ -0,0 +1,16 @@
+local addonName, addonTable = ...
+local e = CreateFrame("Frame")
+local L_DIALOG = addonTable.L_DIALOG
+local L_NOTES = addonTable.L_NOTES
+local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
+local NOTES = {
+	[170080] = L_NOTES["Note: CLICK ME!"],		-- Exposed Boggart
+	[159915] = L_NOTES["Note: CLICK ME!"],		-- Gatamatos
+	[173806] = L_NOTES["Note: CLICK ME!"],		-- Gatamatos
+	[161726] = L_NOTES["Note: CLICK ME!"],		-- Kael'thas Sunstrider (Sin #1)
+	[161734] = L_NOTES["Note: CLICK ME!"],		-- Kael'thas Sunstrider (Sin #2)
+	[161737] = L_NOTES["Note: CLICK ME!"],		-- Kael'thas Sunstrider (Sin #3)
+	[164675] = L_NOTES["Note: Torghast Lever"],	-- Lever (Torghast Chest)
+}
+
+addonTable.NOTES = NOTES
--- Systems/Notes.lua
+++ Systems/Notes.lua
@@ -4,15 +4,6 @@ local L = addonTable.L
 local L_DIALOG = addonTable.L_DIALOG
 local L_NOTES = addonTable.L_NOTES
 local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
-local creatureDb = {
-	[170080] = L_NOTES["Note: CLICK ME!"],		-- Exposed Boggart
-	[159915] = L_NOTES["Note: CLICK ME!"],		-- Gatamatos
-	[173806] = L_NOTES["Note: CLICK ME!"],		-- Gatamatos
-	[161726] = L_NOTES["Note: CLICK ME!"],		-- Kael'thas Sunstrider (Sin #1)
-	[161734] = L_NOTES["Note: CLICK ME!"],		-- Kael'thas Sunstrider (Sin #2)
-	[161737] = L_NOTES["Note: CLICK ME!"],		-- Kael'thas Sunstrider (Sin #3)
-	[164675] = L_NOTES["Note: Torghast Lever"],	-- Lever (Torghast Chest)
-}
 
 e:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
 e:SetScript("OnEvent", function(self, event, ...)
@@ -22,7 +13,7 @@ e:SetScript("OnEvent", function(self, event, ...)
 			local guid = UnitGUID("mouseover")
 			if guid then
 				local _, _, _, _, _, npcId = strsplit("-", guid); npcId = tonumber(npcId)
-				for k,v in pairs(creatureDb) do
+				for k,v in pairs(addonTable.NOTES) do
 					if k == npcId then
 						for i=1,GameTooltip:NumLines() do
 							if string.find(_G[GameTooltip:GetName().."TextLeft"..i]:GetText(), addonName) then return end
