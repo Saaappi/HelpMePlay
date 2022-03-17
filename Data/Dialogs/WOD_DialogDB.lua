04c75891ac03de0a3219df16d3cb507d91a13e0b
Saaappi <alex.metz1024@gmail.com>
2022-03-17 16:56:43 -0700

    Updated more dialogs to the new format.

--- Data/Dialogs/WOD_DialogDB.lua
+++ Data/Dialogs/WOD_DialogDB.lua
@@ -8,126 +8,96 @@ local DIALOG_WOD = {
 	-- Garrison (Alliance)
 	[79243] = { -- Baros Alexston
 		["g"] = {
-			L_DIALOG["Baros Alexston 1"],
+			{
+				["text"] 		= L_DIALOG["Baros Alexston 1"],
+				["condition"] 	= "none",
+			},
 		},
 	},
 	[84455] = { -- Assistant Brightstone
 		["g"] = {
-			L_DIALOG["Assistant Brightstone 1"],
+			{
+				["text"] 		= L_DIALOG["Assistant Brightstone 1"],
+				["condition"] 	= "none",
+			},
 		},
 	},
 	[81441] = { -- Shelly Hamby
 		["g"] = {
-			L_DIALOG["Shelly Hamby 1"],
+			{
+				["text"] 		= L_DIALOG["Shelly Hamby 1"],
+				["condition"] 	= "none",
+			},
 		},
 	},
 	[81103] = { -- Dungar Longdrink
 		["g"] = {
-			L_DIALOG["Dungar Longdrink 1"],
+			{
+				["text"] 		= L_DIALOG["Dungar Longdrink 1"],
+				["condition"] 	= "none",
+			},
 		},
 	},
 	-- End of Garrison (Alliance)
 	-- Garrison (Horde)
 	[80225] = { -- Skaggit
 		["g"] = {
-			L_DIALOG["Skaggit 1"],
+			{
+				["text"] 		= L_DIALOG["Skaggit 1"],
+				["condition"] 	= "none",
+			},
 		},
 	},
 	[86775] = { -- Senior Peon II
 		["g"] = {
-			L_DIALOG["Senior Peon II 1"],
+			{
+				["text"] 		= L_DIALOG["Senior Peon II 1"],
+				["condition"] 	= "none",
+			},
 		},
 	},
 	[88228] = { -- Sergeant Grimjaw
 		["g"] = {
-			L_DIALOG["Sergeant Grimjaw 1"],
+			{
+				["text"] 		= L_DIALOG["Sergeant Grimjaw 1"],
+				["condition"] 	= "none",
+			},
 		},
 	},
 	-- End of Garrison (Horde)
-	-- Frostfire Ridge
-	[87122] = { -- Gargra
-		["g"] = {
-			L_DIALOG["Let's do this!"],
-		},
-	},
-	-- End of Frostfire Ridge
-	-- Shadowmoon Valley
-	[84372] = { -- Madari
-		["g"] = {
-			L_DIALOG["Madari 1"],
-		},
-	},
-	[72871] = { -- All-Seeing Eye
-		["g"] = {
-			L_DIALOG["All-Seeing Eye 1"],
-		},
-	},
-	[78556] = { -- Ariok
-		["g"] = {
-			L_DIALOG["Ariok 1"],
-		},
-	},
-	[87124] = { -- Ashlei
-		["g"] = {
-			L_DIALOG["Let's do this!"],
-		},
-	},
-	-- End of Shadowmoon Valley
 	-- Gorgrond
 	[81013] = { -- Rangari Rajess
 		["g"] = {
-			L_DIALOG["D'kaan is coming with help."],
+			{
+				["text"] 		= L_DIALOG["D'kaan is coming with help."],
+				["condition"] 	= "none",
+			},
 		},
 	},
 	[81018] = { -- Rangari Kolaan
 		["g"] = {
-			L_DIALOG["D'kaan is coming with help."],
+			{
+				["text"] 		= L_DIALOG["D'kaan is coming with help."],
+				["condition"] 	= "none",
+			},
 		},
 	},
 	[81020] = { -- Rangari Jonaa
 		["g"] = {
-			L_DIALOG["D'kaan is coming with help."],
+			{
+				["text"] 		= L_DIALOG["D'kaan is coming with help."],
+				["condition"] 	= "none",
+			},
 		},
 	},
 	-- End of Gorgrond
-	-- Talador
-	[87125] = { -- Taralune
-		["g"] = {
-			L_DIALOG["Let's do this!"],
-		},
-	},
-	-- End of Talador
-	-- Spires of Arak
-	[82553] = { -- Villi Gobsplat
-		["g"] = {
-			L_DIALOG["I'd like to heal and revive my battle pets."],
-		},
-		["c"] = {
-			L_DIALOG["A small fee for supplies is required."],
-		},
-	},
-	-- End of Spires of Arak
-	-- Nagrand
-	[87110] = { -- Tarr the Terrible
-		["g"] = {
-			L_DIALOG["Let's do this!"],
-		},
-	},
-	-- End of Nagrand
-	-- Stormshield
-	[85963] = { -- Orville Manfred
-		["g"] = {
-			L_DIALOG["I'd like to heal and revive my battle pets."],
-		},
-		["c"] = {
-			L_DIALOG["A small fee for supplies is required."],
-		},
-	},
-	-- End of Stormshield
 	-- Tanaan Jungle
 	[78568] = { -- Thaelin Darkanvil
 		["g"] = {
-			L_DIALOG["Thaelin Darkanvil 1"],
+			{
+				["text"] 		= L_DIALOG["Thaelin Darkanvil 1"],
+				["condition"] 	= "none",
+			},
 		},
 	},
 	-- End of Tanaan Jungle
--- Data/Dialogs/WOTLK_DialogDB.lua
+++ Data/Dialogs/WOTLK_DialogDB.lua
@@ -5,38 +5,11 @@ local L_NOTES = addonTable.L_NOTES
 local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
 local DIALOG_WOTLK = {
 	-- Supported Maps: 113 (Northrend)
-	-- Borean Tundra
-	[26044] = { -- Durkot Wolfbrother
-		["g"] = {
-			L_DIALOG["I'd like to heal and revive my battle pets."],
-		},
-		["c"] = {
-			L_DIALOG["A small fee for supplies is required."],
-		},
-	},
-	[27194] = { -- Trapper Saghani
-		["g"] = {
-			L_DIALOG["I'd like to heal and revive my battle pets."],
-		},
-		["c"] = {
-			L_DIALOG["A small fee for supplies is required."],
-		},
-	},
-	-- End of Borean Tundra
-	-- Zul'Drak
-	[28790] = { -- Fala Softhoof
-		["g"] = {
-			L_DIALOG["I'd like to heal and revive my battle pets."],
-		},
-		["c"] = {
-			L_DIALOG["A small fee for supplies is required."],
-		},
-	},
-	-- End of Zul'Drak
 	-- Grizzly Hills
 	[33224] = { -- Maiden of Ashwood Lake
 		["g"] = {
-			L_DIALOG["Maiden of Ashwood Lake 1"],
+			["text"] 		= L_DIALOG["Maiden of Ashwood Lake 1"],
+			["condition"] 	= "none",
 		},
 	},
 	-- End of Grizzly Hills
