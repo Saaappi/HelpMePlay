809282f5a5d21eb224bc584db55d9d67a90eec90
Saaappi <alex.metz1024@gmail.com>
2022-04-17 22:59:31 -0700

    Color coded the percentage for the Weights tech.

--- Systems/Weights.lua
+++ Systems/Weights.lua
@@ -4,31 +4,29 @@ local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
 
 local function AddWeightToTooltip(self)
 	if HelpMePlayOptionsDB.Weights == false or HelpMePlayOptionsDB.Weights == nil then return end
-	C_Timer.After(0.1, function()
-		if not self or not self.NumLines or self:NumLines() == 0 then return end
 
-		local name, unit = self:GetUnit()
-		if not unit then return end
+	if not self or not self.NumLines or self:NumLines() == 0 then return end
 
-		local GUID = UnitGUID(unit)
-		if not GUID or GUID == "" then return end
+	local name, unit = self:GetUnit()
+	if not unit then return end
 
-		local npcID = select(6, ("-"):split(GUID))
-		if not npcID or npcID == "" then return end
+	local GUID = UnitGUID(unit)
+	if not GUID or GUID == "" then return end
 
-		local weightsTable = lib:GetNPCWeightByCurrentQuests(tonumber(npcID))
-		if not weightsTable then return end
+	local npcID = select(6, ("-"):split(GUID))
+	if not npcID or npcID == "" then return end
 
-		for questID, npcWeight in next, weightsTable do
-			local questTitle = QuestUtils_GetQuestName(questID)
-			for j = 1, self:NumLines() do
-				if _G["GameTooltipTextLeft" .. j] and _G["GameTooltipTextLeft" .. j]:GetText() == questTitle then
-					_G["GameTooltipTextLeft" .. j]:SetText(_G["GameTooltipTextLeft" .. j]:GetText() .. " - " .. tostring(math.floor((npcWeight * 100) + 0.5) / 100) .. "%")
-				end
+	local weightsTable = lib:GetNPCWeightByCurrentQuests(tonumber(npcID))
+	if not weightsTable then return end
+
+	for questID, npcWeight in next, weightsTable do
+		local questTitle = QuestUtils_GetQuestName(questID)
+		for j = 1, self:NumLines() do
+			if _G["GameTooltipTextLeft" .. j] and _G["GameTooltipTextLeft" .. j]:GetText() == questTitle then
+				_G["GameTooltipTextLeft" .. j]:SetText(_G["GameTooltipTextLeft" .. j]:GetText() .. " - |cff19A2FF" .. tostring(math.floor((npcWeight * 100) + 0.5) / 100) .. "%|r")
 			end
 		end
-	end)
-	
+	end
 end
 
 GameTooltip:HookScript("OnTooltipSetUnit", AddWeightToTooltip)
