b42b012bf791c754062a7914617802b6e5336e23
Saaappi <alex.metz1024@gmail.com>
2022-04-29 01:20:35 -0700

    Fixed a problem with Vehicle automation.

--- Automations/Vehicles.lua
+++ Automations/Vehicles.lua
@@ -12,12 +12,14 @@ e:SetScript("OnEvent", function(self, event, ...)
 		if guid then
 			local _, _, _, _, _, npcId = strsplit("-", guid); npcId = tonumber(npcId)
 			if npcId then
-				for _, id in ipairs(addonTable.VEHICLES) do
-					if id == npcId then
-						OverrideActionBarLeaveFrameLeaveButton:Click()
-						break
+				C_Timer.After(1, function()
+					for _, id in ipairs(addonTable.VEHICLES) do
+						if id == npcId then
+							OverrideActionBarLeaveFrameLeaveButton:Click()
+							break
+						end
 					end
-				end
+				end)
 			end
 		end
 	end
