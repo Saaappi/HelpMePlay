f372e32013a59192e9d27e7c576234431e832f3a
Saaappi <alex.metz1024@gmail.com>
2022-05-02 00:38:48 -0700

    Colored the time.

--- Systems/Commands.lua
+++ Systems/Commands.lua
@@ -323,7 +323,7 @@ SlashCmdList["HelpMePlay"] = function(command, editbox)
 											reagents[reagentName]["count"] = reagents[reagentName]["count"] + reagentCount
 										else
 											if HelpMePlayOptionsDB.Logging then
-												print(L_GLOBALSTRINGS["Reagent Name is Nil"] .. " " .. date("%X"))
+												print(L_GLOBALSTRINGS["Reagent Name is Nil"] .. " |cffe6cc80" .. date("%X") .. "|r")
 											end
 										end
 									end
