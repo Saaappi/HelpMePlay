257de1ef1a2e76df23d5ad22b3d7ac13d1c68aec
Saaappi <alex.metz1024@gmail.com>
2022-05-08 23:01:06 -0700

    Added another shadehound to the vehicle db.

--- Data/VehicleDB.lua
+++ Data/VehicleDB.lua
@@ -5,7 +5,8 @@ local L_NOTES = addonTable.L_NOTES
 local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
 local VEHICLES = {
 	89619,	-- Murky (Highmountain)
-	173149,	-- Bridled Shadehound (The Maw)
+	173149,	-- Bridled Shadehound (The Maw) (Stolen from the Outriders)
+	176390,	-- Bridled Shadehound (The Maw) (Chained up in the area)
 }
 
 addonTable.VEHICLES = VEHICLES
