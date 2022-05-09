581af1a40afd87c8f5596ada21218ac1556e6bbb
Saaappi <alex.metz1024@gmail.com>
2022-05-08 22:54:35 -0700

    Added shadehound to vehicle db.

--- Data/VehicleDB.lua
+++ Data/VehicleDB.lua
@@ -4,7 +4,8 @@ local L_DIALOG = addonTable.L_DIALOG
 local L_NOTES = addonTable.L_NOTES
 local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
 local VEHICLES = {
-	89619, -- Murky (Highmountain)
+	89619,	-- Murky (Highmountain)
+	173149,	-- Bridled Shadehound (The Maw)
 }
 
 addonTable.VEHICLES = VEHICLES
