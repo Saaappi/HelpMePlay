f27367dd1ac1cec710fd15f01dfccfc1e7a48474
Saaappi <alex.metz1024@gmail.com>
2022-05-12 16:18:53 -0700

    Added a keybind to add items to the Junker db.

--- Keybinding.lua
+++ Keybinding.lua
@@ -8,6 +8,7 @@ BINDING_HEADER_HELPMEPLAY = "HelpMePlay"
 BINDING_NAME_HELPMEPLAY_OPEN_SETTINGS = L_GLOBALSTRINGS["Open Settings"]
 BINDING_NAME_HELPMEPLAY_LEARN_ALL_TRANSMOG = L_GLOBALSTRINGS["Learn All Transmog"]
 BINDING_NAME_HELPMEPLAY_CALCULATE_REAGENTS = L_GLOBALSTRINGS["Calculate Reagents"]
+BINDING_NAME_HELPMEPLAY_ADD_TO_JUNKER = L_GLOBALSTRINGS["Add to Junker"]
 
 function HelpMePlayKeyPressHandler(key)
 	if key == GetBindingKey("HELPMEPLAY_OPEN_SETTINGS") then
@@ -16,5 +17,19 @@ function HelpMePlayKeyPressHandler(key)
 		HelpMePlay_GetEquippedItems()
 	elseif key == GetBindingKey("HELPMEPLAY_CALCULATE_REAGENTS") then
 		HelpMePlay_CalculateReagents()
+	elseif key == GetBindingKey("HELPMEPLAY_ADD_TO_JUNKER") then
+		if GameTooltip:IsVisible() then
+			local _, itemLink = GameTooltip:GetItem()
+			if itemLink then
+				local _, itemId = string.split(":", itemLink); itemId = tonumber(itemId)
+				if HelpMePlayJunkerDB[itemId] then
+					HelpMePlayJunkerDB[itemId] = true
+					print(string.format(L_GLOBALSTRINGS["Colored Addon Name"] .. ": " .. L_GLOBALSTRINGS["Added Item Text"], itemLink))
+				else
+					HelpMePlayJunkerDB[itemId] = nil
+					print(string.format(L_GLOBALSTRINGS["Colored Addon Name"] .. ": " .. L_GLOBALSTRINGS["Removed Item Text"], itemLink))
+				end
+			end
+		end
 	end
 end
