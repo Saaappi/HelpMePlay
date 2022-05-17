d2e81bfaebac886f9e14cd775d96e8eeea4641f9
Saaappi <alex.metz1024@gmail.com>
2022-05-17 16:22:53 -0700

    Removed additional fluff text.

--- Automations/PlayerChoice.lua
+++ Automations/PlayerChoice.lua
@@ -22,51 +22,46 @@ e:SetScript("OnEvent", function(self, event, ...)
 				end
 			end
 			--[[
-			if unitGuid then
-				--local _, _, _, _, _, npcId = strsplit("-", unitGuid); npcId = tonumber(npcId)
-				if mapId == 1670 then -- Oribos
-				elseif mapId == 543 then -- Gorgrond
-					if HelpMePlayOptionsDB.GarrisonTables then
-						-- Choose the Sparring Arena, otherwise
-						-- choose nothing.
-						choiceOptionInfo = C_PlayerChoice.GetPlayerChoiceOptionInfo(2);
+			if mapId == 543 then -- Gorgrond
+				if HelpMePlayOptionsDB.GarrisonTables then
+					-- Choose the Sparring Arena, otherwise
+					-- choose nothing.
+					choiceOptionInfo = C_PlayerChoice.GetPlayerChoiceOptionInfo(2);
+					SendPlayerChoiceResponse(choiceOptionInfo.buttons[1].id)
+					HideUIPanel(PlayerChoiceFrame)
+				end
+			end
+			-- Not every player choice is associated
+			-- with an NPC, so these will be the
+			-- player choices prompted by objects.
+			if mapId == 84 or mapId == 85 then -- Orgrimmar / Stormwind City
+				if HelpMePlayOptionsDB.AdventureMaps == false or HelpMePlayOptionsDB.AdventureMaps == nil then return end
+				for i=1,3 do
+					choiceOptionInfo = C_PlayerChoice.GetPlayerChoiceOptionInfo(i)
+					if choiceOptionInfo.id == 1014 then
+						-- This is the Tanaan Jungle (Dark Portal) option.
 						SendPlayerChoiceResponse(choiceOptionInfo.buttons[1].id)
 						HideUIPanel(PlayerChoiceFrame)
+						break
 					end
 				end
-			else
-				-- Not every player choice is associated
-				-- with an NPC, so these will be the
-				-- player choices prompted by objects.
-				if mapId == 84 or mapId == 85 then -- Orgrimmar / Stormwind City
-					if HelpMePlayOptionsDB.AdventureMaps == false or HelpMePlayOptionsDB.AdventureMaps == nil then return end
-					for i=1,3 do
-						choiceOptionInfo = C_PlayerChoice.GetPlayerChoiceOptionInfo(i)
-						if choiceOptionInfo.id == 1014 then
-							-- This is the Tanaan Jungle (Dark Portal) option.
-							SendPlayerChoiceResponse(choiceOptionInfo.buttons[1].id)
-							HideUIPanel(PlayerChoiceFrame)
-							break
-						end
-					end
-				elseif mapId == 1670 then -- Oribos
-					if HelpMePlayOptionsDB.Covenant == L_GLOBALSTRINGS["Kyrian"] then
-						choiceOptionInfo = C_PlayerChoice.GetPlayerChoiceOptionInfo(1)
-						SendPlayerChoiceResponse(choiceOptionInfo.buttons[1].id)
-						HideUIPanel(PlayerChoiceFrame)
-					elseif HelpMePlayOptionsDB.Covenant == L_GLOBALSTRINGS["Venthyr"] then
-						choiceOptionInfo = C_PlayerChoice.GetPlayerChoiceOptionInfo(3)
-						SendPlayerChoiceResponse(choiceOptionInfo.buttons[1].id)
-						HideUIPanel(PlayerChoiceFrame)
-					elseif HelpMePlayOptionsDB.Covenant == L_GLOBALSTRINGS["Necrolord"] then
-						choiceOptionInfo = C_PlayerChoice.GetPlayerChoiceOptionInfo(5)
-						SendPlayerChoiceResponse(choiceOptionInfo.buttons[1].id)
-						HideUIPanel(PlayerChoiceFrame)
-					elseif HelpMePlayOptionsDB.Covenant == L_GLOBALSTRINGS["Night Fae"] then
-						choiceOptionInfo = C_PlayerChoice.GetPlayerChoiceOptionInfo(7)
-						SendPlayerChoiceResponse(choiceOptionInfo.buttons[1].id)
-						HideUIPanel(PlayerChoiceFrame)
-					end
+			elseif mapId == 1670 then -- Oribos
+				if HelpMePlayOptionsDB.Covenant == L_GLOBALSTRINGS["Kyrian"] then
+					choiceOptionInfo = C_PlayerChoice.GetPlayerChoiceOptionInfo(1)
+					SendPlayerChoiceResponse(choiceOptionInfo.buttons[1].id)
+					HideUIPanel(PlayerChoiceFrame)
+				elseif HelpMePlayOptionsDB.Covenant == L_GLOBALSTRINGS["Venthyr"] then
+					choiceOptionInfo = C_PlayerChoice.GetPlayerChoiceOptionInfo(3)
+					SendPlayerChoiceResponse(choiceOptionInfo.buttons[1].id)
+					HideUIPanel(PlayerChoiceFrame)
+				elseif HelpMePlayOptionsDB.Covenant == L_GLOBALSTRINGS["Necrolord"] then
+					choiceOptionInfo = C_PlayerChoice.GetPlayerChoiceOptionInfo(5)
+					SendPlayerChoiceResponse(choiceOptionInfo.buttons[1].id)
+					HideUIPanel(PlayerChoiceFrame)
+				elseif HelpMePlayOptionsDB.Covenant == L_GLOBALSTRINGS["Night Fae"] then
+					choiceOptionInfo = C_PlayerChoice.GetPlayerChoiceOptionInfo(7)
+					SendPlayerChoiceResponse(choiceOptionInfo.buttons[1].id)
+					HideUIPanel(PlayerChoiceFrame)
 				end
 			end
 			]]--
