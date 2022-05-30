595096c14663d3adf3729dc22ca87025bb24a0fc
Saaappi <alex.metz1024@gmail.com>
2022-05-29 19:09:55 -0700

    Added comments to Queues and Repairs modules.

--- Automations/Queues.lua
+++ Automations/Queues.lua
@@ -3,6 +3,11 @@ local L_DIALOG = addonTable.L_DIALOG
 local L_NOTES = addonTable.L_NOTES
 local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
 
+--[[
+	Description:
+		If Queues automation is enabled, then automatically
+		accept LFG queues.
+]]--
 LFGDungeonReadyDialogEnterDungeonButton:SetScript("OnShow", function()
 	if HelpMePlayOptionsDB.Queues then
 		LFGDungeonReadyDialogEnterDungeonButton:Click()
--- Automations/Repairs.lua
+++ Automations/Repairs.lua
@@ -4,16 +4,19 @@ local L_DIALOG = addonTable.L_DIALOG
 local L_NOTES = addonTable.L_NOTES
 local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
 
+--[[
+	Description:
+		If the player needs to repair, has the money to
+		repair, and the merchant can repair the player's
+		equipment, then automatically repair, assuming
+		the Merchants automation is enabled.
+		
+		This automation will *NEVER* use guild repairs!
+]]--
 e:RegisterEvent("MERCHANT_SHOW")
 e:SetScript("OnEvent", function(self, event, ...)
 	if event == "MERCHANT_SHOW" then
 		if HelpMePlayOptionsDB.Merchants == false or HelpMePlayOptionsDB.Merchants == nil then return end
-		-- Check if the player needs to repair.
-		-- If the player needs repairs, check
-		-- if the merchant can repair, then repair
-		-- if the player has enough money.
-		--
-		-- Does NOT use guild repair.
 		local repairCost, needsRepair = GetRepairAllCost()
 		if repairCost > 0 and GetMoney() > repairCost then
 			if CanMerchantRepair() then
