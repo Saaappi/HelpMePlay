79e93d33adf5b0f2d38b2a9253689321f7b7593a
Saaappi <alex.metz1024@gmail.com>
2022-10-09 04:31:39 -0700

    Some dialogs for WOD.

--- Data/Dialogs/WOD_DialogDB.lua
+++ Data/Dialogs/WOD_DialogDB.lua
@@ -5,11 +5,34 @@ local DIALOG_WOD = {
 	-- Supported Maps: 572 (Draenor)
 	-- Garrison (Alliance)
 	-- End of Garrison (Alliance)
-	-- Garrison (Horde)
-	-- End of Garrison (Horde)
 	-- Gorgrond
 	-- End of Gorgrond
 	-- Tanaan Jungle
+	[78556] = { -- Ariok
+		["g"] = {
+			{
+				["o"] 	= 1,
+				["c"] 	= { "none" },
+			},
+		},
+	},
+	[78568] = { -- Thaelin Darkanvil
+		["g"] = {
+			{
+				["o"] 	= 1,
+				["c"] 	= { "quests.isActive" },
+				["q"] 	= { 35747 }, -- Taking a Trip to the Top of the Tank
+			},
+		},
+	},
+	[79243] = { -- Baros Alexston
+		["g"] = {
+			{
+				["o"] 	= 1,
+				["c"] 	= { "none" },
+			},
+		},
+	},
 	-- End of Tanaan Jungle
 }
 
