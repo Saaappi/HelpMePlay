139614f3376b0a794393c09335f17ad0497cb93a
Saaappi <alex.metz1024@gmail.com>
2022-10-08 17:23:36 -0700

    The soulbind tech is now implemented. Just need to add individual soulbinds and spec support.

--- Data/SoulbindsDB.lua
+++ Data/SoulbindsDB.lua
@@ -2,71 +2,117 @@ local addonName, addonTable = ...
 local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
 local SOULBINDS_BY_SPEC = {
 	[1] = { -- Warrior
-		[71] = 4, 	-- Arms (Night Fae)
-		[72] = 1, 	-- Fury (Kyrian)
-		[73] = 1, 	-- Protection (Kyrian)
+		[71] = {}, 	-- Arms (Night Fae)
+		[72] = {}, 	-- Fury (Kyrian)
+		[73] = {}, 	-- Protection (Kyrian)
 	},
 	[2] = { -- Paladin
-		[65] = 2, 	-- Holy (Venthyr)
-		[66] = 1, 	-- Protection (Kyrian)
-		[70] = 1, 	-- Retribution (Kyrian)
+		[65] = {}, 	-- Holy (Venthyr)
+		[66] = {}, 	-- Protection (Kyrian)
+		[70] = {}, 	-- Retribution (Kyrian)
 	},
 	[3] = { -- Hunter
-		[253] = 4, 	-- Beast Mastery (Night Fae)
-		[254] = 1, 	-- Marksmanship (Kyrian)
-		[255] = 1, 	-- Survival (Kyrian)
+		[253] = {}, 	-- Beast Mastery (Night Fae)
+		[254] = {}, 	-- Marksmanship (Kyrian)
+		[255] = {}, 	-- Survival (Kyrian)
 	},
 	[4] = { -- Rogue
-		[259] = 2, 	-- Assassination (Venthyr)
-		[260] = 1, 	-- Outlaw (Kyrian)
-		[261] = 2, 	-- Subtlety (Venthyr)
+		[259] = {}, 	-- Assassination (Venthyr)
+		[260] = {}, 	-- Outlaw (Kyrian)
+		[261] = {}, 	-- Subtlety (Venthyr)
 	},
 	[5] = { -- Priest
-		[256] = 1, 	-- Discipline (Kyrian)
-		[257] = 4, 	-- Holy (Night Fae)
-		[258] = 3, 	-- Shadow (Necrolord)
+		[256] = {}, 	-- Discipline (Kyrian)
+		[257] = {}, 	-- Holy (Night Fae)
+		[258] = {}, 	-- Shadow (Necrolord)
 	},
 	[6] = { -- Death Knight
-		[250] = 2, 	-- Blood (Venthyr)
-		[251] = 4, 	-- Frost (Night Fae)
-		[252] = 4, 	-- Unholy (Night Fae)
+		[250] = {}, 	-- Blood (Venthyr)
+		[251] = {}, 	-- Frost (Night Fae)
+		[252] = {}, 	-- Unholy (Night Fae)
 	},
 	[7] = { -- Shaman
-		[262] = 3, 	-- Elemental (Necrolord)
-		[263] = 3, 	-- Enhancement (Necrolord)
-		[264] = 2, 	-- Restoration (Venthyr)
+		[262] = {}, 	-- Elemental (Necrolord)
+		[263] = {}, 	-- Enhancement (Necrolord)
+		[264] = {}, 	-- Restoration (Venthyr)
 	},
 	[8] = { -- Mage
-		[62] = 1, 	-- Arcane (Kyrian)
-		[63] = 4, 	-- Fire (Night Fae)
-		[64] = 4, 	-- Frost (Night Fae)
+		[62] = {}, 	-- Arcane (Kyrian)
+		[63] = {}, 	-- Fire (Night Fae)
+		[64] = {}, 	-- Frost (Night Fae)
 	},
 	[9] = { -- Warlock
-		[265] = 4, 	-- Affliction (Night Fae)
+		[265] = {}, 	-- Affliction (Night Fae)
 		[266] = { 	-- Demonology (Night Fae)
-			["soulbind"] = 2, -- Dreamweaver
-			
+			["id"] 		= 2, -- Dreamweaver
+			["tree"] 	= 275,
+			["conduits"] = {
+				{ -- 1
+					["n"] = 870,
+					["c"] = 160, -- Resolute Barrier
+				},
+				{ -- 2
+					["n"] = 836,
+					["c"] = 204, -- Borne of Blood
+				},
+				{ -- 3
+					["n"] = 827,
+					["c"] = 0, 
+				},
+				{ -- 4
+					["n"] = 830,
+					["c"] = 205, -- Carnivorous Stalkers
+				},
+				{ -- 5
+					["n"] = 832,
+					["c"] = 222, -- Diabolic Bloodstone
+				},
+				{ -- 6
+					["n"] = 834,
+					["c"] = 0,
+				},
+				{ -- 7
+					["n"] = 824,
+					["c"] = 0,
+				},
+				{ -- 8
+					["n"] = 1843,
+					["c"] = 207, -- Fel Commando
+				},
+				{ -- 9
+					["n"] = 1845,
+					["c"] = 0,
+				},
+				{ -- 10
+					["n"] = 1847,
+					["c"] = 168, -- Shade of Terror
+				},
+				{ -- 11
+					["n"] = 1848,
+					["c"] = 0,
+				},
+			}
 		},
-		[267] = 4, 	-- Destruction (Night Fae)
+		[267] = {}, 	-- Destruction (Night Fae)
 	},
 	[10] = { -- Monk
-		[268] = 4, 	-- Brewmaster (Night Fae)
-		[270] = 2, 	-- Mistweaver (Venthyr)
-		[269] = 3, 	-- Windwalker (Necrolord)
+		[268] = {}, 	-- Brewmaster (Night Fae)
+		[270] = {}, 	-- Mistweaver (Venthyr)
+		[269] = {}, 	-- Windwalker (Necrolord)
 	},
 	[11] = { -- Druid
-		[102] = 4, 	-- Balance (Night Fae)
-		[103] = 4, 	-- Feral (Night Fae)
-		[104] = 4, 	-- Guardian (Night Fae)
-		[105] = 4, 	-- Restoration (Night Fae)
+		[102] = {}, 	-- Balance (Night Fae)
+		[103] = {}, 	-- Feral (Night Fae)
+		[104] = {}, 	-- Guardian (Night Fae)
+		[105] = {}, 	-- Restoration (Night Fae)
 	},
 	[12] = { -- Demon Hunter
-		[577] = 2, 	-- Havoc (Venthyr)
-		[581] = 1, 	-- Vengeance (Kyrian)
+		[577] = {}, 	-- Havoc (Venthyr)
+		[581] = {}, 	-- Vengeance (Kyrian)
 	},
 	[13] = { -- Evoker
-		[1467] = 4, -- Devastation (Night Fae)
-		[1468] = 4, -- Preservation (Night Fae)
+		[1467] = {}, -- Devastation (Night Fae)
+		[1468] = {}, -- Preservation (Night Fae)
 	},
 }
 
--- Systems/Soulbinds.lua
+++ Systems/Soulbinds.lua
@@ -24,7 +24,78 @@ e:SetScript("OnEvent", function(self, event, addon, ...)
 		end)
 		HMPSoulbindButton:SetScript("OnClick", function(self)
 			if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
-			print("I WAS CLICKED!")
+			
+			-- First get the player's class and specialization.
+			local _, _, classId = UnitClass("player")
+			local specIndex = GetSpecialization()
+			local specId = GetSpecializationInfo(specIndex)
+			
+			local soulbind = addonTable.SOULBINDS_BY_SPEC[classId][specId]
+			if soulbind == {} then
+				-- This spec is currently unsupported.
+				print(L_GLOBALSTRINGS["Text.Output.ColoredAddOnName"] .. ": " .. L_GLOBALSTRINGS["Text.Output.SpecUnsupporteForSoulbinds"])
+				return
+			end
+			
+			-- Determine if the assigned soulbind is unlocked.
+			if C_Soulbinds.GetSoulbindData(soulbind.id).unlocked then
+				-- Check if the assigned soulbind can be activated.
+				-- Failure here would indicate the assigned soulbind
+				-- is already activated.
+				if C_Soulbinds.CanActivateSoulbind(soulbind.id) then
+					-- Activate the soulbind.
+					C_Soulbinds.ActivateSoulbind(soulbind.id)
+					
+					C_Timer.After(1, function()
+						-- Get the current conduit tree for the assigned
+						-- soulbind. Let's see if we need to activate any
+						-- nodes or change any assigned conduits.
+						local tree = C_Soulbinds.GetTree(soulbind.tree)
+						for i = 1, #tree.nodes do
+							-- Check to see if any nodes are unavailable.
+							if tree.nodes[i].state == 0 then
+								print(L_GLOBALSTRINGS["Text.Output.ColoredAddOnName"] .. ": " .. L_GLOBALSTRINGS["Text.Output.ConduitsNotUnlocked"])
+								return
+							end
+						end
+						
+						-- After a short delay, attempt to select the
+						-- assigned node.
+						for _, node in ipairs(soulbind.conduits) do
+							C_Timer.After(0.5, function()
+								C_Soulbinds.SelectNode(node.n)
+							end)
+						end
+						
+						-- Ensure all the proper conduit nodes are selected.
+						C_Timer.After(1, function()
+							tree = C_Soulbinds.GetTree(soulbind.tree)
+							for i = 1, #tree.nodes do
+								for _, node in ipairs(soulbind.conduits) do
+									if tree.nodes[i].ID == node.n then
+										if tree.nodes[i].state ~= 3 then
+											print(L_GLOBALSTRINGS["Text.Output.ColoredAddOnName"] .. ": " .. L_GLOBALSTRINGS["Text.Output.ConduitNodesIncorrect"])
+											return
+										end
+									end
+								end
+							end
+						end)
+						
+						-- Install the conduits.
+						C_Timer.After(1, function()
+							for _, node in ipairs(soulbind.conduits) do
+								C_Timer.After(0.1, function()
+									C_Soulbinds.ModifyNode(node.n, node.c, 0)
+									SoulbindViewer.CommitConduitsButton:Click()
+								end)
+							end
+						end)
+					end)
+				end
+			else
+				print(L_GLOBALSTRINGS["Text.Output.ColoredAddOnName"] .. ": " .. L_GLOBALSTRINGS["Text.Output.AssignedSoulbindLocked"])
+			end
 		end)
 	end
 end)
