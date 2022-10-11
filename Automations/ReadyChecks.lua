e59efabd2ccfc8cc4d3acc24723191775613f9d9
Saaappi <alex.metz1024@gmail.com>
2022-10-11 16:04:52 -0700

    Added a Ready Checks option to automatically respond to a ready check. Organized the features toggles in alphabetical order.

---
+++ Automations/ReadyChecks.lua
@@ -0,0 +1,14 @@
+local addonName, addonTable = ...
+local e = CreateFrame("Frame")
+
+e:RegisterEvent("READY_CHECK")
+e:SetScript("OnEvent", function(self, event, ...)
+	if event == "READY_CHECK" then
+		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
+		if HelpMePlayDB.ReadyChecksEnabled == false or HelpMePlayDB.ReadyChecksEnabled == nil then return false end
+		
+		if HelpMePlayDB.ReadyChecksEnabled then
+			ConfirmReadyCheck(1)
+		end
+	end
+end)
