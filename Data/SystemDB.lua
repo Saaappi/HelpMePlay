8c3337ad7907d466327259c41214187ce514e248
Saaappi <alex.metz1024@gmail.com>
2022-10-16 02:27:38 -0700

    Added Artaius to the list of developer characters.

--- Data/SystemDB.lua
+++ Data/SystemDB.lua
@@ -2,6 +2,7 @@ local addonName, addonTable = ...
 local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
 
 local GUID = {
+	["Player-1426-0DEEAE42"] = 0, -- Artaius (Aerie Peak)
 	["Player-1426-0ADB1BEE"] = 0, -- Grimfeast (Aerie Peak)
 	["Player-1426-0DEE000B"] = 0, -- Highchants (Aerie Peak)
 }
