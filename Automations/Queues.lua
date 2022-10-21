5e768f056e230c910e39fc95bf39bc501de78a70
Saaappi <alex.metz1024@gmail.com>
2022-10-20 23:54:51 -0700

    This should hopefully resolve the holiday boss button.

--- Automations/Queues.lua
+++ Automations/Queues.lua
@@ -10,19 +10,6 @@ local HMPQueueButton = _G.CreateFrame(
 	"OptionsButtonTemplate"
 )
 
-local function GetTimeForHoliday(eventId)
-	local startTime, endTime = "", ""
-	for i = 1, 10 do
-		local eventData = C_Calendar.GetDayEvent(0, date("%d"), i)
-		if eventData.eventID == eventId then
-			startTime = eventData.startTime.month .. "/" .. eventData.startTime.monthDay .. " " .. eventData.startTime.hour .. ":00"
-			endTime = eventData.endTime.month .. "/" .. eventData.endTime.monthDay .. " " .. eventData.endTime.hour .. ":00"
-			break
-		end
-	end
-	return startTime, endTime
-end
-
 LFGDungeonReadyDialogEnterDungeonButton:SetScript("OnShow", function()
 	if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 	if HelpMePlayDB.QueuesEnabled then
@@ -38,41 +25,46 @@ e:SetScript("OnEvent", function(self, event, ...)
 		if HelpMePlayDB.HolidayQueuesEnabled then
 			local isInitialLogin, isReload = ...
 			if isInitialLogin or isReload then
-				if date("%m") == "10" then
-					local startTime, endTime = GetTimeForHoliday(324) -- Hallow's End
-					if date("%m/%d %H:%M") >= startTime and date("%m/%d %H:%M") <= endTime then
-						local isDailyRewardCollected = GetLFGDungeonRewards(285)
-						if isDailyRewardCollected == false then
-							if not select(11, C_MountJournal.GetMountInfoByID(219)) then
-								normalTexture:SetTexture("Interface\\ICONS\\inv_belt_12")
-								normalTexture:SetSize(28, 26)
+				local calendarDate = C_DateAndTime.GetCurrentCalendarTime()
+				local timeStamp = calendarDate.month .. "/" .. calendarDate.monthDay .. " " .. calendarDate.hour .. ":" .. calendarDate.minute
+				for i = 1, C_Calendar.GetNumDayEvents(0, calendarDate.monthDay) do
+					local event = C_Calendar.GetDayEvent(0, calendarDate.monthDay, i)
+					if event then
+						local startTime = event.startTime.month .. "/" .. event.startTime.monthDay .. " " .. event.startTime.hour .. ":00"
+						local endTime = event.endTime.month .. "/" .. event.endTime.monthDay .. " " .. event.endTime.hour .. ":00"
+						if event.eventID == 324 then -- Hallow's End
+							if timeStamp >= startTime and timeStamp <= endTime then
+								local isDailyRewardCollected = GetLFGDungeonRewards(285)
+								if not isDailyRewardCollected then
+									if not select(11, C_MountJournal.GetMountInfoByID(219)) then
+										normalTexture:SetTexture("Interface\\ICONS\\inv_belt_12")
+										normalTexture:SetSize(28, 26)
+									end
+								end
 							end
-						end
-					end
-				elseif date("%m") == "09" then
-					local startTime, endTime = GetTimeForHoliday(372) -- Brewfest
-					if date("%m/%d %H:%M") >= startTime and date("%m/%d %H:%M") <= endTime then
-						local isDailyRewardCollected = GetLFGDungeonRewards(287)
-						if isDailyRewardCollected == false then
-							if not select(11, C_MountJournal.GetMountInfoByID(202)) or not select(11, C_MountJournal.GetMountInfoByID(226)) then
-								normalTexture:SetTexture("Interface\\ICONS\\ability_mount_kotobrewfest")
-								normalTexture:SetSize(28, 26)
+						elseif event.eventID == 372 then -- Brewfest
+							if timeStamp >= startTime and timeStamp <= endTime then
+								local isDailyRewardCollected = GetLFGDungeonRewards(287)
+								if not isDailyRewardCollected then
+									if not select(11, C_MountJournal.GetMountInfoByID(202)) or not select(11, C_MountJournal.GetMountInfoByID(226)) then
+										normalTexture:SetTexture("Interface\\ICONS\\ability_mount_kotobrewfest")
+										normalTexture:SetSize(28, 26)
+									end
+								end
 							end
-						end
-					end
-				elseif date("%m") == "02" then
-					local startTime, endTime = GetTimeForHoliday(423) -- Love is in the Air
-					if date("%m/%d %H:%M") >= startTime and date("%m/%d %H:%M") <= endTime then
-						local isDailyRewardCollected = GetLFGDungeonRewards(288)
-						if isDailyRewardCollected == false then
-							if not select(11, C_MountJournal.GetMountInfoByID(352)) then
-								normalTexture:SetTexture("Interface\\ICONS\\inv_rocketmountpink")
-								normalTexture:SetSize(28, 26)
+						elseif event.eventID == 423 then -- Love is in the Air
+							if timeStamp >= startTime and timeStamp <= endTime then
+								local isDailyRewardCollected = GetLFGDungeonRewards(288)
+								if not isDailyRewardCollected then
+									if not select(11, C_MountJournal.GetMountInfoByID(352)) then
+										normalTexture:SetTexture("Interface\\ICONS\\inv_rocketmountpink")
+										normalTexture:SetSize(28, 26)
+									end
+								end
 							end
 						end
 					end
 				end
-				
 				highlightTexture:SetTexture("Interface\\Buttons\\ButtonHilight-Square")
 				highlightTexture:SetSize(24, 23)
 				
