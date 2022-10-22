8a132a817b0b552e60f6ec83ca9df9b4770d3117
Saaappi <alex.metz1024@gmail.com>
2022-10-21 18:01:02 -0700

    Added some initial code to show the button.

--- Automations/Openables.lua
+++ Automations/Openables.lua
@@ -7,6 +7,19 @@ e:SetScript("OnEvent", function(self, event, ...)
 	if event == "ITEM_PUSH" then
 		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 		if HelpMePlayDB.OpenablesEnabled == false or HelpMePlayDB.OpenablesEnabled == nil then return false end
-		local _, fileIconId = ...
+		local bagId, fileIconId = ...
+		if bagId and fileIconId then
+			local numSlots = GetContainerNumSlots(bagId)
+			for slot = 1, numSlots do
+				local item = GetContainerItemInfo(bagId, slot)
+				if item then
+					if item.lootable then
+						if item.texture == fileIconId then
+							-- make the button
+						end
+					end
+				end
+			end
+		end
 	end
 end)
