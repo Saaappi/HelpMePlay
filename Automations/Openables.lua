7a1627de66f1c96df420f1bbe57ff09edf2a0f4f
Saaappi <alex.metz1024@gmail.com>
2022-10-21 19:42:12 -0700

    Hopefully this solves the problem?

--- Automations/Openables.lua
+++ Automations/Openables.lua
@@ -5,45 +5,44 @@ local bag, bagSlot = 0, 0
 local HMPOpenableButton = _G.CreateFrame(
 	"Button",
 	"HMPOpenableButton",
-	_G.CharacterMicroButton,
+	nil,
 	"OptionsButtonTemplate"
 )
 local normalTexture = e:CreateTexture()
 local highlightTexture = e:CreateTexture()
 highlightTexture:SetTexture("Interface\\Buttons\\ButtonHilight-Square")
-highlightTexture:SetSize(28, 29)
+highlightTexture:SetSize(24, 23)
+
+local containerItems = {
+	[117392] = true, 	-- Loot-Filled Pumpkin
+}
 
 e:RegisterEvent("ITEM_DATA_LOAD_RESULT")
 e:SetScript("OnEvent", function(self, event, ...)
 	if event == "ITEM_DATA_LOAD_RESULT" then
 		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 		if HelpMePlayDB.OpenablesEnabled == false or HelpMePlayDB.OpenablesEnabled == nil then return false end
-		local itemId = ...
 		for bagId = 0, 4 do
 			local numSlots = GetContainerNumSlots(bagId)
 			for slot = 1, numSlots do
-				local texture, _, _, _, _, lootable, _, _, _, containerItemId = GetContainerItemInfo(bagId, slot)
-				if texture then
-					if lootable then
-						if containerItemId == itemId then
-							bag = bagId
-							bagSlot = slot
-							
-							HMPQueueButton:RegisterForClicks("LeftButtonUp", "RightButtonUp")
-							
-							normalTexture:SetTexture("Interface\\ICONS\\inv_misc_bag_10")
-							normalTexture:SetSize(32, 32)
-				
-							HMPOpenableButton:SetNormalTexture(normalTexture)
-							HMPOpenableButton:SetHighlightTexture(highlightTexture, "ADD")
-				
-							HMPOpenableButton:SetSize(32, 32)
-							HMPOpenableButton:SetPoint("LEFT", -45, 0)
-				
-							HMPOpenableButton:Show()
-							return
-						end
-					end
+				local containerItemId = select(10, GetContainerItemInfo(bagId, slot))
+				if containerItems[containerItemId] then
+					bag = bagId
+					bagSlot = slot
+					
+					HMPQueueButton:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+					
+					normalTexture:SetTexture("Interface\\ICONS\\inv_misc_bag_10")
+					normalTexture:SetSize(28, 26)
+		
+					HMPOpenableButton:SetNormalTexture(normalTexture)
+					HMPOpenableButton:SetHighlightTexture(highlightTexture, "ADD")
+		
+					HMPOpenableButton:SetSize(28, 26)
+					HMPOpenableButton:SetPoint("CENTER", "UIParent")
+		
+					HMPOpenableButton:Show()
+					return
 				end
 			end
 		end
