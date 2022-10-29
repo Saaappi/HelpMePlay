ca818e579b3e416affbb1115e5bb90d54378ef78
Saaappi <alex.metz1024@gmail.com>
2022-10-28 19:05:43 -0700

    I believe this commit fixes the problem with the icon remaining on nameplates once they've been removed.

--- Systems/QuestMobs.lua
+++ Systems/QuestMobs.lua
@@ -1,49 +1,58 @@
 local addonName, addonTable = ...
 local e = CreateFrame("Frame")
 local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
+local iconKey = addonName .. "Icon"
 local tooltip = _G.CreateFrame("GameTooltip", "HMPQuestTooltip", nil, "GameTooltipTemplate")
 
-local function AddIconToNameplate(unit)
-	unit.questIcon = _G.CreateFrame("Frame", nil, unit)
-	unit.questIcon:SetSize(20, 20)
-	unit.questIcon:SetPoint("TOP", 0, 10)   
-
-	unit.questIconTexture = unit.questIcon:CreateTexture(nil, "OVERLAY")                                   
-	unit.questIconTexture:SetTexture("Interface\\Garrison\\MobileAppIcons")
-	unit.questIconTexture:SetTexCoord(0.381836, 0.506836, 0.254883, 0.379883)
-	unit.questIconTexture:SetAllPoints()
-	
-	unit.questIcon:Show()
-end
-
-hooksecurefunc("CompactUnitFrame_UpdateName", function(frame)
-	if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
-	if HelpMePlayDB.QuestMobsEnabled == false or HelpMePlayDB.QuestMobsEnabled == nil then return false end
-	
-	local GUID = UnitGUID(frame.unit)
-	local fType = string.split("-", GUID)
-	if fType ~= "Player" then
+local function UpdateNamePlate(namePlate, unit)
+	local icon = namePlate:CreateTexture(nil, "OVERLAY")
+	namePlate[iconKey] = icon
+	if not UnitIsPlayer(unit) then
 		tooltip:SetOwner(WorldFrame, "ANCHOR_NONE")
-		tooltip:SetHyperlink("unit:" .. GUID)
+		tooltip:SetHyperlink("unit:" .. UnitGUID(unit))
 		for i = 3, tooltip:NumLines() do
 			local line = _G["HMPQuestTooltipTextLeft"..i]
 			local text = line:GetText()
 			if string.find(text, "\%\)") or string.find(text, "%d\/%d") then
-				AddIconToNameplate(frame)
+				icon:ClearAllPoints()
+				icon:SetSize(20, 20)
+				icon:SetPoint("TOP", 0, 10)
+				icon:SetTexture("Interface\\Garrison\\MobileAppIcons")
+				icon:SetTexCoord(0.381836, 0.506836, 0.254883, 0.379883)
+				
+				for k,v in pairs(namePlate) do
+					print(k)
+				end
+				
+				icon:Show()
+				return
 			end
 		end
 	end
-end)
+	if icon then
+		icon:Hide()
+	end
+end
 
-hooksecurefunc(NamePlateDriverFrame, "OnNamePlateRemoved", function(base)
-	if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
-	if HelpMePlayDB.QuestMobsEnabled == false or HelpMePlayDB.QuestMobsEnabled == nil then return false end
+e:RegisterEvent("NAME_PLATE_UNIT_ADDED")
+e:RegisterEvent("NAME_PLATE_UNIT_REMOVED")
+e:SetScript("OnEvent", function(self, event, unit)
+	if event == "NAME_PLATE_UNIT_ADDED" then
+		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
+		if HelpMePlayDB.QuestMobsEnabled == false or HelpMePlayDB.QuestMobsEnabled == nil then return false end
+		
+		local namePlate = C_NamePlate.GetNamePlateForUnit(unit)
+		UpdateNamePlate(namePlate, unit)
+	end
 	
-	base.UnitFrame.questIcon:Hide()
-	base.UnitFrame.questIconTexture = nil
-	base.UnitFrame.questIcon = nil
-	for k,v in pairs(base.UnitFrame) do
-		print(k)
+	if event == "NAME_PLATE_UNIT_REMOVED" then
+		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
+		if HelpMePlayDB.QuestMobsEnabled == false or HelpMePlayDB.QuestMobsEnabled == nil then return false end
+		
+		local namePlate = C_NamePlate.GetNamePlateForUnit(unit)
+        if namePlate[iconKey] then
+            namePlate[iconKey]:Hide()
+        end
 	end
 end)
 
