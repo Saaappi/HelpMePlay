c9554c1a38d8b5ae7f12ab8c87830c47aacc005b
Saaappi <alex.metz1024@gmail.com>
2022-10-30 20:49:26 -0700

    The water reminder text will now show on screen opposed to in the chat frame.

--- Systems/PersonalHealth.lua
+++ Systems/PersonalHealth.lua
@@ -6,9 +6,9 @@ local function WaterReminder()
 	if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 	if HelpMePlayDB.WaterReminderEnabled == false or HelpMePlayDB.WaterReminderEnabled == nil then return false end
 		
-	local interval = (math.random(10, 20))*60
+	local interval = (math.random(1, 2))*60
 	C_Timer.After(interval, function()
-		print(L_GLOBALSTRINGS["Text.Output.ColoredAddOnName"] .. ": " .. L_GLOBALSTRINGS["Text.Output.WaterReminder"])
+		UIErrorsFrame:AddMessage("|cffFFD100" .. L_GLOBALSTRINGS["Text.Output.WaterReminder"] .. "|r")
 		PlaySound(110896, "Master", true, true) -- SPELL_8.0_Stormsong_FoulTidewater_PickUp
 		WaterReminder()
 	end)
