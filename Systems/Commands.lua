39b980cf5a38068cb858f8d48f6a42f6c58b042e
Saaappi <alex.metz1024@gmail.com>
2022-11-05 18:45:47 -0700

    Removed systemdb. Changed the conduit, soulbind, and taxi commands to only run in Dev Mode.

--- Systems/Commands.lua
+++ Systems/Commands.lua
@@ -117,30 +117,29 @@ function HelpMePlay:SlashCommandHandler(cmd)
 		end
 		self:Print(string.format(L_GLOBALSTRINGS["Text.Output.ColoredAddOnName"] .. ": " .. L_GLOBALSTRINGS["Imported To Junker From List Text"], count))
 	elseif cmd == L_GLOBALSTRINGS["Command.Soulbind"] and arg1 ~= nil then
-		-- This command should only be available to the Author's characters.
-		if not addonTable.GUID[UnitGUID("player")] then return end
-		
-		-- Create an empty table we'll use for sorting the data
-		-- later.
-		--
-		-- Get the node data for the provided soulbind (via ID).
-		-- Soulbinds can be found @ https://wow.tools/dbc/?dbc=soulbind
-		local sortingTbl = {}
-		local nodes = C_Soulbinds.GetTree(arg1).nodes
-		
-		-- Add the node data, the ID and the corresponding row,
-		-- to the sorting table in a contiguous format.
-		for _, node in ipairs(nodes) do
-			table.insert(sortingTbl, { node.ID, node.row })
-		end
-		
-		-- Sort the sorting table in a descending order (high to low).
-		-- The sort is based on the row number.
-		table.sort(sortingTbl, function(a, b) return a[2] < b[2] end)
-		
-		-- Print the sorting table.
-		for _, node in ipairs(sortingTbl) do
-			print(node[1] .. ": " .. node[2])
+		if HelpMePlayDB.DevModeEnabled then
+			-- Create an empty table we'll use for sorting the data
+			-- later.
+			--
+			-- Get the node data for the provided soulbind (via ID).
+			-- Soulbinds can be found @ https://wow.tools/dbc/?dbc=soulbind
+			local sortingTbl = {}
+			local nodes = C_Soulbinds.GetTree(arg1).nodes
+			
+			-- Add the node data, the ID and the corresponding row,
+			-- to the sorting table in a contiguous format.
+			for _, node in ipairs(nodes) do
+				table.insert(sortingTbl, { node.ID, node.row })
+			end
+			
+			-- Sort the sorting table in a descending order (high to low).
+			-- The sort is based on the row number.
+			table.sort(sortingTbl, function(a, b) return a[2] < b[2] end)
+			
+			-- Print the sorting table.
+			for _, node in ipairs(sortingTbl) do
+				print(node[1] .. ": " .. node[2])
+			end
 		end
 	elseif cmd == L_GLOBALSTRINGS["Command.Conduit"] and arg1 ~= nil then
 		--[[
@@ -148,42 +147,39 @@ function HelpMePlay:SlashCommandHandler(cmd)
 			1: Potency
 			2: Endurance
 		]]--
-	
-		-- This command should only be available to the Author's characters.
-		if not addonTable.GUID[UnitGUID("player")] then return end
-		
-		-- Create an empty table we'll use for sorting the data
-		-- later.
-		--
-		-- Get the conduit data for the provided conduit type (via ID).
-		--
-		-- Add the node data, the ID and the corresponding row,
-		-- to the sorting table in a contiguous format.
-		local sortingTbl = {}
-		local conduits = C_Soulbinds.GetConduitCollection(arg1)
-		for _, conduit in ipairs(conduits) do
-			table.insert(sortingTbl, { conduit.conduitID, conduit.conduitItemID })
-		end
-		
-		-- Sort the sorting table in a descending order (high to low).
-		-- The sort is based on the conduit item ID.
-		table.sort(sortingTbl, function(a, b) return a[2] < b[2] end)
-		
-		-- Print the sorting table.
-		for _, conduit in ipairs(sortingTbl) do
-			print(conduit[2] .. ": " .. conduit[1])
+		if HelpMePlayDB.DevModeEnabled then
+			-- Create an empty table we'll use for sorting the data
+			-- later.
+			--
+			-- Get the conduit data for the provided conduit type (via ID).
+			--
+			-- Add the node data, the ID and the corresponding row,
+			-- to the sorting table in a contiguous format.
+			local sortingTbl = {}
+			local conduits = C_Soulbinds.GetConduitCollection(arg1)
+			for _, conduit in ipairs(conduits) do
+				table.insert(sortingTbl, { conduit.conduitID, conduit.conduitItemID })
+			end
+			
+			-- Sort the sorting table in a descending order (high to low).
+			-- The sort is based on the conduit item ID.
+			table.sort(sortingTbl, function(a, b) return a[2] < b[2] end)
+			
+			-- Print the sorting table.
+			for _, conduit in ipairs(sortingTbl) do
+				print(conduit[2] .. ": " .. conduit[1])
+			end
 		end
 	elseif cmd == L_GLOBALSTRINGS["Command.Taxi"] and arg1 ~= nil then
-		-- This command should only be available to the Author's characters.
-		if not addonTable.GUID[UnitGUID("player")] then return end
-		
-		-- The flight map frame must be visible.
-		if not FlightMapFrame:IsVisible() then return end
-		
-		for _, taxiNodeData in ipairs(C_TaxiMap.GetAllTaxiNodes(GetTaxiMapID())) do
-			local dest, zone = string.split(",", taxiNodeData.name)
-			if string.find(string.lower(dest), string.lower(arg1)) then
-				print(dest .. "," .. zone .. ": " .. taxiNodeData.nodeID)
+		if HelpMePlayDB.DevModeEnabled then
+			-- The flight map frame must be visible.
+			if not FlightMapFrame:IsVisible() then return end
+			
+			for _, taxiNodeData in ipairs(C_TaxiMap.GetAllTaxiNodes(GetTaxiMapID())) do
+				local dest, zone = string.split(",", taxiNodeData.name)
+				if string.find(string.lower(dest), string.lower(arg1)) then
+					print(dest .. "," .. zone .. ": " .. taxiNodeData.nodeID)
+				end
 			end
 		end
 	elseif cmd == L_GLOBALSTRINGS["Help Command"] then
