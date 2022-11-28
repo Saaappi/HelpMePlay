c9484065576d029e9ecfc224f8ff17de4998fbd2
Saaappi <alex.metz1024@gmail.com>
2022-11-27 23:45:02 -0800

    Finalized the Groups automation.

--- Automations/Groups.lua
+++ Automations/Groups.lua
@@ -7,5 +7,7 @@ e:SetScript("OnEvent", function(self, event, ...)
 	if event == "PARTY_INVITE_REQUEST" then
 		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 		if HelpMePlayDB.GroupsEnabled == false or HelpMePlayDB.GroupsEnabled == nil then return false end
+		
+		StaticPopup1Button1:Click("LeftButton")
 	end
 end)
