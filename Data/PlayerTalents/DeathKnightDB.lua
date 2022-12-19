e6cd365434e76ae87717d1444a3dded1d3fb349d
Saaappi <alex.metz1024@gmail.com>
2022-12-18 16:51:14 -0800

    Added Frost DK talents.

--- Data/PlayerTalents/DeathKnightDB.lua
+++ Data/PlayerTalents/DeathKnightDB.lua
@@ -249,6 +249,250 @@ local DEATH_KNIGHT_TALENTS = {
 		},
 	},
 	[251] = { -- Frost
+		{ -- Death Strike
+			["n"] = 76071,
+			["e"] = 96200,
+		},
+		{ -- Frost Strike
+			["n"] = 76115,
+			["e"] = 96245,
+		},
+		{ -- Raise Dead
+			["n"] = 76072,
+			["e"] = 96201,
+		},
+		{ -- Obliterate
+			["n"] = 76116,
+			["e"] = 96246,
+		},
+		{ -- Mind Freeze
+			["n"] = 76082,
+			["e"] = 96211,
+		},
+		{ -- Howling Blast
+			["n"] = 76114,
+			["e"] = 96244,
+		},
+		{ -- Cleaving Strikes
+			["n"] = 76073,
+			["e"] = 96202,
+		},
+		{ -- Killing Machine
+			["n"] = 76117,
+			["e"] = 96247,
+		},
+		{ -- Anti-Magic Shell
+			["n"] = 76070,
+			["e"] = 96199,
+		},
+		{ -- Rime
+			["n"] = 76113,
+			["e"] = 96243,
+		},
+		{ -- Blinding Sleet
+			["n"] = 76044,
+			["e"] = 96172,
+		},
+		{ -- Unleashed Frenzy
+			["n"] = 76118,
+			["e"] = 96248,
+		},
+		{ -- Improved Death Strike
+			["n"] = 76067,
+			["e"] = 96196,
+		},
+		{ -- Improved Frost Strike
+			["n"] = 76103,
+			["e"] = 96233,
+		},
+		{ -- March of Darkness
+			["n"] = 76069,
+			["e"] = 96198,
+		},
+		{ -- Improved Frost Strike
+			["n"] = 76103,
+			["e"] = 96233,
+		},
+		{ -- Enfeeble
+			["n"] = 76060,
+			["e"] = 96189,
+		},
+		{ -- Remorseless Winter
+			["n"] = 76112,
+			["e"] = 96242,
+		},
+		{ -- Icebound Fortitude
+			["n"] = 76084,
+			["e"] = 96213,
+		},
+		{ -- Improved Obliterate
+			["n"] = 76119,
+			["e"] = 96249,
+		},
+		{ -- Veteran of the Third War
+			["n"] = 76068,
+			["e"] = 96197,
+		},
+		{ -- Glacial Advance
+			["n"] = 76092,
+			["e"] = 96221,
+		},
+		{ -- Veteran of the Third War
+			["n"] = 76068,
+			["e"] = 96197,
+		},
+		{ -- Pillar of Frost
+			["n"] = 76104,
+			["e"] = 96234,
+		},
+		{ -- Brittle
+			["n"] = 76061,
+			["e"] = 96190,
+		},
+		{ -- Improved Rime
+			["n"] = 76111,
+			["e"] = 96241,
+		},
+		{ -- Merciless Strikes
+			["n"] = 76085,
+			["e"] = 96214,
+		},
+		{ -- Frigid Executioner
+			["n"] = 76120,
+			["e"] = 96251,
+		},
+		{ -- Might of Thassarian
+			["n"] = 76076,
+			["e"] = 96205,
+		},
+		{ -- Cold Heart
+			["n"] = 76035,
+			["e"] = 96163,
+		},
+		{ -- Proliferating Chill
+			["n"] = 76086,
+			["e"] = 96215,
+		},
+		{ -- Frostreaper
+			["n"] = 76089,
+			["e"] = 96218,
+		},
+		{ -- Gloom Ward
+			["n"] = 76052,
+			["e"] = 96180,
+		},
+		{ -- Frostwhelp's Aid
+			["n"] = 76106,
+			["e"] = 96236,
+		},
+		{ -- Death's Reach
+			["n"] = 76057,
+			["e"] = 96186,
+		},
+		{ -- Frostwhelp's Aid
+			["n"] = 76106,
+			["e"] = 96236,
+		},
+		{ -- Runic Attenuation
+			["n"] = 76087,
+			["e"] = 96216,
+		},
+		{ -- Biting Cold
+			["n"] = 76036,
+			["e"] = 96164,
+		},
+		{ -- Unholy Ground
+			["n"] = 76058,
+			["e"] = 96187,
+		},
+		{ -- Murderous Efficiency
+			["n"] = 76121,
+			["e"] = 96252,
+		},
+		{ -- Blood Draw
+			["n"] = 76079,
+			["e"] = 96208,
+		},
+		{ -- Inexorable Assault
+			["n"] = 76037,
+			["e"] = 96165,
+		},
+		{ -- Blood Draw
+			["n"] = 76079,
+			["e"] = 96208,
+		},
+		{ -- Icecap
+			["n"] = 76034,
+			["e"] = 96162,
+		},
+		{ -- Death's Echo
+			["n"] = 76056,
+			["e"] = 96184,
+		},
+		{ -- Gathering Storm
+			["n"] = 76109,
+			["e"] = 96239,
+		},
+		{ -- Icy Talons
+			["n"] = 76051,
+			["e"] = 96179,
+		},
+		{ -- Might of the Frozen Wastes
+			["n"] = 76090,
+			["e"] = 96219,
+		},
+		{ -- Icy Talons
+			["n"] = 76051,
+			["e"] = 96179,
+		},
+		{ -- Avalanche
+			["n"] = 76105,
+			["e"] = 96235,
+		},
+		{ -- Rune Mastery
+			["n"] = 76080,
+			["e"] = 96209,
+		},
+		{ -- Icebreaker
+			["n"] = 76033,
+			["e"] = 96161,
+		},
+		{ -- Rune Mastery
+			["n"] = 76080,
+			["e"] = 96209,
+		},
+		{ -- Icebreaker
+			["n"] = 76033,
+			["e"] = 96161,
+		},
+		{ -- Unholy Bond
+			["n"] = 76055,
+			["e"] = 96183,
+		},
+		{ -- Everfrost
+			["n"] = 76107,
+			["e"] = 96237,
+		},
+		{ -- Unholy Bond
+			["n"] = 76055,
+			["e"] = 96183,
+		},
+		{ -- Frostwyrm's Fury
+			["n"] = 76095,
+			["e"] = 96224,
+		},
+		{ -- Empower Rune Weapon
+			["n"] = 76099,
+			["e"] = 96229,
+		},
+		{ -- Absolute Zero
+			["n"] = 76094,
+			["e"] = 96223,
+		},
+		{ -- Abomination Limb
+			["n"] = 76049,
+			["e"] = 96177,
+		},
 	},
 	[252] = { -- Unholy
 	},
