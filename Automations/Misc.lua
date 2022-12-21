7f2be80f8fefaf9b59fcc6d1e3b9a0894ced7917
Saaappi <alex.metz1024@gmail.com>
2022-12-21 12:54:02 -0800

    When targeting a player, there's a random chance you'll wave at them.

---
+++ Automations/Misc.lua
@@ -0,0 +1,20 @@
+local addonName, addonTable = ...
+local e = CreateFrame("Frame")
+local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
+
+-- Random and miscellaneous automations.
+e:RegisterEvent("PLAYER_TARGET_CHANGED")
+e:SetScript("OnEvent", function(self, event, ...)
+	if event == "PLAYER_TARGET_CHANGED" then
+		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
+		
+		local randomNum = math.random(0, 1)
+		if randomNum == 1 then
+			local GUID = UnitGUID("target")
+			local type = string.split("-", GUID)
+			if type == "Player" then
+				DoEmote("wave", nil)
+			end
+		end
+	end
+end)
