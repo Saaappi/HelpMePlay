14f34f4ee7bab41a46888bc61c190465e82a4566
Saaappi <alex.metz1024@gmail.com>
2022-12-28 18:52:09 -0800

    Removed lots of unneeded slot ID code.

--- Data/ConstantsDB.lua
+++ Data/ConstantsDB.lua
@@ -6,30 +6,9 @@ local CONSTANTS = {
 	["HALF_SECOND"]				= 0.5,
 	["ONE_SECOND"]				= 1,
 	["FIVE_SECONDS"]			= 5,
-	-- Inventory Slot IDs
-	["INVTYPE_HEAD"] 			= 1,
-	["INVTYPE_NECK"] 			= 2,
-	["INVTYPE_SHOULDER"] 		= 3,
-	["INVTYPE_BODY"] 			= 4,
-	["INVTYPE_CHEST"] 			= 5,
-	["INVTYPE_WAIST"] 			= 6,
-	["INVTYPE_LEGS"] 			= 7,
-	["INVTYPE_FEET"] 			= 8,
-	["INVTYPE_WRIST"] 			= 9,
-	["INVTYPE_HAND"] 			= 10,
-	["INVTYPE_FINGER"] 			= 11, -- and 12
-	["INVTYPE_TRINKET"] 		= 13, -- and 14
-	["INVTYPE_CLOAK"] 			= 15,
-	["INVTYPE_WEAPON"] 			= 16, -- and 17
-	["INVTYPE_2HWEAPON"] 		= 16,
-	["INVTYPE_RANGEDRIGHT"] 	= 16,
-	["INVTYPE_RANGED"] 			= 16,
-	["INVTYPE_SHIELD"] 			= 17,
-	["INVTYPE_HOLDABLE"] 		= 17,
 	-- Levels
 	["MAX_PLAYER_LEVEL"] 		= 70,
 	["WAR_MODE_MAX_LEVEL"]		= 60,
-	["MAX_HEIRLOOM_LEVEL"]		= 59,
 	-- Dragonriding
 	["DRAGONRIDING_TREE_ID"] 	= 672,
 }
