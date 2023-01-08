29d3b7de9bf135c1e331f85892e51b3ad513d09f
Saaappi <alex.metz1024@gmail.com>
2023-01-07 20:52:10 -0800

    The addon should now be able to equip items the player loots from enemies.

---
+++ Automations/Loot.lua
@@ -0,0 +1,86 @@
+local addonName, addonTable = ...
+local e = CreateFrame("Frame")
+local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
+
+local function EquipItem(itemLink, inventoryType)
+	if UnitLevel("player") < addonTable.CONSTANTS["MAX_PLAYER_LEVEL"] then
+		if not UnitAffectingCombat("player") then
+			if itemLink then
+				local equipSlot = 0
+				local _, rewardItemID = string.split(":", itemLink); rewardItemID = tonumber(rewardItemID)
+				local lootItemLevel = GetDetailedItemLevelInfo(itemLink)
+				C_Timer.After(addonTable.CONSTANTS["HALF_SECOND"], function()
+					local equippedItem = 0
+					local equippedItemQuality = 0
+					if type(addonTable.SLOTS[inventoryType]) == "table" then
+						for _, invSlotID in ipairs(addonTable.SLOTS[inventoryType]) do
+							equippedItem = ItemLocation:CreateFromEquipmentSlot(invSlotID)
+							if equippedItem:IsValid() then
+								local equippedItemLevel = C_Item.GetCurrentItemLevel(equippedItem)
+								equippedItemQuality = C_Item.GetItemQuality(equippedItem)
+								if lootItemLevel > equippedItemLevel then
+									equipSlot = invSlotID
+								end
+							end
+						end
+					else
+						equippedItem = ItemLocation:CreateFromEquipmentSlot(addonTable.SLOTS[inventoryType])
+						if equippedItem:IsValid() then
+							local equippedItemLevel = C_Item.GetCurrentItemLevel(equippedItem)
+							equippedItemQuality = C_Item.GetItemQuality(equippedItem)
+							if lootItemLevel > equippedItemLevel then
+								equipSlot = addonTable.SLOTS[inventoryType]
+							end
+						end
+					end
+					
+					if equipSlot > 0 then
+						-- If the item would replace an heirloom, and the player's level
+						-- is lower than 60, then don't equip it.
+						if equippedItemQuality == 7 and UnitLevel("player") <= addonTable.CONSTANTS["MAX_HEIRLOOM_LEVEL"] then return end
+						C_Timer.After(addonTable.CONSTANTS["HALF_SECOND"], function()
+							for bagID = 0, 4 do
+								for slotID = 1, C_Container.GetContainerNumSlots(bagID) do
+									local containerItemInfo = C_Container.GetContainerItemInfo(bagID, slotID)
+									if containerItemInfo then
+										local containerItemLink = C_Item.GetItemLink(ItemLocation:CreateFromBagAndSlot(bagID, slotID))
+										if containerItemInfo.itemID == rewardItemID then
+											local containerItemIcon = C_Item.GetItemIcon(ItemLocation:CreateFromBagAndSlot(bagID, slotID))
+											print(string.format("%s: %s |T%s:0|t %s", L_GLOBALSTRINGS["Text.Output.ColoredAddOnName"], L_GLOBALSTRINGS["Text.Output.EquipItemUpgrade"], containerItemIcon, containerItemLink))
+											ClearCursor()
+											C_Container.PickupContainerItem(bagID, slotID)
+											EquipCursorItem(equipSlot)
+											if HelpMePlayDB.JunkerEnabled then
+												HelpMePlayJunkerGlobalDB[containerItemInfo.itemID] = true
+											end
+											break
+										end
+									end
+								end
+							end
+						end)
+					end
+				end)
+			end
+		else
+			EquipItem(itemLink)
+		end
+	end
+end
+
+e:RegisterEvent("CHAT_MSG_LOOT")
+e:SetScript("OnEvent", function(self, event, ...)
+	if event == "CHAT_MSG_LOOT" then
+		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
+		if HelpMePlayDB.AutoEquipQuestRewardsEnabled == false or HelpMePlayDB.AutoEquipQuestRewardsEnabled == nil then return false end
+		
+		local text, _, _, _, _, _, _, _, _, _, _, GUID = ...
+		if GUID == UnitGUID("player") then
+			local itemLink = string.match(text,"|%x+|Hitem:.-|h.-|h|r")
+			local inventoryType = C_Item.GetItemInventoryTypeByID(itemLink)
+			if inventoryType > 0 then
+				EquipItem(itemLink, inventoryType)
+			end
+		end
+	end
+end)
--- Data/ConstantsDB.lua
+++ Data/ConstantsDB.lua
@@ -12,5 +12,29 @@ local CONSTANTS = {
 	["MAX_HEIRLOOM_LEVEL"]		= 59,
 	-- Dragonriding
 	["DRAGONRIDING_TREE_ID"] 	= 672,
+	["SLOTS"] 					= {
+		[1] 	= 1, 			-- Head
+		[2] 	= 2, 			-- Neck
+		[3] 	= 3, 			-- Shoulder
+		[4] 	= 4, 			-- Shirt
+		[5] 	= 5, 			-- Chest
+		[6] 	= 6, 			-- Waist
+		[7] 	= 7, 			-- Legs
+		[8] 	= 8, 			-- Feet
+		[9] 	= 9, 			-- Wrist
+		[10] 	= 10, 			-- Hands
+		[11] 	= { 11, 12 },	-- Finger
+		[12] 	= { 13, 14 },	-- Trinket
+		[13] 	= { 16, 17 }, 	-- One-Handed Weapon
+		[14] 	= 17, 			-- Off-Hand (Shield)
+		[15] 	= 16, 			-- Ranged (Bow, Crossbow, Gun, etc.)
+		[16] 	= 15, 			-- Back
+		[17] 	= { 16, 17 }, 	-- Two-Handed Weapon
+		[20] 	= 5, 			-- Robe (Chest)
+		[21] 	= 16, 			-- Main-Hand Weapon
+		[22] 	= 17, 			-- Off-Hand Weapon
+		[23] 	= 17, 			-- Holdable
+		[26] 	= 16, 			-- Ranged Right Weapon
+	},
 }
 addonTable.CONSTANTS = CONSTANTS
