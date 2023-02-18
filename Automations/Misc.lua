313fa6b3b73f22cf3dc24fa8f67d9fd92592b67f
Saaappi <alex.metz1024@gmail.com>
2023-02-18 13:02:56 -0800

    More updates!

--- Automations/Misc.lua
+++ Automations/Misc.lua
@@ -4,17 +4,25 @@ local L_GLOBALSTRINGS = addonTable.L_GLOBALSTRINGS
 
 -- Random and miscellaneous automations.
 e:RegisterEvent("CHAT_MSG_WHISPER")
+e:RegisterEvent("CHAT_MSG_BN_WHISPER")
 e:RegisterEvent("PLAYER_TARGET_CHANGED")
 e:SetScript("OnEvent", function(self, event, ...)
-	if event == "CHAT_MSG_WHISPER" then
+	if event == "CHAT_MSG_WHISPER" or event == "CHAT_MSG_BN_WHISPER" then
 		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 		if HelpMePlayDB.AutoInviteEnabled == false or HelpMePlayDB.AutoInviteEnabled == nil then return false end
 		
-		local msg, player = ...
+		local msg, player, _, _, _, _, _, _, _, _, _, _, bnSenderID = ...
 		msg = string.lower(msg)
 		if msg == HelpMePlayDB.InviteKeyword or (msg == "invite" or msg == "inv") then
 			if C_PartyInfo.CanInvite() then
 				if C_PartyInfo.IsPartyFull() == false then
+					if bnSenderID ~= nil and bnSenderID ~= "" then
+						local accountInfo = C_BattleNet.GetAccountInfoByID(bnSenderID)
+						if accountInfo then
+							local gameAccountInfo = accountInfo.gameAccountInfo
+							player = gameAccountInfo.characterName .. "-" .. gameAccountInfo.realmName
+						end
+					end
 					C_PartyInfo.InviteUnit(player)
 				end
 			end
