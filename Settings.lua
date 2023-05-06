48263b0b9d61cb833ae4410860d59f63a05c26e8
Saaappi <alex.metz1024@gmail.com>
2023-05-05 17:24:05 -0700

    .

--- Settings.lua
+++ Settings.lua
@@ -3,16 +3,22 @@ local coloredDash = "|cffFFD100-|r "
 local icon = ""
 --local authorNote = "|cff009AE4"
 
+local function ItemInfo(itemID)
+	local info = GetItemInfo(itemID)
+	if (info) then
+		return info
+	else
+		C_Timer.After(0.10, function()
+			ItemInfo(itemID)
+		end)
+	end
+end
+
 local function StartsWith(text, prefix)
 	return text:find(prefix, 1, true) == 1
 end
 
 local function StringToTable(str, delimiter)
-	-- Create an empty table.
-	--
-	-- Split the string by the delimiter, then
-	-- insert each value into the table, then
-	-- return the table to the caller.
 	local tbl = {}
 	for match in (str .. delimiter):gmatch("(.-)" .. delimiter) do
 		if tonumber(match) == nil then
@@ -2124,6 +2130,43 @@ local settings = {
 						HelpMePlayDB.purchaseQuantity = 1
 						HelpMePlayDB.merchantQuestID = 0
 					end,
+				},
+				MerchantItemsDropdown = {
+					name = "Merchant Items:",
+					order = 16,
+					desc = "Select an item from the dropdown to remove it from the table.",
+					type = "select",
+					style = "dropdown",
+					values = function()
+						local items = {}
+						for merchantID,info in pairs(HelpMePlayDB.PlayerDB.Merchants) do
+							--[[if (info) and (info ~= {}) then
+								local itemName = ItemInfo(info.itemID)
+								local _, _, _, _, itemIcon = GetItemInfoInstant(info.itemID)
+								if (itemName) then
+									items[merchantID] = "|T"..itemIcon..":0|t "..itemName
+								end
+							else]]
+								local values = {
+									[0] = "<add items first...>",
+								}
+								return values
+							--end
+						end
+						return items
+					end,
+					--[[get = function()
+						return HelpMePlayDB.PlayerDB.Merchants
+					end,]]
+					--[[set = function(_, rarityID)
+						HelpMePlayDB.RarityID = rarityID
+						if rarityID == 5 then
+							if HelpMePlayDB.JunkerSoulboundModeEnabled then
+								-- Disable Soulbound Mode since the player chose the Preserve Transmog option.
+								HelpMePlayDB.JunkerSoulboundModeEnabled = false
+							end
+						end
+					end,]]
 				},
             },
         },
