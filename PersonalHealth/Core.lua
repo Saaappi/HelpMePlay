489833a08e6b054c63f3d19c9425db0204a7a1b6
Saaappi <alex.metz1024@gmail.com>
2023-07-24 17:23:41 -0700

    Syntax and performance improvements for the PersonalHealth module.

--- PersonalHealth/Core.lua
+++ PersonalHealth/Core.lua
@@ -1,11 +1,11 @@
 local addonName, addon = ...
-local e = CreateFrame("Frame")
+local events = CreateFrame("Frame")
 
 local function WaterReminder()
 	if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 	if HelpMePlayDB.WaterReminderEnabled == false or HelpMePlayDB.WaterReminderEnabled == nil then return false end
 		
-	local interval = (math.random(HelpMePlayDB.WaterReminderMinDelay, HelpMePlayDB.WaterReminderMaxDelay))*60
+	local interval = (math.random(HelpMePlayDB.WaterReminderMinDelay, HelpMePlayDB.WaterReminderMaxDelay)) * 60
 	C_Timer.After(interval, function()
 		UIErrorsFrame:SetTimeVisible(5)
 		UIErrorsFrame:AddMessage("|cffFFD100" .. "|T463570:0|t Hey, listen! You should take a sip of water! |T463570:0|t" .. "|r", 1, 0, 0, nil)
@@ -14,9 +14,11 @@ local function WaterReminder()
 	end)
 end
 
-e:RegisterEvent("PLAYER_ENTERING_WORLD")
-e:SetScript("OnEvent", function(self, event, ...)
-	if event == "PLAYER_ENTERING_WORLD" then
+events:RegisterEvent("PLAYER_ENTERING_WORLD")
+events:SetScript("OnEvent", function(self, event, ...)
+	if ( event == "PLAYER_ENTERING_WORLD" ) then
+	    events:UnregisterEvent("PLAYER_ENTERING_WORLD")
+
 		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 		if HelpMePlayDB.WaterReminderEnabled == false or HelpMePlayDB.WaterReminderEnabled == nil then return false end
 		
