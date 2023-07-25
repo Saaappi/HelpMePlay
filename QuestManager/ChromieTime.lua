40c17c1bdb4e678f91e7d13d7798cab0fffdb6f9
Saaappi <alex.metz1024@gmail.com>
2023-07-24 18:08:30 -0700

    Syntax and performance update for QuestManager\ChromieTime module.

--- QuestManager/ChromieTime.lua
+++ QuestManager/ChromieTime.lua
@@ -1,22 +1,30 @@
 local addonName, addon = ...
-local e = CreateFrame("Frame")
+local events = CreateFrame("Frame")
 
-e:RegisterEvent("PLAYER_INTERACTION_MANAGER_FRAME_SHOW")
-e:SetScript("OnEvent", function(self, event, ...)
-	if event == "PLAYER_INTERACTION_MANAGER_FRAME_SHOW" then
+events:RegisterEvent("PLAYER_INTERACTION_MANAGER_FRAME_SHOW")
+events:SetScript("OnEvent", function(self, event, ...)
+	if ( event == "PLAYER_INTERACTION_MANAGER_FRAME_SHOW" ) then
 		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 		if HelpMePlayDB.ChromieTimeID == 99 or HelpMePlayDB.ChromieTimeID == nil then return false end
 		
 		local type = ...
-		if type == 45 then
+		local chromieTimeID = UnitChromieTimeID("player")
+		local playerLevel = UnitLevel("player")
+		if ( type == 45 ) then
 			if IsShiftKeyDown() == false then
-				local playerLevel = UnitLevel("player")
-				if playerLevel < addon.CONSTANTS["CHROMIE_TIME_MAX_LEVEL"] then
-					if UnitChromieTimeID("player") ~= HelpMePlayDB.ChromieTimeID then
+				if ( playerLevel < addon.CONSTANTS["CHROMIE_TIME_MAX_LEVEL"] ) then
+					if ( chromieTimeID ~= HelpMePlayDB.ChromieTimeID ) then
 						C_ChromieTime.SelectChromieTimeOption(HelpMePlayDB.ChromieTimeID)
+						events:UnregisterEvent("PLAYER_INTERACTION_MANAGER_FRAME_SHOW")
 					end
 				end
 			end
+		end
+
+		if ( chromieTimeID == HelpMePlayDB.ChromieTimeID or playerLevel >= addon.CONSTANTS["CHROMIE_TIME_MAX_LEVEL"] ) then
+		    if ( events:IsEventRegistered("PLAYER_INTERACTION_MANAGER_FRAME_SHOW") ) then
+		        events:UnregisterEvent("PLAYER_INTERACTION_MANAGER_FRAME_SHOW")
+		    end
 		end
 	end
 end)
