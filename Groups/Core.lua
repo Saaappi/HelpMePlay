9ae59fd2ba897ef4d5971c9a0a9502a0f8be7718
Saaappi <alex.metz1024@gmail.com>
2023-07-24 17:11:41 -0700

    Syntax update for the Groups module.

--- Groups/Core.lua
+++ Groups/Core.lua
@@ -1,9 +1,9 @@
 local addonName, addon = ...
-local e = CreateFrame("Frame")
+local events = CreateFrame("Frame")
 
-e:RegisterEvent("PARTY_INVITE_REQUEST")
-e:SetScript("OnEvent", function(self, event, ...)
-	if event == "PARTY_INVITE_REQUEST" then
+events:RegisterEvent("PARTY_INVITE_REQUEST")
+events:SetScript("OnEvent", function(self, event, ...)
+	if ( event == "PARTY_INVITE_REQUEST" ) then
 		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 		if HelpMePlayDB.GroupsEnabled == false or HelpMePlayDB.GroupsEnabled == nil then return false end
 		
