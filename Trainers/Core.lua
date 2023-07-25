a9f662491df0294e18d2e82c83452c6f3e90e104
Saaappi <alex.metz1024@gmail.com>
2023-07-24 17:44:36 -0700

    Syntax update for the Trainers module.

--- Trainers/Core.lua
+++ Trainers/Core.lua
@@ -1,16 +1,17 @@
 local addonName, addon = ...
-local e = CreateFrame("Frame")
+local events = CreateFrame("Frame")
 
-e:RegisterEvent("TRAINER_SHOW")
-e:RegisterEvent("TRAINER_UPDATE")
-e:SetScript("OnEvent", function(self, event, ...)
-	if event == "TRAINER_SHOW" or event == "TRAINER_UPDATE" then
+events:RegisterEvent("TRAINER_SHOW")
+events:RegisterEvent("TRAINER_UPDATE")
+events:SetScript("OnEvent", function(self, event, ...)
+	if ( event == "TRAINER_SHOW" or event == "TRAINER_UPDATE" ) then
 		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 		if HelpMePlayDB.TrainersEnabled == false or HelpMePlayDB.TrainersEnabled == nil then return false end
+
 		local numTrainerServices = GetNumTrainerServices()
-		if numTrainerServices > 0 then
+		if ( numTrainerServices > 0 ) then
 			for i = 1, numTrainerServices do
-				if GetMoney() > (tonumber(GetTrainerServiceCost(i) + HelpMePlayDB.TrainersMinAmount)) then
+				if ( GetMoney() > (tonumber(GetTrainerServiceCost(i) + HelpMePlayDB.TrainersMinAmount)) ) then
 					BuyTrainerService(i)
 				end
 			end
