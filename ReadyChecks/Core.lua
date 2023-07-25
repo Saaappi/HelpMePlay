626e80012589ec77b718549b212fdb1477a56514
Saaappi <alex.metz1024@gmail.com>
2023-07-24 17:24:24 -0700

    Syntax update for the ReadyChecks module.

--- ReadyChecks/Core.lua
+++ ReadyChecks/Core.lua
@@ -1,13 +1,13 @@
 local addonName, addon = ...
-local e = CreateFrame("Frame")
+local events = CreateFrame("Frame")
 
-e:RegisterEvent("READY_CHECK")
-e:SetScript("OnEvent", function(self, event, ...)
-	if event == "READY_CHECK" then
+events:RegisterEvent("READY_CHECK")
+events:SetScript("OnEvent", function(self, event, ...)
+	if ( event == "READY_CHECK" ) then
 		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 		if HelpMePlayDB.ReadyChecksEnabled == false or HelpMePlayDB.ReadyChecksEnabled == nil then return false end
 		
-		if HelpMePlayDB.ReadyChecksEnabled then
+		if ( HelpMePlayDB.ReadyChecksEnabled ) then
 			ConfirmReadyCheck(1)
 		end
 	end
