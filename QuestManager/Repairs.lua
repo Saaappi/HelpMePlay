b89678e4edc162a37574864f67b7ea390c4f8c31
Saaappi <alex.metz1024@gmail.com>
2023-07-24 18:14:30 -0700

    Syntax and performance update for the QuestManager\Vehicles module.

--- QuestManager/Repairs.lua
+++ QuestManager/Repairs.lua
@@ -1,23 +1,15 @@
 local addonName, addon = ...
-local e = CreateFrame("Frame")
+local events = CreateFrame("Frame")
 
---[[
-	Description:
-		If the player needs to repair, has the money to
-		repair, and the merchant can repair the player's
-		equipment, then automatically repair, assuming
-		the Merchants automation is enabled.
-		
-		This automation will *NEVER* use guild repairs!
-]]--
-e:RegisterEvent("MERCHANT_SHOW")
-e:SetScript("OnEvent", function(self, event, ...)
-	if event == "MERCHANT_SHOW" then
+events:RegisterEvent("MERCHANT_SHOW")
+events:SetScript("OnEvent", function(self, event, ...)
+	if ( event == "MERCHANT_SHOW" ) then
 		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 		if HelpMePlayDB.RepairsEnabled == false or HelpMePlayDB.RepairsEnabled == nil then return false end
+
 		local repairCost, needsRepair = GetRepairAllCost()
-		if repairCost > 0 and GetMoney() > repairCost then
-			if CanMerchantRepair() then
+		if ( repairCost > 0 and GetMoney() > repairCost ) then
+			if ( CanMerchantRepair() ) then
 				RepairAllItems(false)
 			end
 		end
--- QuestManager/Vehicles.lua
+++ QuestManager/Vehicles.lua
@@ -1,25 +1,23 @@
 local addonName, addon = ...
-local e = CreateFrame("Frame")
+local events = CreateFrame("Frame")
 
---[[
-	Description:
-		If the vehicle the player enters is a registered
-		vehicle, then the addon will automatically exit
-		the vehicle.
-]]--
-e:RegisterEvent("UNIT_ENTERED_VEHICLE")
-e:SetScript("OnEvent", function(self, event, ...)
-	if event == "UNIT_ENTERED_VEHICLE" then
+events:RegisterEvent("UNIT_ENTERED_VEHICLE")
+events:SetScript("OnEvent", function(self, event, ...)
+	if ( event == "UNIT_ENTERED_VEHICLE" ) then
 		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
 		if HelpMePlayDB.VehiclesEnabled == false or HelpMePlayDB.VehiclesEnabled == nil then return false end
-		local _, _, _, _, guid = ...
-		if guid then
-			local _, _, _, _, _, npcId = strsplit("-", guid); npcId = tonumber(npcId)
-			if npcId then
+
+		local _, _, _, _, GUID = ...
+		if ( GUID ) then
+			local _, _, _, _, _, npcID = strsplit("-", guid); npcID = tonumber(npcID)
+			if ( npcID ) then
 				C_Timer.After(addon.CONSTANTS["HALF_SECOND"], function()
 					for _, id in ipairs(addon.VEHICLES) do
-						if id == npcId then
+						if ( id == npcID ) then
 							OverrideActionBarLeaveFrameLeaveButton:Click()
+							if ( events:IsEventRegistered("UNIT_ENTERED_VEHICLE") ) then
+							    events:UnregisterEvent("UNIT_ENTERED_VEHICLE")
+							end
 							break
 						end
 					end
