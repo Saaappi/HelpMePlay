baf5dcb4dbad4fea049904b710f6a07d1144090a
Saaappi <alex.metz1024@gmail.com>
2023-07-24 17:25:11 -0700

    Syntax update for the RoleCheck module.

--- RoleCheck/Core.lua
+++ RoleCheck/Core.lua
@@ -1,13 +1,12 @@
 local addonName, addon = ...
-local e = CreateFrame("Frame")
-local normalTexture = e:CreateTexture()
-local highlightTexture = e:CreateTexture()
+local events = CreateFrame("Frame")
 
-e:RegisterEvent("LFG_ROLE_CHECK_SHOW")
-e:SetScript("OnEvent", function(self, event, ...)
-	if event == "LFG_ROLE_CHECK_SHOW" then
+events:RegisterEvent("LFG_ROLE_CHECK_SHOW")
+events:SetScript("OnEvent", function(self, event, ...)
+	if ( event == "LFG_ROLE_CHECK_SHOW" ) then
 		if HelpMePlayDB.Enabled == false or HelpMePlayDB.Enabled == nil then return false end
-		if HelpMePlayDB.QueuesEnabled then
+
+		if ( HelpMePlayDB.QueuesEnabled ) then
 			LFDRoleCheckPopupAcceptButton:Click()
 		end
 	end
