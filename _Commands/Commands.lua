58aab7beed32eaae0aecaa329df6a4df3e05c63b
Saaappi <alex.metz1024@gmail.com>
2024-05-02 12:10:16 -0700

    Fixed the slash command names and changed the print statement for the help command.

--- _Commands/Commands.lua
+++ _Commands/Commands.lua
@@ -1,13 +1,13 @@
 local addonName, addon = ...
 
-SLASH_HelpMePlay21 = "/hmp2"
-SLASH_HelpMePlay22 = "/helpmeplay2"
+SLASH_HelpMePlay21 = "/hmp"
+SLASH_HelpMePlay22 = "/helpmeplay"
 SlashCmdList["HelpMePlay2"] = function(cmd)
 	local command, subcommand, arg1 = string.split(" ", cmd)
 	if not command or command == "" then
 		InterfaceOptionsFrame_OpenToCategory(addonName)
     elseif command == "help" then
-        print("har har")
+        HelpMePlay.Print("This command hasn't been implemented yet.")
     else
         HelpMePlay.Print("Unknown command detected. Use |cffFFD100help|r for assistance.")
 	end
