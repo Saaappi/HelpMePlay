07f451eba1e751748c4d8abb41e7f51575706ba6
Saaappi <alex.metz1024@gmail.com>
2024-05-04 21:28:29 -0700

    Chromie Time level constant is now dynamic too.

--- _Data/Constants.lua
+++ _Data/Constants.lua
@@ -1,7 +1,10 @@
 local addonName, addon = ...
 
 addon.Constants = {
-    ["CHROMIE_TIME_MAX_LEVEL"] = 60,
     ["PLAYER_MAX_LEVEL"] = GetMaxLevelForPlayerExpansion(),
     ["TIMER_DELAY"] = 0.1,
 }
+
+-- It's not possible to do forward references in Lua during table creation.
+-- So... let's create the Chromie Time max level entry after creation.
+addon.Constants["CHROMIE_TIME_MAX_LEVEL"] = addon.Constants["PLAYER_MAX_LEVEL"] - 10
