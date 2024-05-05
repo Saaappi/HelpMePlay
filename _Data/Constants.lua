d7676497951443eafc3ddf8e960891e6aa0f6bcf
Saaappi <alex.metz1024@gmail.com>
2024-05-04 21:25:15 -0700

    Using a function to get the max level for the current expansion now.

--- _Data/Constants.lua
+++ _Data/Constants.lua
@@ -2,6 +2,6 @@ local addonName, addon = ...
 
 addon.Constants = {
     ["CHROMIE_TIME_MAX_LEVEL"] = 60,
-    ["PLAYER_MAX_LEVEL"] = 70,
+    ["PLAYER_MAX_LEVEL"] = GetMaxLevelForPlayerExpansion(),
     ["TIMER_DELAY"] = 0.1,
 }
