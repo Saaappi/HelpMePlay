1045dc61c90e05f21303bd84596105753d9ccc35
Saaappi <alex.metz1024@gmail.com>
2024-05-17 13:41:20 -0700

    Changed Timewalking event icons.

--- WorldEvents/WorldEventsQueue.lua
+++ WorldEvents/WorldEventsQueue.lua
@@ -6,103 +6,6 @@ local rightChevron
 local activeEvents = {}
 local currentEventIndex = 1
 
-local worldEvents = {
-    [559] = {
-        ["name"] = "Timewalking: The Burning Crusade",
-        ["atlas"] = "ChromieTime-Portrait-Small-BurningCrusade",
-        ["dungeonQueueID"] = 744,
-        ["questID"] = 40168,
-        ["conditions"] = {
-            "QUEST_INCOMPLETE = 40168",
-        },
-    },
-    [562] = {
-        ["name"] = "Timewalking: Wrath of the Lich King",
-        ["atlas"] = "ChromieTime-Portrait-Small-WrathoftheLichKing",
-        ["dungeonQueueID"] = 995,
-        ["questID"] = 40173,
-        ["conditions"] = {
-            "QUEST_INCOMPLETE = 40173",
-        },
-    },
-    [587] = {
-        ["name"] = "Timewalking: Cataclysm",
-        ["atlas"] = "ChromieTime-Portrait-Small-Cataclysm",
-        ["dungeonQueueID"] = 1146,
-        ["questID"] = 40786,
-        ["conditions"] = {
-            "QUEST_INCOMPLETE = 40786",
-        },
-    },
-    [643] = {
-        ["name"] = "Timewalking: Mists of Pandaria",
-        ["atlas"] = "ChromieTime-Portrait-Small-MistsofPandaria",
-        ["dungeonQueueID"] = 1453,
-        ["questID"] = 45563,
-        ["conditions"] = {
-            "QUEST_INCOMPLETE = 45563",
-        },
-    },
-    [1056] = {
-        ["name"] = "Timewalking: Warlords of Draenor",
-        ["atlas"] = "ChromieTime-Portrait-Small-WarlordsofDraenor",
-        ["dungeonQueueID"] = 1971,
-        ["questID"] = 55499,
-        ["conditions"] = {
-            "QUEST_INCOMPLETE = 55499",
-        },
-    },
-    [1263] = {
-        ["name"] = "Timewalking: Legion",
-        ["atlas"] = "ChromieTime-Portrait-Small-Legion",
-        ["dungeonQueueID"] = 2274,
-        ["questID"] = 64710,
-        ["conditions"] = {
-            "QUEST_INCOMPLETE = 64710",
-        },
-    },
-    [423] = {
-        ["name"] = "Love is in the Air",
-        ["atlas"] = nil,
-        ["texture"] = 135450,
-        ["dungeonQueueID"] = 288,
-        ["questID"] = 79104,
-        ["conditions"] = {
-            "QUEST_INCOMPLETE = 79104",
-        },
-    },
-    [341] = {
-        ["name"] = "Midsummer Fire Festival",
-        ["atlas"] = nil,
-        ["texture"] = 135269,
-        ["dungeonQueueID"] = 286,
-        ["questID"] = 11976,
-        ["conditions"] = {
-            "QUEST_INCOMPLETE = 11976",
-        },
-    },
-    [372] = {
-        ["name"] = "Brewfest",
-        ["atlas"] = nil,
-        ["texture"] = 133201,
-        ["dungeonQueueID"] = 287,
-        ["questID"] = 77775,
-        ["conditions"] = {
-            "QUEST_INCOMPLETE = 77775",
-        },
-    },
-    [324] = {
-        ["name"] = "Hallow's End",
-        ["atlas"] = nil,
-        ["texture"] = 5222578,
-        ["dungeonQueueID"] = 285,
-        ["questID"] = 78474,
-        ["conditions"] = {
-            "QUEST_INCOMPLETE = 78474",
-        },
-    },
-}
-
 -- This function is used to update (refresh) the button when the player
 -- completes an active event.
 local function RefreshEvents()
@@ -111,7 +14,7 @@ local function RefreshEvents()
         HelpMePlay.Print("No more active events...")
     end
 
-    for eventID, event in pairs(activeEvents) do
+    for _, event in pairs(activeEvents) do
         if event.atlas then
             worldEventQueueButton.icon:SetAtlas(event.atlas)
         else
@@ -153,10 +56,10 @@ eventHandler:SetScript("OnEvent", function(self, event, ...)
         local events = addon.GetActiveEvents(currentDate)
         if next(events) ~= nil then
             for _, evt in ipairs(events) do
-                if worldEvents[evt.eventID] then
-                    local isWorldEventValid = HelpMePlay.EvalConditions(worldEvents[evt.eventID].conditions)
+                if addon.WorldEvents[evt.eventID] then
+                    local isWorldEventValid = HelpMePlay.EvalConditions(addon.WorldEvents[evt.eventID].conditions)
                     if isWorldEventValid then
-                        activeEvents[evt.eventID] = worldEvents[evt.eventID]
+                        activeEvents[evt.eventID] = addon.WorldEvents[evt.eventID]
                     end
                 end
             end
--- _Data/WorldEvents_Data.lua
+++ _Data/WorldEvents_Data.lua
@@ -8,3 +8,106 @@ addon.HolidayItems = {
     [117393] = true,    -- Keg-Shaped Treasure Chest
     [216874] = true,    -- Loot-Filled Basket
 }
+
+addon.WorldEvents = {
+    [559] = {
+        ["name"] = "Timewalking: The Burning Crusade",
+        ["atlas"] = nil,
+        ["dungeonQueueID"] = 744,
+        ["questID"] = 40168,
+        ["texture"] = 630783,
+        ["conditions"] = {
+            "QUEST_INCOMPLETE = 40168",
+        },
+    },
+    [562] = {
+        ["name"] = "Timewalking: Wrath of the Lich King",
+        ["atlas"] = nil,
+        ["dungeonQueueID"] = 995,
+        ["questID"] = 40173,
+        ["texture"] = 630787,
+        ["conditions"] = {
+            "QUEST_INCOMPLETE = 40173",
+        },
+    },
+    [587] = {
+        ["name"] = "Timewalking: Cataclysm",
+        ["atlas"] = nil,
+        ["dungeonQueueID"] = 1146,
+        ["questID"] = 40786,
+        ["texture"] = 630784,
+        ["conditions"] = {
+            "QUEST_INCOMPLETE = 40786",
+        },
+    },
+    [643] = {
+        ["name"] = "Timewalking: Mists of Pandaria",
+        ["atlas"] = nil,
+        ["dungeonQueueID"] = 1453,
+        ["questID"] = 45563,
+        ["texture"] = 630786,
+        ["conditions"] = {
+            "QUEST_INCOMPLETE = 45563",
+        },
+    },
+    [1056] = {
+        ["name"] = "Timewalking: Warlords of Draenor",
+        ["atlas"] = nil,
+        ["dungeonQueueID"] = 1971,
+        ["questID"] = 55499,
+        ["texture"] = 2838050,
+        ["conditions"] = {
+            "QUEST_INCOMPLETE = 55499",
+        },
+    },
+    [1263] = {
+        ["name"] = "Timewalking: Legion",
+        ["atlas"] = nil,
+        ["dungeonQueueID"] = 2274,
+        ["questID"] = 64710,
+        ["texture"] = 1408999,
+        ["conditions"] = {
+            "QUEST_INCOMPLETE = 64710",
+        },
+    },
+    [423] = {
+        ["name"] = "Love is in the Air",
+        ["atlas"] = nil,
+        ["texture"] = 135450,
+        ["dungeonQueueID"] = 288,
+        ["questID"] = 79104,
+        ["conditions"] = {
+            "QUEST_INCOMPLETE = 79104",
+        },
+    },
+    [341] = {
+        ["name"] = "Midsummer Fire Festival",
+        ["atlas"] = nil,
+        ["texture"] = 135269,
+        ["dungeonQueueID"] = 286,
+        ["questID"] = 11976,
+        ["conditions"] = {
+            "QUEST_INCOMPLETE = 11976",
+        },
+    },
+    [372] = {
+        ["name"] = "Brewfest",
+        ["atlas"] = nil,
+        ["texture"] = 133201,
+        ["dungeonQueueID"] = 287,
+        ["questID"] = 77775,
+        ["conditions"] = {
+            "QUEST_INCOMPLETE = 77775",
+        },
+    },
+    [324] = {
+        ["name"] = "Hallow's End",
+        ["atlas"] = nil,
+        ["texture"] = 5222578,
+        ["dungeonQueueID"] = 285,
+        ["questID"] = 78474,
+        ["conditions"] = {
+            "QUEST_INCOMPLETE = 78474",
+        },
+    },
+}
