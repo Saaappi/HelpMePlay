f660b5deaee2ee6985db44bc303d5beffd107e63
Saaappi <alex.metz1024@gmail.com>
2024-05-29 23:15:02 -0700

    EvalCondition is now able to evaluate multiple quests for QUEST_ACTIVE (and soon QUEST_INACTIVE) using conjunction such as AND/OR.

--- _Functions/EvalConditions.lua
+++ _Functions/EvalConditions.lua
@@ -2,165 +2,166 @@ local addonName, addon = ...
 local eventHandler = CreateFrame("Frame")
 
 eventHandler:RegisterEvent("ADDON_LOADED")
-eventHandler:SetScript("OnEvent", function(self, event, addonLoaded)
+eventHandler:SetScript("OnEvent", function(self, event, ...)
     if event == "ADDON_LOADED" then
-        function HelpMePlay.EvalConditions(conditions)
-            local numConditions = #conditions
-            for _, condition in ipairs(conditions) do
-                local cond, string = condition:match("^(%S+)%s*=%s*%d+%s*(%s*(AND|OR)%s*%d+%s*)*$")
-                if cond == "NONE" then
-                    numConditions = numConditions - 1
-                elseif cond == "QUEST_ACTIVE" then
-                    local quests = {}
-                    for questID in string:gmatch("%d+") do
-                        table.insert(quests, tonumber(questID))
-                    end
+        local addonLoaded = ...
+        if addonLoaded == addonName then
+            function HelpMePlay.EvalConditions(conditions)
+                local numConditions = #conditions
+                for _, condition in ipairs(conditions) do
+                    local cond, string = condition:match("(%w+_%w+) = (.+)")
+                    if cond == "NONE" then
+                        numConditions = numConditions - 1
+                    elseif cond == "QUEST_ACTIVE" then
+                        local quests = {}
+                        for questID in string:gmatch("%d+") do
+                            table.insert(quests, tonumber(questID))
+                        end
 
-                    print(string)
-
-                    local conjunction = string:match("%s(OR|AND)%s"); print(conjunction)
-                    local numQuests = #quests; print(numQuests)
-                    if conjunction == "AND" then
-                        -- If all the quests are active, then return true.
-                        for _, questID in ipairs(quests) do
-                            if C_QuestLog.IsOnQuest(questID) then
-                                numQuests = numQuests - 1
+                        local conjunction = string:match("%s(OR|AND)%s")
+                        local numQuests = #quests
+                        if conjunction == "AND" then
+                            -- If all the quests are active, then return true.
+                            for _, questID in ipairs(quests) do
+                                if C_QuestLog.IsOnQuest(questID) then
+                                    numQuests = numQuests - 1
+                                end
+                                if numQuests == 0 then
+                                    numConditions = numConditions - 1
+                                    break
+                                end
                             end
-                            if numQuests == 0 then
-                                numConditions = numConditions - 1
-                                break
-                            end
-                        end
-                        print(numConditions)
-                    else
-                        -- If any of the quests are active, then return true.
-                        for _, questID in ipairs(quests) do
-                            if C_QuestLog.IsOnQuest(questID) then
-                                numConditions = numConditions - 1
-                                break
+                            print(numConditions)
+                        else
+                            -- If any of the quests are active, then return true.
+                            for _, questID in ipairs(quests) do
+                                if C_QuestLog.IsOnQuest(questID) then
+                                    numConditions = numConditions - 1
+                                    break
+                                end
                             end
                         end
                     end
+                    --[[local operator, operand = condition:match("([%w_]+)%s*=%s*([%w_,]+)")
+                    if tonumber(operand) then
+                        operand = tonumber(operand, 10)
+                    end
+                    if operator == "NONE" then
+                        numConditions = numConditions - 1
+                    elseif operator == "QUEST_ACTIVE" then
+                        if C_QuestLog.IsOnQuest(operand) then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "QUEST_INACTIVE" then
+                        if not C_QuestLog.IsOnQuest(operand) then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "QUEST_COMPLETE" then
+                        if C_QuestLog.IsQuestFlaggedCompleted(operand) then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "QUEST_INCOMPLETE" then
+                        if not C_QuestLog.IsQuestFlaggedCompleted(operand) then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "QUEST_OBJECTIVE_COMPLETE" then
+                        operand = tostring(operand)
+                        local questID, index = string.split(",", operand); questID = tonumber(questID, 10); index = tonumber(index, 10)
+                        local objectives = C_QuestLog.GetQuestObjectives(questID)
+                        if objectives[index].finished then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "QUEST_OBJECTIVE_INCOMPLETE" then
+                        operand = tostring(operand)
+                        local questID, index = string.split(",", operand); questID = tonumber(questID, 10); index = tonumber(index, 10)
+                        local objectives = C_QuestLog.GetQuestObjectives(questID)
+                        if not objectives[index].finished then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "QUEST_OBJECTIVES_COMPLETE" then
+                        local numObjectives = C_QuestLog.GetNumQuestObjectives(operand)
+                        local objectives = C_QuestLog.GetQuestObjectives(operand)
+                        for _, objective in ipairs(objectives) do
+                            if objective.finished then
+                                numObjectives = numObjectives - 1
+                            end
+                        end
+                        if numObjectives == 0 then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "QUEST_OBJECTIVES_INCOMPLETE" then
+                        local numObjectives = C_QuestLog.GetNumQuestObjectives(operand)
+                        local objectives = C_QuestLog.GetQuestObjectives(operand)
+                        for _, objective in ipairs(objectives) do
+                            if not objective.finished then
+                                numObjectives = numObjectives - 1
+                            end
+                        end
+                        if numObjectives == 0 then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "LEVEL_LOWER" then
+                        if UnitLevel("player") < operand then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "LEVEL_LOWER_OR_EQUAL" then
+                        if UnitLevel("player") <= operand then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "LEVEL_EQUAL" then
+                        if UnitLevel("player") == operand then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "LEVEL_GREATER" then
+                        if UnitLevel("player") > operand then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "LEVEL_GREATER_OR_EQUAL" then
+                        if UnitLevel("player") >= operand then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "LEVEL_BETWEEN" then
+                        operand = tostring(operand)
+                        local playerLevel = UnitLevel("player")
+                        local lowerLevel, higherLevel = string.split(",", operand); lowerLevel = tonumber(lowerLevel, 10); higherLevel = tonumber(higherLevel, 10)
+                        if playerLevel >= lowerLevel and playerLevel <= higherLevel then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "CHROMIE_TIME_ACTIVE" then
+                        if UnitChromieTimeID("player") == operand then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "CHROMIE_TIME_INACTIVE" then
+                        if UnitChromieTimeID("player") ~= operand then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "WAR_MODE_ACTIVE" then
+                        -- Blizzard made me this way...
+                        local isEnlisted = false
+                        local spellID = C_UnitAuras.GetAuraDataBySpellName("player", "Enlisted", "HELPFUL").spellId
+                        if spellID == 269083 or spellID == 282559 then
+                            isEnlisted = true
+                        end
+                        if isEnlisted then
+                            numConditions = numConditions - 1
+                        end
+                    elseif operator == "WAR_MODE_INACTIVE" then
+                        -- Blizzard made me this way...
+                        local isEnlisted = false
+                        local spellID = C_UnitAuras.GetAuraDataBySpellName("player", "Enlisted", "HELPFUL").spellId
+                        if spellID == 269083 or spellID == 282559 then -- Enlisted
+                            isEnlisted = true
+                        end
+                        if not isEnlisted then
+                            numConditions = numConditions - 1
+                        end
+                    end]]
                 end
-                --[[local operator, operand = condition:match("([%w_]+)%s*=%s*([%w_,]+)")
-                if tonumber(operand) then
-                    operand = tonumber(operand, 10)
+                if numConditions == 0 then
+                    return true
                 end
-                if operator == "NONE" then
-                    numConditions = numConditions - 1
-                elseif operator == "QUEST_ACTIVE" then
-                    if C_QuestLog.IsOnQuest(operand) then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "QUEST_INACTIVE" then
-                    if not C_QuestLog.IsOnQuest(operand) then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "QUEST_COMPLETE" then
-                    if C_QuestLog.IsQuestFlaggedCompleted(operand) then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "QUEST_INCOMPLETE" then
-                    if not C_QuestLog.IsQuestFlaggedCompleted(operand) then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "QUEST_OBJECTIVE_COMPLETE" then
-                    operand = tostring(operand)
-                    local questID, index = string.split(",", operand); questID = tonumber(questID, 10); index = tonumber(index, 10)
-                    local objectives = C_QuestLog.GetQuestObjectives(questID)
-                    if objectives[index].finished then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "QUEST_OBJECTIVE_INCOMPLETE" then
-                    operand = tostring(operand)
-                    local questID, index = string.split(",", operand); questID = tonumber(questID, 10); index = tonumber(index, 10)
-                    local objectives = C_QuestLog.GetQuestObjectives(questID)
-                    if not objectives[index].finished then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "QUEST_OBJECTIVES_COMPLETE" then
-                    local numObjectives = C_QuestLog.GetNumQuestObjectives(operand)
-                    local objectives = C_QuestLog.GetQuestObjectives(operand)
-                    for _, objective in ipairs(objectives) do
-                        if objective.finished then
-                            numObjectives = numObjectives - 1
-                        end
-                    end
-                    if numObjectives == 0 then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "QUEST_OBJECTIVES_INCOMPLETE" then
-                    local numObjectives = C_QuestLog.GetNumQuestObjectives(operand)
-                    local objectives = C_QuestLog.GetQuestObjectives(operand)
-                    for _, objective in ipairs(objectives) do
-                        if not objective.finished then
-                            numObjectives = numObjectives - 1
-                        end
-                    end
-                    if numObjectives == 0 then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "LEVEL_LOWER" then
-                    if UnitLevel("player") < operand then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "LEVEL_LOWER_OR_EQUAL" then
-                    if UnitLevel("player") <= operand then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "LEVEL_EQUAL" then
-                    if UnitLevel("player") == operand then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "LEVEL_GREATER" then
-                    if UnitLevel("player") > operand then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "LEVEL_GREATER_OR_EQUAL" then
-                    if UnitLevel("player") >= operand then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "LEVEL_BETWEEN" then
-                    operand = tostring(operand)
-                    local playerLevel = UnitLevel("player")
-                    local lowerLevel, higherLevel = string.split(",", operand); lowerLevel = tonumber(lowerLevel, 10); higherLevel = tonumber(higherLevel, 10)
-                    if playerLevel >= lowerLevel and playerLevel <= higherLevel then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "CHROMIE_TIME_ACTIVE" then
-                    if UnitChromieTimeID("player") == operand then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "CHROMIE_TIME_INACTIVE" then
-                    if UnitChromieTimeID("player") ~= operand then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "WAR_MODE_ACTIVE" then
-                    -- Blizzard made me this way...
-                    local isEnlisted = false
-                    local spellID = C_UnitAuras.GetAuraDataBySpellName("player", "Enlisted", "HELPFUL").spellId
-                    if spellID == 269083 or spellID == 282559 then
-                        isEnlisted = true
-                    end
-                    if isEnlisted then
-                        numConditions = numConditions - 1
-                    end
-                elseif operator == "WAR_MODE_INACTIVE" then
-                    -- Blizzard made me this way...
-                    local isEnlisted = false
-                    local spellID = C_UnitAuras.GetAuraDataBySpellName("player", "Enlisted", "HELPFUL").spellId
-                    if spellID == 269083 or spellID == 282559 then -- Enlisted
-                        isEnlisted = true
-                    end
-                    if not isEnlisted then
-                        numConditions = numConditions - 1
-                    end
-                end]]
+                return false
             end
-            if numConditions == 0 then
-                return true
-            end
-            return false
         end
 
         -- Unload the event for performance.
