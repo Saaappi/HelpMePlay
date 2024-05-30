1f69c2d4374c8db1f187d2fb0b64e734562bf51c
Saaappi <alex.metz1024@gmail.com>
2024-05-29 20:31:53 -0700

    Added another gossip.

--- _Lib/LibHelpMePlay/Data/Gossips.lua
+++ _Lib/LibHelpMePlay/Data/Gossips.lua
@@ -4,15 +4,22 @@ if not LHMP then return end
 -- The first ID is an NPC ID, then nested are the gossip IDs
 -- and their condition(s).
 LHMP.Gossips = {
-    [0] = {
-        -- The 0 ID is used for any gossip associated with a non-creature.
-    },
+    [0] = {},
     [62202] = {
         {
             ID = 35586,
             CanConfirm = false,
             Conditions = {
-                "NONE = NONE",
+                "LEVEL_LOWER = 30",
+            }
+        }
+    },
+    [62538] = {
+        {
+            ID = 35587,
+            CanConfirm = false,
+            Conditions = {
+                "LEVEL_LOWER = 30",
             }
         }
     },
