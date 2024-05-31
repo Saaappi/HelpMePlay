e330dc3a4021cad495c48207619dbdd13163ead8
Saaappi <alex.metz1024@gmail.com>
2024-05-30 22:27:43 -0700

    Minified a couple more tables.

--- _Lib/LibHelpMePlay/Data/Gossips.lua
+++ _Lib/LibHelpMePlay/Data/Gossips.lua
@@ -1,9 +1,8 @@
 local LHMP = LibStub("LibHelpMePlay")
 if not LHMP then return end
 
--- The first ID is an NPC ID, then nested are the gossip IDs
--- and their condition(s).
-LHMP.Gossips = {
+LHMP.Gossips={[0]={},[62202]={{ID=35586,CanConfirm=false,Conditions={"QUEST_ACTIVE = 31006"}}},[62538]={{ID=35587,CanConfirm=true,Conditions={"QUEST_ACTIVE = 31007 OR 31660 OR 31175"}}},[63876]={{ID=28603,CanConfirm=false,Conditions={"QUEST_ACTIVE = 31091"}}},[62667]={{ID=35828,CanConfirm=false,Conditions={"QUEST_ACTIVE = 31129"}}},[62845]={{ID=35823,CanConfirm=false,Conditions={"QUEST_ACTIVE = 31129"}}},[62666]={{ID=35825,CanConfirm=false,Conditions={"QUEST_ACTIVE = 31129"}}},[62771]={{ID=40853,CanConfirm=false,Conditions={"QUEST_ACTIVE = 31084"}}},[64259]={{ID=40852,CanConfirm=false,Conditions={"QUEST_ACTIVE = 31189"}}}}
+--[[LHMP.Gossips = {
     [0] = {},
     [62202] = {
         {
@@ -77,4 +76,4 @@ LHMP.Gossips = {
             }
         }
     },
-}
+}]]
