d4b949db086d82c09f82fa298612faf3829ef848
Saaappi <alex.metz1024@gmail.com>
2024-05-30 19:23:00 -0700

    The heirloom button no longer shows for Remix characters.

--- Character/Heirloom.lua
+++ Character/Heirloom.lua
@@ -59,7 +59,7 @@ local function CreateHeirloom(classID)
 end
 
 addon.CreateHeirloomButton = function(classID)
-    if addon.playerLevel <= 10 and (not HelpMePlayDB_Character["UsedHeirloomButton"]) then
+    if addon.playerLevel <= 10 and (not HelpMePlayDB_Character["UsedHeirloomButton"]) and (PlayerGetTimerunningSeasonID() ~= 1) then
         if not heirloomButton and (#HelpMePlayDB["Heirlooms"][classID] > 0) then
             heirloomButton = CreateFrame("Button", addonName .. "HeirloomSecureButton", UIParent, "SecureActionButtonTemplate")
             heirloomButton:ClearAllPoints()
