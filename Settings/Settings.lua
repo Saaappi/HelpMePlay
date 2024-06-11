7cc62d85754262832ea1168fd68a98ec9ca45fa0
Saaappi <alex.metz1024@gmail.com>
2024-06-10 19:28:07 -0700

    Consolidated all General settings.

--- Settings/Settings.lua
+++ Settings/Settings.lua
@@ -1,18 +1,12 @@
 local addonName, addon = ...
 local eventHandler = CreateFrame("Frame")
+local LHMP = LibStub("LibHelpMePlay")
 
 -- Local references to Blizzard settings functions.
 local RegisterAddOnSetting = Settings.RegisterAddOnSetting
 local SetOnValueChangedCallback = Settings.SetOnValueChangedCallback
 local CreateCheckbox = Settings.CreateCheckbox or Settings.CreateCheckBox
 local CreateSlider = Settings.CreateSlider
-local function CreateSliderOptions(minValue, maxValue, rate)
-	local options = CreateFromMixins(SettingsSliderOptionsMixin)
-	options.minValue = minValue
-	options.maxValue = maxValue
-	options.steps = rate
-	return options
-end
 
 local function OnSettingChanged(_, setting, value)
     -- Get the variable name from the setting.
@@ -89,9 +83,6 @@ local author = C_AddOns.GetAddOnMetadata(addonName, "Author")
 local version = C_AddOns.GetAddOnMetadata(addonName, "Version")
 layout:AddInitializer(CreateSettingsListSectionHeaderInitializer(format("|cffFFD100Author:|r %s\n|cffFFD100Version:|r %s", author, version)))
 
--- Initialize a section for general stuff.
-layout:AddInitializer(CreateSettingsListSectionHeaderInitializer(GENERAL_SECTION))
-
 eventHandler:RegisterEvent("ADDON_LOADED")
 eventHandler:SetScript("OnEvent", function(self, event, ...)
     if event == "ADDON_LOADED" then
@@ -101,19 +92,128 @@ eventHandler:SetScript("OnEvent", function(self, event, ...)
                 ------------------------
                 -- GENERAL SECTION -----
                 ------------------------
-                -- Sort the table before we iterate through it.
-                table.sort(addon.Settings.General, Compare)
+                -- Initialize a section for general stuff.
+                layout:AddInitializer(CreateSettingsListSectionHeaderInitializer(GENERAL_SECTION))
 
-                -- Iterate through the now-sorted table and add them to
-                -- the addon's category.
-                for _, setting in ipairs(addon.Settings.General) do
-                    if setting.Type == "BasicButton" then
-                        addon.New(setting.Type, setting.Name, setting.ButtonText, setting.ClickHandler, setting.TooltipText, setting.AddSearchTags)
-                    elseif setting.Type == "CheckButton" then
-                        addon.New(setting.Type, setting.Name, category, setting.TooltipText, setting.SavedVariable)
-                    elseif setting.Type == "DropDown" or setting.Type == "Slider" then
-                        addon.New(setting.Type, setting.Name, category, setting.TooltipText, setting.Options, setting.SavedVariable)
-                    end
+                -- Chat Icons
+                do
+                    local variable = "ShowChatIcons"
+                    local name = "Chat Icons"
+                    local tooltipText = "Toggle to show item and appearance collection status icons in the chat frame."
+                    local setting = RegisterAddOnSetting(category, name, variable, type(HelpMePlayDB[variable]), HelpMePlayDB[variable])
+                    CreateCheckbox(category, setting, tooltipText)
+                    SetOnValueChangedCallback(variable, OnSettingChanged)
+                end
+
+                -- Emotes
+                do
+                    local variable = "UseEmotes"
+                    local name = "Emotes"
+                    local tooltipText = "Automatically use the appropriate emote against quest-related non-player characters."
+                    local setting = RegisterAddOnSetting(category, name, variable, type(HelpMePlayDB[variable]), HelpMePlayDB[variable])
+                    CreateCheckbox(category, setting, tooltipText)
+                    SetOnValueChangedCallback(variable, OnSettingChanged)
+                end
+
+                -- Event Queue
+                do
+                    local variable = "UseWorldEventQueue"
+                    local name = "Event Queue"
+                    local tooltipText = "Toggle to add a button to the top middle of your screen. This button can be used to quickly queue into ongoing events."
+                    local setting = RegisterAddOnSetting(category, name, variable, type(HelpMePlayDB[variable]), HelpMePlayDB[variable])
+                    CreateCheckbox(category, setting, tooltipText)
+                    SetOnValueChangedCallback(variable, OnSettingChanged)
+                end
+
+                -- Gossip
+                do
+                    local variable = "AcceptGossip"
+                    local name = "Gossip"
+                    local tooltipText = "Automatically select the appropriate quest-related gossip options from non-player characters."
+                    local setting = RegisterAddOnSetting(category, name, variable, type(HelpMePlayDB[variable]), HelpMePlayDB[variable])
+                    CreateCheckbox(category, setting, tooltipText)
+                    SetOnValueChangedCallback(variable, OnSettingChanged)
+                end
+
+                -- Mute Talking Head
+                do
+                    local variable = "MuteTalkingHead"
+                    local name = "Mute Talking Head"
+                    local tooltipText = "Toggle to hide and prevent the talking head popup."
+                    local setting = RegisterAddOnSetting(category, name, variable, type(HelpMePlayDB[variable]), HelpMePlayDB[variable])
+                    CreateCheckbox(category, setting, tooltipText)
+                    SetOnValueChangedCallback(variable, OnSettingChanged)
+                end
+
+                -- Open Containers
+                do
+                    local variable = "OpenContainers"
+                    local name = "Open Containers"
+                    local tooltipText = "Toggle to automatically open (supported) container items when you loot them."
+                    local setting = RegisterAddOnSetting(category, name, variable, type(HelpMePlayDB[variable]), HelpMePlayDB[variable])
+                    CreateCheckbox(category, setting, tooltipText)
+                    SetOnValueChangedCallback(variable, OnSettingChanged)
+                end
+
+                -- Rare Scan
+                do
+                    local variable = "RareScan"
+                    local name = "Rare Scan"
+                    local tooltipText = "Toggle if you wish to receive a popup notification and sound for nearby rares."
+                    local setting = RegisterAddOnSetting(category, name, variable, type(HelpMePlayDB[variable]), HelpMePlayDB[variable])
+                    CreateCheckbox(category, setting, tooltipText)
+                    SetOnValueChangedCallback(variable, OnSettingChanged)
+                end
+
+                -- Ready Checks
+                do
+                    local variable = "AcceptReadyChecks"
+                    local name = "Ready Checks"
+                    local tooltipText = "Toggle to automatically accept ready checks."
+                    local setting = RegisterAddOnSetting(category, name, variable, type(HelpMePlayDB[variable]), HelpMePlayDB[variable])
+                    CreateCheckbox(category, setting, tooltipText)
+                    SetOnValueChangedCallback(variable, OnSettingChanged)
+                end
+
+                -- Role Checks
+                do
+                    local variable = "AcceptRoleChecks"
+                    local name = "Role Checks"
+                    local tooltipText = "Toggle to automatically accept role checks."
+                    local setting = RegisterAddOnSetting(category, name, variable, type(HelpMePlayDB[variable]), HelpMePlayDB[variable])
+                    CreateCheckbox(category, setting, tooltipText)
+                    SetOnValueChangedCallback(variable, OnSettingChanged)
+                end
+
+                -- Skip Cutscenes
+                do
+                    local variable = "SkipCutscenes"
+                    local name = "Skip Cutscenes"
+                    local tooltipText = "Toggle skipping cutscenes and in-game movies."
+                    local setting = RegisterAddOnSetting(category, name, variable, type(HelpMePlayDB[variable]), HelpMePlayDB[variable])
+                    CreateCheckbox(category, setting, tooltipText)
+                    SetOnValueChangedCallback(variable, OnSettingChanged)
+                end
+
+                -- Skyriding
+                do
+                    local variable = "UseDynamicFlightButton"
+                    local name = "Skyriding"
+                    local tooltipText = "Toggle to add a button to the Skyriding UI to quickly learn its traits."
+                    local setting = RegisterAddOnSetting(category, name, variable, type(HelpMePlayDB[variable]), HelpMePlayDB[variable])
+                    CreateCheckbox(category, setting, tooltipText)
+                    SetOnValueChangedCallback(variable, OnSettingChanged)
+                end
+
+                -- War Mode
+                do
+                    local variable = "UseWarMode"
+                    local name = "War Mode"
+                    local tooltipText = "Toggle to automatically enable War Mode when entering or logging into Orgrimmar or Stormwind City.\n\n" ..
+                    LHMP:ColorText("RED", format("This setting doesn't apply to players at or above level %d.", addon.Constants["CHROMIE_TIME_MAX_LEVEL"]))
+                    local setting = RegisterAddOnSetting(category, name, variable, type(HelpMePlayDB[variable]), HelpMePlayDB[variable])
+                    CreateCheckbox(category, setting, tooltipText)
+                    SetOnValueChangedCallback(variable, OnSettingChanged)
                 end
 
                 ------------------------
