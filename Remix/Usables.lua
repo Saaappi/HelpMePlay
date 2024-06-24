e15cbf8bab41759a984b34ed55f0ad507a683451
Saaappi <alex.metz1024@gmail.com>
2024-06-23 18:03:51 -0700

    Error messages are silenced for the Remix button.

--- Remix/Usables.lua
+++ Remix/Usables.lua
@@ -6,6 +6,7 @@ local eventHandler = CreateFrame("Frame")
 local LHMP = LibStub("LibHelpMePlay")
 local remixItems = {210681, 210714, 210715, 210716, 210717, 210718, 211106, 211107, 211123, 211124, 216639, 216640, 216641, 216643, 216644, 219273, 220367, 220368, 220370, 220371, 220372, 220374, 226142, 226143, 226144, 226145, 210984, 217722, 219282, 219264, 219275, 219276, 219274, 219277, 219280, 210982, 219281, 210983, 219262, 219278, 219258, 219279, 219256, 219269, 219267, 210990, 210985, 219266, 219263, 210989, 219272, 219257, 219259, 219260, 219271, 219268, 210987, 210986, 219265, 219261, 219270}
 local btn
+local lastTime
 
 local function MakeButton(anchor, parent, relativeAnchor, xOff, yOff)
 	if not btn then
@@ -22,6 +23,10 @@ local function MakeButton(anchor, parent, relativeAnchor, xOff, yOff)
 		btn:SetAttribute("type", "macro")
 		local macrotext = "/use item:" .. table.concat(remixItems, "\n/use item:")
 		btn:SetAttribute("macrotext", macrotext)
+
+		btn:SetScript("PreClick", function()
+			lastTime = GetTime()
+		end)
 
 		btn:SetScript("OnEnter", function(self)
 			addon.Tooltip_OnEnter(self, "Remix: Mists of Pandaria", "\nClick to combine gems and add threads to your Cloak of Infinite Potential.")
@@ -66,3 +71,8 @@ function(self, event, ...)
 end
 )
 
+hooksecurefunc(UIErrorsFrame, "TryDisplayMessage", function()
+	if GetTime() == lastTime then
+		UIErrorsFrame:Clear()
+	end
+end)
