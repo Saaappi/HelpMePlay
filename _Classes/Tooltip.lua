7068d7fe25c7e5c93042aecbad60d8f5d3e19978
Saaappi <alex.metz1024@gmail.com>
2024-06-25 23:18:44 -0700

    Changed the tooltip anchor.

--- _Classes/Tooltip.lua
+++ _Classes/Tooltip.lua
@@ -1,7 +1,7 @@
 local addonName, addon = ...
 
 addon.Tooltip_OnEnter = function(self, header, text)
-    GameTooltip:SetOwner(self, "ANCHOR_CURSOR_RIGHT")
+    GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
     GameTooltip:SetText(header)
     GameTooltip:AddLine(text, 1, 1, 1, true)
     GameTooltip:Show()
