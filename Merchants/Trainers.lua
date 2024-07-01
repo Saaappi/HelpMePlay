8749921a62e808fe3529cc4d66921fe677575315
Saaappi <alex.metz1024@gmail.com>
2024-06-30 22:41:34 -0700

    Replaced the HelpMePlay.Learn function with addon.BuyTrainerService.

--- Merchants/Trainers.lua
+++ Merchants/Trainers.lua
@@ -7,13 +7,9 @@ eventHandler:SetScript("OnEvent", function(self, event, ...)
 	if event == "PLAYER_INTERACTION_MANAGER_FRAME_SHOW" then
 		local type = ...
 		if type == 7 then
-	        C_Timer.After(addon.Constants["TIMER_DELAY"], function()
-	            HelpMePlay.Learn()
-	        end)
+			addon.BuyTrainerService()
 		end
     elseif event == "TRAINER_UPDATE" then
-        C_Timer.After(addon.Constants["TIMER_DELAY"], function()
-            HelpMePlay.Learn()
-        end)
+        addon.BuyTrainerService()
 	end
 end)
--- _Functions/Trainers.lua
+++ _Functions/Trainers.lua
@@ -1,24 +1,13 @@
-local addonName, addon = ...
-local eventHandler = CreateFrame("Frame")
+local _, addon = ...
 
-eventHandler:RegisterEvent("ADDON_LOADED")
-eventHandler:SetScript("OnEvent", function(self, event, addonLoaded)
-    if event == "ADDON_LOADED" then
-        if addonLoaded == addonName then
-            function HelpMePlay.Learn()
-                local numServices = GetNumTrainerServices()
-                for index = 1, numServices do
-                    -- The player's money must be greater than the protection value for
-                    -- the automation to work.
-                    if GetMoney() > HelpMePlayDB["TrainerProtectionValue"] and HelpMePlayDB["TrainerProtectionValue"] ~= 0 then
-                        SelectTrainerService(index)
-                        BuyTrainerService(index)
-                    end
-                end
-            end
+addon.BuyTrainerService = function()
+    local numServices = GetNumTrainerServices()
+    for index = 1, numServices do
+        -- The player's money must be greater than the protection value for
+        -- the automation to work.
+        if GetMoney() > HelpMePlayDB["TrainerProtectionValue"] and HelpMePlayDB["TrainerProtectionValue"] ~= 0 then
+            SelectTrainerService(index)
+            BuyTrainerService(index)
         end
-
-        -- Unload the event for performance.
-        eventHandler:UnregisterEvent("ADDON_LOADED")
     end
-end)
+end
