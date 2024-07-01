7e6d7315ce4087d32b7f8f2514ef96ce085a3b8d
Saaappi <alex.metz1024@gmail.com>
2024-06-30 22:44:11 -0700

    Removed the Functions\Trainers file since the function was only used in one file.

--- Merchants/Trainers.lua
+++ Merchants/Trainers.lua
@@ -1,5 +1,17 @@
-local addonName, addon = ...
+local _, _ = ...
 local eventHandler = CreateFrame("Frame")
+
+local function BuyTrainerService()
+	local numServices = GetNumTrainerServices()
+    for index = 1, numServices do
+        -- The player's money must be greater than the protection value for
+        -- the automation to work.
+        if GetMoney() > HelpMePlayDB["TrainerProtectionValue"] and HelpMePlayDB["TrainerProtectionValue"] ~= 0 then
+            SelectTrainerService(index)
+            BuyTrainerService(index)
+        end
+    end
+end
 
 eventHandler:RegisterEvent("PLAYER_INTERACTION_MANAGER_FRAME_SHOW")
 eventHandler:RegisterEvent("TRAINER_UPDATE")
@@ -7,9 +19,9 @@ eventHandler:SetScript("OnEvent", function(self, event, ...)
 	if event == "PLAYER_INTERACTION_MANAGER_FRAME_SHOW" then
 		local type = ...
 		if type == 7 then
-			addon.BuyTrainerService()
+			BuyTrainerService()
 		end
     elseif event == "TRAINER_UPDATE" then
-        addon.BuyTrainerService()
+        BuyTrainerService()
 	end
 end)
