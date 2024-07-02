82daf750e5ba684a686665cf6bf20d94115b1aa7
Saaappi <alex.metz1024@gmail.com>
2024-07-01 18:18:46 -0700

    Renamed the BuyTrainerService to BuyService so as not to conflict with the Blizzard function.

--- Merchants/Trainers.lua
+++ Merchants/Trainers.lua
@@ -1,7 +1,7 @@
 local _, _ = ...
 local eventHandler = CreateFrame("Frame")
 
-local function BuyTrainerService()
+local function BuyService()
 	local numServices = GetNumTrainerServices()
     for index = 1, numServices do
         -- The player's money must be greater than the protection value for
@@ -19,9 +19,11 @@ eventHandler:SetScript("OnEvent", function(self, event, ...)
 	if event == "PLAYER_INTERACTION_MANAGER_FRAME_SHOW" then
 		local type = ...
 		if type == 7 then
-			BuyTrainerService()
+			BuyService()
 		end
     elseif event == "TRAINER_UPDATE" then
-        BuyTrainerService()
+        C_Timer.After(0.1, function()
+            BuyService()
+        end)
 	end
 end)
