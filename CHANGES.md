cebb2628bbde357486a09ddf8ae9a7bab65d0355
Saaappi <alex.metz1024@gmail.com>
2024-07-24 10:55:26 -0700

    Fixed Skyriding talents and the button.

--- CHANGES.md
+++ CHANGES.md
@@ -1,12 +1,9 @@
-## Version 3.0.1 - 2024/07/23
+## Version 3.0.2 - 2024/07/24
 
 ### Added
-- Added an out-of-date message to the tooltip of the micro menu button when the addon is considered outdated.
+- Implemented support for the "new" Skyriding talents.
 ### Changed or Updated
-- Changed the location of the Categorize Mounts button to accommodate Blizzard's new Skyriding button.
 ### Deprecated
 ### Removed
 ### Fixed
-- Resolved an issue where the Settings failed to generate checkboxes and dropdown menus.
-- Addressed an issue with the Quick Quest Abandon keybind, restoring its intended functionality.
-- Resolved an issue where world events were erroneously detected after their scheduled end time on the current day.
+- Resolved the issue where the Skyriding talent button was not appearing in the Skyriding talents frame.
--- Talents/Skyriding.lua
+++ Talents/Skyriding.lua
@@ -3,29 +3,22 @@ local dynamicFlightTalentsButton = CreateFrame("Button")
 
 local skyridingTalents = {
     { ["nodeID"] = 64066, ["entryID"] = 82387 }, 	-- Take to the Skies
-    { ["nodeID"] = 81466, ["entryID"] = 102427 }, 	-- Redirection
     { ["nodeID"] = 64069, ["entryID"] = 82390 }, 	-- Dynamic Stretching
-    { ["nodeID"] = 64068, ["entryID"] = 82389 }, 	-- Thrill Chaser
     { ["nodeID"] = 94579, ["entryID"] = 117167 }, 	-- Second Wind
-    { ["nodeID"] = 64067, ["entryID"] = 82388 }, 	-- Ohn'ahra's Gusts
-    { ["nodeID"] = 64065, ["entryID"] = 82385 },    -- Dragonrider's Compassion
-    { ["nodeID"] = 92672, ["entryID"] = 114806 }, 	-- Aerial Halt
+    { ["nodeID"] = 64068, ["entryID"] = 82389 }, 	-- Thrill Chaser
     { ["nodeID"] = 64064, ["entryID"] = 82384 }, 	-- Restorative Travels
+    { ["nodeID"] = 92672, ["entryID"] = 114806 }, 	-- Aerial Halt
     { ["nodeID"] = 92679, ["entryID"] = 114815 }, 	-- Ground Skimming
-    { ["nodeID"] = 92671, ["entryID"] = 114805 }, 	-- Airborne Recovery
-    { ["nodeID"] = 64063, ["entryID"] = 82383 }, 	-- Airborne Tumbling
-    { ["nodeID"] = 92678, ["entryID"] = 114814 }, 	-- Land's Blessing
-    { ["nodeID"] = 94578, ["entryID"] = 117165 }, 	-- Wind's Respite
     { ["nodeID"] = 64061, ["entryID"] = 82380 }, 	-- Dragonriding Learner
-    { ["nodeID"] = 94577, ["entryID"] = 117164 }, 	-- Swift Skimming
-    { ["nodeID"] = 64062, ["entryID"] = 82381 },    -- Dragonrider's Hunt
     { ["nodeID"] = 64059, ["entryID"] = 82378 }, 	-- Beyond Infinity
     { ["nodeID"] = 64060, ["entryID"] = 82379 }, 	-- Yearning for the Sky
     { ["nodeID"] = 64058, ["entryID"] = 82377 }, 	-- At Home Aloft
+    { ["nodeID"] = 100167, ["entryID"] = 123785 },  -- Riding Along Enabled
+    { ["nodeID"] = 100168, ["entryID"] = 123787 }   -- Whirling Surge
 }
 
 local function PurchaseTalents()
-    local treeID = 672
+    local treeID = GenericTraitFrame:GetTalentTreeID() -- 672 (Skyriding)
     local configID = C_Traits.GetConfigIDByTreeID(treeID)
 
     C_Traits.ResetTree(configID, treeID)
@@ -40,15 +33,31 @@ local function PurchaseTalents()
             end
         end
         C_Traits.CommitConfig(configID)
+        --[[for talentButton in GenericTraitFrame:EnumerateAllTalentButtons() do
+            local nodeInfo = talentButton:GetNodeInfo()
+            if #nodeInfo.entryIDs > 1 then
+                for _, entryID in next, nodeInfo.entryIDs do
+                    local entryInfo = C_Traits.GetEntryInfo(configID, entryID)
+                    local definitionInfo = C_Traits.GetDefinitionInfo(entryInfo.definitionID)
+                    local spellName = (GetSpellInfo(definitionInfo.spellID))
+                    print(nodeInfo.ID .. "-" .. entryID .. " (" .. spellName .. ")")
+                end
+            else
+                local entryInfo = C_Traits.GetEntryInfo(configID, nodeInfo.entryIDs[1])
+                local definitionInfo = C_Traits.GetDefinitionInfo(entryInfo.definitionID)
+                local spellName = (GetSpellInfo(definitionInfo.spellID))
+                print(nodeInfo.ID .. "-" .. nodeInfo.entryIDs[1] .. " (" .. spellName .. ")")
+            end
+        end]]
     end)
 end
 
 EventRegistry:RegisterCallback("GenericTraitFrame.OnShow", function()
     if HelpMePlayDB["UseDynamicFlightButton"] == false then return end
 
-	if GenericTraitFrame.Header.Title:GetText() == "Dragonriding" then
+	if GenericTraitFrame.Header.Title:GetText() == GENERIC_TRAIT_FRAME_DRAGONRIDING_TITLE then
         dynamicFlightTalentsButton = HelpMePlay.CreateWidget("IconButton", {
-            name = format("%sDynamicFlightTalentsButton", addonName),
+            name = format("%sSkyridingTalentButton", addonName),
             texture = 4728198,
             parent = GenericTraitFrame.Currency,
             anchor = "TOPRIGHT",
