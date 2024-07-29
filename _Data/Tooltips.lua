0783aa3f772495ca427ec34dd35ff07e9dc59e99
Saaappi <alex.metz1024@gmail.com>
2024-07-29 12:31:30 -0700

    Moved tooltips to their own file.

---
+++ _Data/Tooltips.lua
@@ -0,0 +1,7 @@
+local _, HelpMePlay = ...
+
+HelpMePlay.Tooltips = {
+    ["MicroMenuButton"] = "Click to open the settings.\n\n" ..
+    "Shift-Click to open the Talent Importer utility.\n\n" ..
+    "Ctrl-Click to reset the current character's configuration.",
+}
