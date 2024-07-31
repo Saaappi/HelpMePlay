cbcbc8693db0d0074e0e52c88e5ac90b349c7ce9
Saaappi <alex.metz1024@gmail.com>
2024-07-31 12:13:27 -0700

    Removed unnecessary code.

--- GuildBank/GuildBank.lua
+++ GuildBank/GuildBank.lua
@@ -1,17 +1,12 @@
 local _, HelpMePlay = ...
 local eventHandler = CreateFrame("Frame")
-local LHMP = LibStub("LibHelpMePlay")
 
--- Initiates a deposit action to the guild bank if the player's current money exceeds a certain threshold,
--- or withdraws from the guild bank if the player's money is below the threshold.
 local function ManageGuildBankFunds(transactionType, transactionAmount)
     C_Timer.After(HelpMePlay.Constants["TIMER_DELAY"] + 0.4, function()
         if transactionType == "DEPOSIT" then
-            --DepositGuildBankMoney(transactionAmount)
             C_Bank.DepositMoney(2, transactionAmount)
             HelpMePlay.Print(format("Deposited %s.", C_CurrencyInfo.GetCoinTextureString(transactionAmount)))
         else
-            --WithdrawGuildBankMoney(transactionAmount)
             C_Bank.WithdrawMoney(2, transactionAmount)
             HelpMePlay.Print(format("Withdrew %s.", C_CurrencyInfo.GetCoinTextureString(transactionAmount)))
         end
@@ -54,7 +49,6 @@ eventHandler:SetScript("OnEvent", function(self, event, ...)
             local type = ...
             if type == 8 then
                 if C_Bank.CanDepositMoney(2) then
-                    -- The player can deposit money into the Warband bank.
                     local transactionAmount = GetMoney() - (HelpMePlayDB["DepositKeepAmount"])
                     if HelpMePlayDB["DepositKeepMeSafe"] then
                         HandleTransaction(transactionAmount)
@@ -67,53 +61,6 @@ eventHandler:SetScript("OnEvent", function(self, event, ...)
                         end
                     end
                 end
-                --[[if HelpMePlayDB["DepositKeepMeSafe"] then
-                    if transactionAmount > 0 then
-                        StaticPopupDialogs["HELPMEPLAY_DEPOSIT_KEEP_ME_SAFE"] = {
-                            text = format("You're about to deposit %s to %s. Do you want to continue?", C_CurrencyInfo.GetCoinTextureString(transactionAmount), LHMP:ColorText("ARTIFACT", (GetGuildInfo("player")))),
-                            button1 = ACCEPT,
-                            button2 = CANCEL,
-                            OnAccept = function()
-                                ManageGuildBankFunds("DEPOSIT", transactionAmount)
-                            end,
-                            timeout = 0,
-                            showAlert = false,
-                            whileDead = false,
-                            hideOnEscape = true,
-                            hasEditBox = false,
-                            enterClicksFirstButton = false,
-                            preferredIndex = 3,
-                        }
-                        StaticPopup_Show("HELPMEPLAY_DEPOSIT_KEEP_ME_SAFE")
-                    else
-                        transactionAmount = transactionAmount * -1
-                        if transactionAmount > 0 then
-                            StaticPopupDialogs["HELPMEPLAY_DEPOSIT_KEEP_ME_SAFE"] = {
-                                text = format("You're about to withdraw %s from %s. Do you want to continue?", C_CurrencyInfo.GetCoinTextureString(transactionAmount), LHMP:ColorText("ARTIFACT", (GetGuildInfo("player")))),
-                                button1 = ACCEPT,
-                                button2 = CANCEL,
-                                OnAccept = function()
-                                    ManageGuildBankFunds("WITHDRAW", transactionAmount)
-                                end,
-                                timeout = 0,
-                                showAlert = false,
-                                whileDead = false,
-                                hideOnEscape = true,
-                                hasEditBox = false,
-                                enterClicksFirstButton = false,
-                                preferredIndex = 3,
-                            }
-                            StaticPopup_Show("HELPMEPLAY_DEPOSIT_KEEP_ME_SAFE")
-                        end
-                    end
-                else
-                    if transactionAmount > 0 then
-                        ManageGuildBankFunds("DEPOSIT", transactionAmount)
-                    else
-                        transactionAmount = transactionAmount * -1
-                        ManageGuildBankFunds("WITHDRAW", transactionAmount)
-                    end
-                end]]
             end
         end
     end
