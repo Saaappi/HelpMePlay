744072f9448385520bb7fef8fa76eb6779fb6e54
Saaappi <alex.metz1024@gmail.com>
2024-08-02 23:53:35 -0700

    Maybe now the War Mode bug is fixed!

--- Quests/WarMode.lua
+++ Quests/WarMode.lua
@@ -1,19 +1,12 @@
 local _, HelpMePlay = ...
 local eventHandler = CreateFrame("Frame")
 
-local function ToggleWarMode()
-	C_PvP.ToggleWarMode()
-	eventHandler:UnregisterEvent("ZONE_CHANGED_NEW_AREA")
-end
-
 local function OnTaxi()
-    C_Timer.After(1, function()
-        if UnitOnTaxi("player") then
-            OnTaxi()
-        else
-            ToggleWarMode()
-        end
-    end)
+	if UnitOnTaxi("player") then
+		C_Timer.After(0.5, OnTaxi)
+	else
+		return false
+	end
 end
 
 eventHandler:RegisterEvent("ZONE_CHANGED_NEW_AREA")
@@ -24,8 +17,22 @@ eventHandler:SetScript("OnEvent", function(self, event, ...)
 
 		if HelpMePlay.playerLevel < HelpMePlay.Constants["CHROMIE_TIME_MAX_LEVEL"] then
 			local mapID = C_Map.GetBestMapForUnit("player")
-			if (mapID == 84 or mapID == 85) and (not C_PvP.IsWarModeActive()) then
-				OnTaxi()
+			if mapID == 84 or mapID == 85 then
+				if not OnTaxi() then
+					local isEnlisted = false
+					local warModeSpells = {269083, 282559}
+					for _, spellID in next, warModeSpells do
+						local aura = C_UnitAuras.GetPlayerAuraBySpellID(spellID)
+						if aura and aura.name then
+							isEnlisted = true
+							break
+						end
+					end
+					if not isEnlisted then
+						C_PvP.ToggleWarMode()
+						eventHandler:UnregisterEvent("ZONE_CHANGED_NEW_AREA")
+					end
+				end
 			end
 		else
 			eventHandler:UnregisterEvent("ZONE_CHANGED_NEW_AREA")
