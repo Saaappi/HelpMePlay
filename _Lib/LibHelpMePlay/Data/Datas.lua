cb40b1e470d3185ccbeffead0bb4b61789e7ad2b
Saaappi <alex.metz1024@gmail.com>
2024-08-20 19:24:26 -0700

    Reformatted some gossips.

--- _Lib/LibHelpMePlay/Data/Datas.lua
+++ _Lib/LibHelpMePlay/Data/Datas.lua
@@ -210,27 +210,27 @@ LHMP.Gossips = {
     -- Teldrassil
     [34761] = {{ID = 38299, CanConfirm = false, Conditions = {{"LEVEL_LOWER", 10}, {"QUEST_ACTIVE", {483}}}}},
     -- Elwynn Forest
-    [240] = {{ID = 38407, CanConfirm = false, Conditions = {"LEVEL_LOWER = 10", "QUEST_ACTIVE = 35"}}},
+    [240] = {{ID = 38407, CanConfirm = false, Conditions = {{"LEVEL_LOWER", 10}, {"QUEST_ACTIVE", {35}}}}},
     -- Darkshore
-    [33037] = {{ID = 35901, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 13547"}}},
-    [33035] = {{ID = 35900, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 13547"}}},
-    [33033] = {{ID = 35899, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 13547"}}},
-    [33001] = {{ID = 37276, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 13547"}}},
-    [33072] = {{ID = 37858, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 13882"}}},
-    [34392] = {{ID = 36843, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 13885"}}},
+    [33037] = {{ID = 35901, CanConfirm = false, Conditions = {{"QUEST_ACTIVE", {13547}}}}},
+    [33035] = {{ID = 35900, CanConfirm = false, Conditions = {{"QUEST_ACTIVE", {13547}}}}},
+    [33033] = {{ID = 35899, CanConfirm = false, Conditions = {{"QUEST_ACTIVE", {13547}}}}},
+    [33001] = {{ID = 37276, CanConfirm = false, Conditions = {{"QUEST_ACTIVE", {13547}}}}},
+    [33072] = {{ID = 37858, CanConfirm = false, Conditions = {{"QUEST_ACTIVE", {13882}}}}},
+    [34392] = {{ID = 36843, CanConfirm = false, Conditions = {{"QUEST_ACTIVE", {13885}}}}},
     -- Ashenvale
-    [34289] = {{ID = 37319, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 13886", "QUEST_OBJECTIVE_INCOMPLETE = 13886,1"}}},
-    [34377] = {{ID = 37711, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 13935", "QUEST_OBJECTIVE_INCOMPLETE = 13935,1"}}},
+    [34289] = {{ID = 37319, CanConfirm = false, Conditions = {{"QUEST_ACTIVE", {13886}}, {"QUEST_OBJECTIVES_COMPLETE", {{13886,1}}}}}},
+    [34377] = {{ID = 37711, CanConfirm = false, Conditions = {{"QUEST_ACTIVE", {13935}}, {"QUEST_OBJECTIVES_COMPLETE", {{13935,1}}}}}},
     [34618] = {
         {ID = 38020, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 26482"}},
         {ID = 38287, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 26482"}}
     },
     -- Stonetalon Mountains
-    [41071] = {{ID = 38774, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 25669", "QUEST_OBJECTIVE_INCOMPLETE = 25669,1"}}},
+    [41071] = {{ID = 38774, CanConfirm = false, Conditions = {{"QUEST_ACTIVE", {25669}}, {"QUEST_OBJECTIVES_COMPLETE", {{25669,1}}}}}},
     -- Southern Barrens
-    [38383] = {{ID = 38139, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 25191"}}},
-    [38619] = {{ID = 37512, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 24934", "LEVEL_LOWER = 20"}}},
-    [38622] = {{ID = 37724, CanConfirm = false, Conditions = {"QUEST_COMPLETE = 24934", "LEVEL_LOWER = 20"}}},
+    [38383] = {{ID = 38139, CanConfirm = false, Conditions = {{"QUEST_ACTIVE", {25191}}}}},
+    [38619] = {{ID = 37512, CanConfirm = false, Conditions = {{"LEVEL_LOWER", 20}, {"QUEST_ACTIVE", {24934}}}}},
+    [38622] = {{ID = 37724, CanConfirm = false, Conditions = {{"LEVEL_LOWER", 20}, {"QUEST_COMPLETE", {24934}}}}},
     [37570] = {{ID = 37335, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 24601", "QUEST_OBJECTIVE_INCOMPLETE = 24601,1"}}},
     [37743] = {{ID = 38196, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 25057", "QUEST_OBJECTIVE_INCOMPLETE = 25057,1"}}},
     [39155] = {{ID = 36883, CanConfirm = false, Conditions = {"QUEST_ACTIVE = 25120", "QUEST_OBJECTIVE_INCOMPLETE = 25120,1"}}},
