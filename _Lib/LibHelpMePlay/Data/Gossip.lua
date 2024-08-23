7b28af1c8487e8348fb031e3c7dba621ee0bb851
Saaappi <alex.metz1024@gmail.com>
2024-08-23 15:54:38 -0700

    Added gossips for the remains of the TWW questline intro.

--- _Lib/LibHelpMePlay/Data/Gossip.lua
+++ _Lib/LibHelpMePlay/Data/Gossip.lua
@@ -679,4 +679,11 @@ LHMP.Gossips = {
     [213624] = {{ID = 120882, CanConfirm = false, Conditions = {{"QUESTS_ACTIVE", {78715}}}}},
     [214669] = {{ID = 121542, CanConfirm = false, Conditions = {{"QUESTS_ACTIVE", {80500}}}}},
     [213625] = {{ID = 123201, CanConfirm = true, Conditions = {{"QUESTS_COMPLETE", {81930}}, {"QUESTS_INACTIVE", {78714}}}}},
+    [213627] = {
+        {ID = 121395, CanConfirm = false, Conditions = {{"QUESTS_ACTIVE", {78717}}}},
+        {ID = 120143, CanConfirm = false, Conditions = {{"QUESTS_ACTIVE", {78722}}}},
+        {ID = 125089, CanConfirm = true, Conditions = {{"QUESTS_COMPLETE", {78716}}, {"QUESTS_INACTIVE", {78717,78719,78722,80500}}}}
+    },
+    [220018] = {{ID = 121860, CanConfirm = false, Conditions = {{"QUESTS_ACTIVE", {78721}}}}},
+    [219956] = {{ID = 124153, CanConfirm = false, Conditions = {{"QUESTS_ACTIVE", {80321}}}}},
 }
