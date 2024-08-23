629e8c1a7e92abc075995af89f6f0750744d6f85
Saaappi <alex.metz1024@gmail.com>
2024-08-23 12:23:56 -0700

    Small change to Gossip.

--- Gossip/Gossip.lua
+++ Gossip/Gossip.lua
@@ -63,7 +63,7 @@ eventHandler:SetScript("OnEvent", function(self, event, ...)
 
         -- If the gossip from is visible, then add a button that can be used to
         -- quickly retrieve the NPC's ID, as well as the listed options.
-        if GossipFrame:IsVisible() and C_BattleNet.GetAccountInfoByGUID(HelpMePlay.playerGUID).battleTag == HelpMePlay.Constants["AUTHOR_BATTLETAG"] then
+        if (GossipFrame and GossipFrame:IsVisible()) and C_BattleNet.GetAccountInfoByGUID(HelpMePlay.playerGUID).battleTag == HelpMePlay.Constants["AUTHOR_BATTLETAG"] then
             if not gossipButton then
                 gossipButton = HelpMePlay.CreateWidget("IconButton", {
                     name = string.format("%sGossipInfoButton", addonName),
