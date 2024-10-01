00e0decdb5ca0351d68bf87628a0daf7c5945504
Saaappi <alex.metz1024@gmail.com>
2024-09-30 23:58:21 -0700

    Small update to the micro button's tooltip.

--- _Data/Tooltips.lua
+++ _Data/Tooltips.lua
@@ -4,7 +4,7 @@ local LHMP = LibStub("LibHelpMePlay")
 HelpMePlay.Tooltips = {
     ["MicroMenuButton"] = LHMP:ColorText("GOLD", "Click to open the settings.\n\n" ..
     "Shift-Click to open the Talent Importer utility.\n\n" ..
-    "Ctrl-Click to reset the current character's configuration."),
+    "Ctrl-Click to reset the current character's configuration settings."),
     ["PetBattleBandageButton"] = LHMP:ColorText("GOLD", "Use left-click to heal your battle pets if they're injured. The " .. LHMP:ColorText("COMMON", "Revive Battle Pets") .. " ability is used before bandages.\n\n" ..
     "Use right-click to equip your " .. LHMP:ColorText("RARE", "[Safari Hat]") .. ".\n\n" ..
     "Click and hold to drag."),
